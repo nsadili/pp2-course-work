909c96bdf0a4c84c8bfdfae2049dee2e1e31acb7
Vidadi Nabiyev 13119 <vnabiyev13119@ada.edu.az>
2023-02-01 11:15:16 +0400

    Easy tasks completed on Exercise 2

---
+++ Week02/Sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Sum{
+    public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+		System.out.print("Enter the first numbers: ");
+		var num1 = sc.nextInt();
+		System.out.print("Enter the second numbers: ");
+		var num2 = sc.nextInt();
+
+		var Sum = num1 + num2;
+
+		System.out.println("The sum of " + num1 + " and " + num2 + " is " + Sum + ".");
+	}
+
+    }
+}
