3856397102237b4f0e2e939b16d3ab3ec68afee0
Nuraddin Sadili <nsadili@ada.edu.az>
2023-02-01 11:15:11 +0400

    Complete Easy tasks on Exercise 2

---
+++ Week02/Calc.java
@@ -0,0 +1,14 @@
+public class Calc {
+    public static void main(String[] args) {
+
+        if (args.length < 2) {
+            System.out.println("Please, provide some input!");
+            System.exit(1);
+        }
+
+        var num1 = Integer.valueOf(args[0]);
+        var num2 = Integer.parseInt(args[0]);
+
+        System.out.println(num1 + num2);
+    }
+}
---
+++ Week02/MonthInfo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MonthInfo {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        var no = scan.nextInt();
+
+        switch (no) {
+            default:
+                System.out.println("Please, enter a valid month number");
+                break;
+            case 1:
+                System.out.println("Jan - 31");
+                break;
+            case 2:
+                System.out.println("Feb - 28");
+                break;
+            case 3:
+                System.out.println("Mar - 31");
+                break;
+
+        }
+
+    }
+}
