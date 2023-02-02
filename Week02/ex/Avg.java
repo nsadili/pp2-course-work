04127db3e0aa98cc73050e32edaca6f5670b2a8e
Ibrahim Huseynov <ihuseynov16086@ada.edu.az>
2023-02-02 18:46:13 +0400

    Add week 02 ex

---
+++ Week02/ex/Avg.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Avg {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        var count =0;
+        var sum=0;
+        while (scan.hasNextInt()){
+
+            sum+= scan.nextInt();
+            count++;
+
+
+        }
+        var avg= (float)sum/count;
+        System.out.println(avg);
+
+
+
+
+
+
+
+
+
+ }
+}
