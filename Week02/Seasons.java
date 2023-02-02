0951406e254cda5614c54d9812acb4cc46f51e6a
Behruz Gurbanli <bgurbanli14129@ada.edu.az>
2023-02-02 21:16:09 +0400

    Fixed all errors

--- Week02/Seasons.java
+++ Week02/Seasons.java
@@ -11,14 +11,14 @@ public class Seasons {
             case 1:
             case 2:
             case 3: System.out.println("Spring");break;
-            case 4: //
+            case 4:
             case 5:
             case 6: System.out.println("Summer");
             case 7:
-            case 8:
             case 9: System.out.println("Autumn");break;
             case 10:
             case 11:
+            case 13:
             case 12: System.out.println("Winter");break;
         }
     }
