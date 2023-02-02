e0eee94e00185b334e0bf8ccf6819ea378f9256c
“Abov <aakhundov17192@ada.edu.az>
2023-02-03 02:06:31 +0400

    .

---
+++ Week02/PW/time.java
@@ -0,0 +1,10 @@
+import java.text.Format;
+import java.util.Scanner;
+
+public class time {
+    public static void main(String[]args){
+    Scanner scan=new Scanner(System.in);
+    var sec=scan.nextInt();
+    System.out.println((float)sec/60);
+    }
+}
