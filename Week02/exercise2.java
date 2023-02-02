7a7c89212d21ae5141ba628552b0a0f68edaf70f
unknown <alievsamil994@gmail.com>
2023-02-02 21:03:33 +0400

    Exercises  added

---
+++ Week02/exercise2.java
@@ -0,0 +1,9 @@
+public class exercise2 {
+    public static void main(String args[]) {
+    int x = 5;
+    int y = 3;
+    int z = 2;
+    int c = x + y +z;
+    System.out.println("The result is:" + c); 
+    }
+    }
---
+++ Week02/exercise3.java
@@ -0,0 +1,10 @@
+public class exercise3 {
+    public static void main(String args[]){
+int x = 10;
+long y = 20;
+float z = 30;
+float res = x + y +z;
+System.out.println("The result is:"+res);
+    }
+    
+}
