3e6df2823cc63f4e20b22c73274ae25069538153
“Abov <aakhundov17192@ada.edu.az>
2023-02-03 00:58:31 +0400

    dwd

---
+++ Sum.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class Sum {
+    
+public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     var num1=sc.nextInt();
+     var num2=sc.nextInt();
+
+     var sum = num1 + num2;
+     System.out.println(sum);
+}
+
+    
+}
