a307436b09f1f2d9a41e85012fe80a92a43d2e85
AliyevaVasila <123635190+AliyevaVasila@users.noreply.github.com>
2023-02-02 19:10:28 +0300

    Delete Elnar_Ashrafov directory

--- Elnar_Ashrafov/FirstJavaApp.java
+++
@@ -1,5 +0,0 @@
-public class FirstJavaApp{
-public static void main(String [] args){
-System.out.println("Hello, I am Elnar Ashrafov");
-}
-}
--- Elnar_Ashrafov/ThirdJavaApp.java
+++
@@ -1,5 +0,0 @@
-public class SecondJavaApp{
-public static void main(){
-System.out.println("This is the third trial to fix some stuff");
-}
-}
