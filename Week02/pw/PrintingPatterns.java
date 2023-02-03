8877764dcf5697c163d683890b2cf36ec4350107
jala-asadli-16143 <jasadli16143@ada.edu.az>
2023-02-04 00:33:06 +0400

    Add week 02 pw

---
+++ Week02/pw/PrintingPatterns.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PrintingPatterns {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the pattern: ");
+        int size = sc.nextInt();
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if((j + i) % 2 == 0){ // if the sum is even, print #; otherwise, *
+                    System.out.print("*");
+            } else {
+              System.out.print("#");
+                   }
+            } 
+            System.out.println();
+
+        }
+    }
+    
+}
+    
+
