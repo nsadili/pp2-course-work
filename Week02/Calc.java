a82893fb12503ca38c8a5ca2dc9bff4c4fcc3fb0
Leyla_Khudiyeva <lkhudiyeva17248@ada.edu.az>
2023-02-03 10:31:23 +0400

    week 2 codes

---
+++ Week02/Calc.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner (System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int sum=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        int rem=a%b;
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(rem);
+    }
+}
