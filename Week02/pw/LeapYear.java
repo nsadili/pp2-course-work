2e93196b3405161976bfff0dded6eac227d38222
Ilkin Alishov 16120 <ialishov16120@ada.edu.az>
2023-02-03 13:19:00 +0400

    Leap year founder

---
+++ Week02/pw/LeapYear.java
@@ -0,0 +1,13 @@
+//package Week02.pw;
+
+public class LeapYear {
+    public static void main(String args[]){
+        int year = Integer.valueOf(args[0]);
+        if(year%4 == 0){
+            System.out.println(year + " is a leap year.");
+        }
+        else{
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
