747e1626ba1b6c918391123026cb27b41f04cf3f
BakhtiyarGuluzada <bguluzada15987@ada.edu.az>
2023-02-03 10:31:28 +0400

    Ex from week 2

---
+++ Week02/ex/Calc.java
@@ -0,0 +1,16 @@
+public class Calc {
+    public static void main(String[] args) {
+        if(args.length < 2){
+            System.out.println("Please enter two integers:");
+            System.exit(1);
+        }
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+      //  System.out.println(args[1]+args[0]);
+
+      var a= Integer.parseInt(args[0]);
+      var b= Integer.valueOf(args[1]);
+      System.out.println(a+b);
+    }
+}
---
+++ Week02/ex/Fact.java
@@ -0,0 +1,23 @@
+public class Fact {
+    public static void main(String[] args) {
+        if(args.length < 1){
+            System.out.println("Please enter an integer:");
+            System.exit(1);
+        }
+
+        var n= Integer.parseInt(args[0]);
+
+        if(n<0){
+            System.out.println("Please enter a non-negative integer:");
+            System.exit(1);
+        }
+        var f= 1L;
+        var c= 1;
+
+        while(c<n){
+            f*=c;
+            c++;
+        }
+        System.out.printf("%d!=%d\n",n,f);
+    }
+}
