a6391b3c8d94f843382bcc539dee1095e07898cb
Nuraddin Sadili <nsadili@ada.edu.az>
2023-02-03 16:30:56 +0400

    Add week 02 Exs

---
+++ Week02/pw/CoinTossing.java
@@ -0,0 +1,21 @@
+public class CoinTossing {
+    public static void main(String[] args) {
+
+        if (args.length < 1) {
+            System.exit(1);
+        }
+
+        var nbOfTrails = Integer.valueOf(args[0]);
+        double res;
+
+        for (int i = 1; i <= nbOfTrails; i++) {
+            res = Math.random();
+
+            if (res < 0.5)
+                System.out.println("Head");
+            else
+                System.out.println("Tail");
+        }
+
+    }
+}
