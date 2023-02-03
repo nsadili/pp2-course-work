ae91548b6d8586fb98833909f4f56550a7f724e6
Aliya Zulfugarli 13080 <azulfugarli13080@ada.edu.az>
2023-02-03 14:31:34 +0400

    Added my java files

---
+++ Week02/ex/Fact.java
@@ -0,0 +1,19 @@
+public class Fact {
+
+    public static void main(String[] args);
+
+    if (n < 0){
+        System.out.println("Please,enter a non-negative number.");
+        System.exit(status:1);
+    }
+
+     
+     var f=1;
+     var c=1;
+
+     while (c <=n) {
+        f *=c;
+        c++;
+     }
+     System.out.printf(format:"%d!= %d\n",n,f)
+}
