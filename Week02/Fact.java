01cbc12bacb91392d90da25e8068eb6d9896b410
Seljan Musayeva <smusayeva17601@ada.edu.az.com>
2023-02-03 19:05:29 +0400

    Complete easy tasks on exercise 2

---
+++ Week02/Fact.java
@@ -0,0 +1,21 @@
+public class Fact {
+    public static void main(String[]args){
+        if(args.length<1){
+            System.out.println("Please, provide a number!");
+            System.exit(1);
+        }    
+     var n=Integer.parseInt(args[0]);
+     if(n<0){
+        System.out.println("Please, provide a positive number!");
+            System.exit(1);
+     }
+     var f=1L;
+     var c=1;
+     while(c<=n){
+        f*=c;
+        c++;
+     }
+     System.out.printf("%d!=%d\n",n,f);
+    }
+    
+}
