735ca074296ccfb84e3ea4d98c5d868275c2713e
Shamil Abbasov 16084 <sabbasov16084@ada.edu.az>
2023-02-04 20:17:09 +0400

    modofoed version for RandomPoints

--- Week02/pw/RandomPointsInCircle.java
+++ Week02/pw/RandomPointsInCircle.java
@@ -1,17 +1,22 @@
 public class RandomPointsInCircle {
     public static void main(String[] args) {
+        
+        if(args.length<1){
+            System.out.println("Please send the argument");
+            return ;
+        }
 
-        var cnt=0;
-
-        while(cnt==0){
-            var x=Math.random();
-            var y=Math.random();
+        var cnt=Integer.parseInt(args[0]);
+        var i=0;
+        while(i!=cnt){
+            var x=2*Math.random()-1;
+            var y=2*Math.random()-1;
             if(x*x+y*y<=1){
                 System.out.printf("x: %f , y: %f\n",x,y);
-                cnt++;
+                i++;
             }
         }
-        System.out.println("Program is finished");
+        System.out.println("Program is finished after " + cnt+ " times");
 
 
     }
