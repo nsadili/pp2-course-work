cf4415e4cfc9804d84f2dbc79b91496814724f35
rufat_hajizada_16231 <rufeth04@gmail.com>
2023-02-04 23:19:23 +0400

    new file

---
+++ Week02/first.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int i=0; i<n; i++){
+            System.out.println("hello");
+        }
+    }
+}
