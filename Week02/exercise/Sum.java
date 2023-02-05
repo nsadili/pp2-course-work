c7d7c34939624140632ed2a65cf500c4f52b07e0
aydanmaliki 17577 <amaliki17577@ada.edu.az>
2023-02-05 20:25:31 +0400

    exercise

---
+++ Week02/exercise/Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] arr) {
+        Scanner scan=new Scanner(System.in);
+        int n1 = scan.nextInt();
+        var n2 = scan.nextInt();
+        var sum= n1 + n2 ;
+        System.out.println(sum);
+
+
+    }
+
+}
