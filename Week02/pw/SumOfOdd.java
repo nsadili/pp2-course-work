b86d9651363120bff36412fc9c42632c22b6842e
roya alizada 16092 <ralizada16092@ada.edu.az>
2023-02-06 02:21:37 +0400

    pw

---
+++ Week02/pw/SumOfOdd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfOdd {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int  sum = 0;
+        for(int i = a; i <= b; i++ ){
+            if(i % 2 != 0){
+                sum = sum +i;
+            }
+           
+        }
+        System.out.println(sum);
+    }
+    
+}
+}
