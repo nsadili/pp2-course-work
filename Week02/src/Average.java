2639d531be62879c8eddcfb0da5a83e78c19f836
Huseyn Jafarli 18088 <hjeferli18088@ada.edu.az>
2023-02-05 14:57:25 +0400

    easy tasks

---
+++ Week02/src/Average.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Average {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        var count=0;
+        var sum =0;
+        while(scan.hasNextInt()){
+            sum +=scan.nextInt();
+count ++;
+        }
+    var avg = (float)sum/count;
+System.out.println(avg);
+
+
+    }
+}
