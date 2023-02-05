0614a7316691eded60eb6202602839ca25aabc22
jala-asadli-16143 <jasadli16143@ada.edu.az>
2023-02-05 23:02:19 +0400

    Add week 02 pw

---
+++ Week02/pw/RollingDie.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class RollingDie {
+    public static void main(String[] args){
+
+        Random sc = new Random();
+        double roll = sc.nextDouble();
+        
+        int result;
+        if (roll < 1.0/8.0) {
+            result = 1;
+        } else if(roll < 2.0/8.0){
+            result = 2;
+        } else if(roll < 3.0/8.0){
+            result = 3;
+        } else if(roll < 4.0/8.0){
+            result = 4;
+        } else if(roll < 6.0/8.0){
+            result = 5;
+        } else {
+            result = 6;
+        }
+        
+        System.out.println("The result is: " + result);
+    }
+}
