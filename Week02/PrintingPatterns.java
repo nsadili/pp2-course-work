cc21c4cb4ea701b3c748d34a664d76b8f71c1b04
IsmayilovaSara <104385057+IsmayilovaSara@users.noreply.github.com>
2023-02-05 20:39:50 +0400

    Add files via upload

---
+++ Week02/PrintingPatterns.java
@@ -0,0 +1,29 @@
+public class PrintingPatterns
+{
+    public static void main(String[] args)
+    {
+        if (args.length < 1)
+        {
+            System.out.println("Please enter a  number");
+            System.exit(1);
+        }
+
+        var size = Integer.parseInt(args[0]);
+
+        for (int i = 0; i < size; i++)
+     {
+        for (int j = 0; j < size; j++)
+         {
+            if ((i + j) % 2 == 0) 
+            {
+                System.out.print("* ");
+            } 
+            else 
+            {
+                System.out.print("# ");
+            }
+        }
+        System.out.println();
+     }
+    }
+}
