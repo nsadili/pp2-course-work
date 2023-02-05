151e1d981254eb1c2d62abe1e365f438e87f49a1
asger_huseynli <ahuseynli17683@ada.edu.az>
2023-02-06 01:15:47 +0400

    Asgar

---
+++ Week02/ex/Average.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = 0, s = 0;
+
+        while (sc.hasNextDouble()) {
+            sum += isc.nextDouble();
+            c++;
+        }
+
+        if (c == 0) {
+            System.out.println("Error");
+        } else
+        {
+            double a = s / c;
+
+            System.out.println(average);
+        }
+    }
+}
