0818e64cf4649fbd042e868c91ee9cd8bab7a3b9
Aliya Zulfugarli 13080 <azulfugarli13080@ada.edu.az>
2023-02-05 23:06:40 +0400

    Added my java files

---
+++ Week02/ex/CoinTossing.java
@@ -0,0 +1,15 @@
+public class CoinTossing {
+    public static void main(String[] args){
+
+        if (args.length < 1){
+            System.out.println("Enter a value");
+            System.exit(1);
+        }
+
+        
+
+        var res = Math.random();
+        if (res < 0.5) System.out.println("Head");
+        else System.out.println("Tails");
+    }
+}
--- Week02/ex/Digit.java
+++ Week02/ex/Digit.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Digits {
+public class Digit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         var number = scan.nextInt();
