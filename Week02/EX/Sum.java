fa817c63b07c5b5cda0336ab3353042a9d6aa432
Madina Alili 15406 <malili15406@ada.edu.az>
2023-02-05 19:54:53 +0400

    init

---
+++ Week02/EX/Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Sum {
+public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+         System.out.print("Input numbers: ");
+ 	    	double number = sc.nextInt();
+ 	    	double number1 = sc.nextInt();
+         System.out.println(number+number1);
+    }
+ }
+
+
+
+
