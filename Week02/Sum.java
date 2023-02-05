6d3157691e364517586b6a43273245cc71b09955
Nazrin Muradova 16062 <nnmuradova16062@ada.edu.az>
2023-02-05 19:56:23 +0400

    Add new exercise

---
+++ Week02/Sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Sum{
+    public static void main (String args[]) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int n1 = scan.nextInt();
+        var n2 = scan.nextInt();
+
+        var sum = n1 + n2;
+        System.out.println(sum);
+
+    }
+}
