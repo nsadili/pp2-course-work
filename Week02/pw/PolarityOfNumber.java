595b7d5f2cdf2dcd37d58c00b17778ddfac1797b
Muaataz_A_Ismaeel <mismaeel17637@ada.edu.az>
2023-02-05 20:13:50 +0400

    Nami

---
+++ Week02/pw/PolarityOfNumber.java
@@ -0,0 +1,13 @@
+public class PolarityOfNumber {
+    public static void main(String[] args) {
+      double num = 3.14;
+      if (num > 0) {
+        System.out.println("positive");
+      } else if (num < 0) {
+        System.out.println("negative");
+      } else {
+        System.out.println("neutral");
+      }
+    }
+  }
+  
