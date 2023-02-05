417e50f005732f7a6c6fcc7e3d93c13121fde89e
Madina Alili 15406 <malili15406@ada.edu.az>
2023-02-05 22:07:08 +0400

    MonthInfo

--- Week02/EX/MonthInfo.java
+++ Week02/EX/MonthInfo.java
@@ -1,33 +1,53 @@
-import java.util.Scanner;
+import java.util.*;
 public class MonthInfo {
     public static void main(String[] args) {
-        Scanner sc=new.Scanner();
+        Scanner sc=new Scanner(System.in);
         System.out.print("Enter month: ");
          int month=sc.nextInt();
         switch(month){
             case 1:
                 System.out.println("Jan - 31");
+                break;
+
             case 2:
                 System.out.println("Feb - 28");
+                break;
+
             case 3:
                 System.out.println("Mar - 31");
+                break;
+
             case 4:
                 System.out.println("Apr - 30");
+                break;
+
             case 5:
-                 System.out.println("Jul - 31");
+                 System.out.println("May - 31");
+                 break;
+
             case 6:
-                System.out.println("Aug ");
+                 System.out.println("Jun - 30");
+                 break;
+
             case 7:
+                 System.out.println("Jul - 31");
+                 break;
 
             case 8:
-            
+                 System.out.println("Aug - 31 ");
+                 break;
             case 9:
-
+                 System.out.println("Sep - 30 ");
+                 break;
             case 10:
-
+                 System.out.println("Oct - 31 ");
+                 break;
             case 11:
-
+                 System.out.println("Nov - 30 ");
+                 break;
             case 12:
+                 System.out.println("Dec - 31 ");
+                 break;
 
         }
     }
