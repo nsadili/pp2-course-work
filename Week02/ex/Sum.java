4a2067cf978141dd639601ad655dc3c6e2f069c4
Muaataz_A_Ismaeel <mismaeel17637@ada.edu.az>
2023-02-05 17:24:32 +0400

    SUIII

---
+++ Week02/ex/Sum.java
@@ -0,0 +1,22 @@
+package ex;
+
+import java.util.Scanner;
+
+public class Sum {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the first number: ");
+
+    int num1 = sc.nextInt();
+
+    System.out.print("Enter the second number: ");
+
+    int num2 = sc.nextInt();
+    
+    int sum = num1 + num2;
+
+    System.out.println("The sum5 of " + num1 + " and " + num2 + " is: " + sum);
+  }
+}
