3dc7f6a5e3a87272019186228000187020363841
Muaataz_A_Ismaeel <mismaeel17637@ada.edu.az>
2023-02-05 18:58:43 +0400

    Hehe

---
+++ Week02/ex/Calc.java
@@ -0,0 +1,16 @@
+
+public class Calc {
+    public static void main(String [] args){
+
+        if (args.length < 2){
+            System.out.println("please, enter two integers.");
+            System.out.println(1);
+        }
+
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+    }
+
+} 
