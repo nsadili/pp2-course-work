fcfddf3b4f932e6ace76c1367281cb59757b6001
Ibrahim Ashrafov 17505 <iashrafov17505@ada.edu.az>
2023-02-05 18:20:49 +0400

    Easy task

---
+++ Week02/Time.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Time {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int Seconds = input.nextInt();
+
+        int hour = Seconds / 3600;
+        int minute = Seconds % 3600 / 60;
+        int second = Seconds % 60;
+
+        System.out.printf("%02d:%02d:%02d", hour, minute, second);
+    }
+}
