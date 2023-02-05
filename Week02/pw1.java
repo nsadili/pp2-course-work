e58b66140c9e8f2a5111d122fe303a73d1b7a37f
ElnurIsmayilzade <118634806+elnur1616@users.noreply.github.com>
2023-02-05 21:52:14 +0400

    Create pw1.java

---
+++ Week02/pw1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class pw1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+
+        for (int i = a; i <= b; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
