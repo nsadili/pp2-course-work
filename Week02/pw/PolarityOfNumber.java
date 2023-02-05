947204e6315448cbc09952868d42971e7925a872
asger_huseynli <ahuseynli17683@ada.edu.az>
2023-02-05 20:17:44 +0400

    Asgar

---
+++ Week02/pw/PolarityOfNumber.java
@@ -0,0 +1,14 @@
+public class PolarityOfNumber {
+    public class main {
+        double x = 2.5;
+        if (x > 0) {
+            System.out.println("positive");
+        }
+        else if (x < 0) {
+            System.out.println("negative");
+        }
+        else {
+            System.out.println("neutral");
+        }
+    }
+}
