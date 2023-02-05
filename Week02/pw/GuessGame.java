e48775ba856ba0137177f751bc0359ce59c251be
Shamil Abbasov 16084 <sabbasov16084@ada.edu.az>
2023-02-05 17:29:13 +0400

    final version of Guess game

--- Week02/pw/GuessGame.java
+++ Week02/pw/GuessGame.java
@@ -3,29 +3,38 @@ import java.util.Scanner;
 public class GuessGame {
     public static void main(String[] args) {
         int a;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("enter the upper bound -> ");
-        a=sc.nextInt();
+        a = sc.nextInt();
 
-        int num=(int)(Math.random()*(a+1));  
-        
-        var guess=0;
+        int num = (int) (Math.random() * (a + 1));
 
-        do{
-            System.out.print("Please enter your guess -> ");
-            guess=sc.nextInt();
+        var guess = 0;
+        var score = 0;
 
-            if(guess>num){
+        System.out.print("Please enter your guess -> ");
+        while (sc.hasNextInt()) {
+          
+            guess = sc.nextInt();
+            score++;
+
+            if (guess > num) {
                 System.out.println("Higher");
-            }else if(guess<num){
+            } else if (guess < num) {
                 System.out.println("Lower");
-            }else{
-                System.out.printf("Game Over!\nNumber is %d",num);
+            } else {
+                System.out.printf("Game Over!\nNumber is %d\n", num);
+                System.out.println("Your Score: " + score);
+                break;
             }
+            
+            
+            System.out.print("Please enter your guess -> ");
 
-
-        }while(guess!=num );
+        }
+        System.out.println("Game Over");
+        System.out.println("Number is: "+num);
 
     }
-    
+
 }
