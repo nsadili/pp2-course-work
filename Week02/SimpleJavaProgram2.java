99409a4cb9ec08421abfedfac1c686fa42c80d43
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-02-06 11:01:51 +0400

    Add files via upload

---
+++ Week02/SimpleJavaProgram2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SimpleJavaProgram2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num1 = input.nextInt();
+        System.out.println("Enter a number: ");
+        int num2 = input.nextInt();
+        System.out.println("Enter a number: ");
+        int num3 = input.nextInt();
+        int sum = num1 + num2 + num3;
+        System.out.println("Sum is: " + sum);
+    }
+}
+
