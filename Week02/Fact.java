12b0fc7efce72f2b4f60b11687f611773e8e2319
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-02-06 10:14:52 +0400

    Add files via upload

---
+++ Week02/Fact.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fact { 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial " + num + " is: " + factorial);
+
+        
+     
+    }
+}
+
+
