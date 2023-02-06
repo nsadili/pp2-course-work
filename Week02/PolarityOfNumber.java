126aff5d8fd8cc6583f0f42b7fb774c5d01eae1a
Taghi Mammadov 17503 <tmammadov17503@ada.edu.az>
2023-02-06 22:28:44 +0400

    New exercise

---
+++ Week02/PolarityOfNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PolarityOfNumber {
+    public static void main(String[] args){
+         try (Scanner sc = new Scanner(System.in)) {
+            //System.out.println("Input the first integer: ");
+            double number = sc.nextDouble();
+    
+       if ( number == 0 ){
+        System.out.println("Answer: neutral");
+       }
+       if ( number < 0 ){
+        System.out.println("Answer: negative");
+       }
+       if ( number > 0 ){
+        System.out.println("Answer: positive");
+       }
+        }
+}
+}
