357cb67940577529b80d3c6db1c13212e70e1488
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-02-06 10:10:13 +0400

    Add files via upload

---
+++ Week02/LeapYear.java
@@ -0,0 +1,17 @@
+// Given a year. Write a program to decide whether it is a leap year or not
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a year: ");
+        int year = input.nextInt();
+        if (year % 4 == 0) {
+            System.out.println("Leap year");
+        } else {
+            System.out.println("Not a leap year");
+        }
+    }
+}
+
