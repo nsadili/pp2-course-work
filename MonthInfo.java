4061d9263838be7687018c4ccbff94bd4f037e15
jtaghiyev18172 <119033156+jtaghiyev18172@users.noreply.github.com>
2023-02-06 16:16:52 +0400

    Add files via upload

---
+++ MonthInfo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MonthInfo {
+    public static void main(String[] args) {
+       
+        Scanner scan = new Scanner(System.in);
+
+        var monthNo = scan.nextInt();
+
+        switch(monthNo){
+            default:
+            System.out.println("Please, enter a month no [1-12]");
+            break;
+            case 1:
+            System.out.println("Jan - 31");
+            break;
+            case 2:
+            System.out.println("Feb - 28");
+            break;
+            case 3:
+            System.out.println("Mar - 31");
+            break;
+            case 4:
+            System.out.println("Apr - 30");
+            break;
+        }
+    
+    }
+ }
