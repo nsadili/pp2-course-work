8e40ebe1f713363c25ee87eeeb27819ca88ec2db
Seljan Mamedova <118668057+SeljanMamedova@users.noreply.github.com>
2023-02-06 11:42:36 +0400

    first push

---
+++ Week02/WriteSomeCodes.java
@@ -0,0 +1,29 @@
+
+package writesomecodes;
+
+
+public class WriteSomeCodes {
+
+    
+    public static void main(String[] args) {
+         int a=8%3;
+	    double b=8/3;
+	    double c=8.0/3.0;
+	    double d=8.0%3.0;
+	    
+	        int e=7;
+	    int z=e+3;
+	    int res=z; 
+	    
+	    
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+		System.out.println("d="+d);
+           
+
+ 		System.out.println(""+res);
+                System.out.println(" "+res);
+    }
+    
+}
