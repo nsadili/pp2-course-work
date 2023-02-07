0c399cc4860e6e4fced42ea5b872a32ce7e763d1
Madina Alili 15406 <malili15406@ada.edu.az>
2023-02-07 21:44:41 +0400

    Fact

--- Week02/EX/Fact.java
+++ Week02/EX/Fact.java
@@ -6,12 +6,18 @@ public class Fact {
         System.out.println("Please enter number: ");
         int num=sc.nextInt();
         int fact=1;
+        if(num>0){
         while (num>=1) {
             fact *= num;
             num--;
         }
-        System.out.println("Factorial of this number is "+fact);
-        
+        System.out.println("Factorial of this number is "+fact); }
+        else if(num==0){
+            System.out.println("Factorial of this number is 1");
+        }
+        else{
+            System.out.println("Error!");
+        }
     }
 }
 //[Fact.java] Write a program that takes an integer using command-line arguments and find
