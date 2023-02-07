5ef6d2632272c31f7cf1fedfece911c476d179fe
Leyla_Khudiyeva <lkhudiyeva17248@ada.edu.az>
2023-02-07 23:48:55 +0400

    salam

---
+++ Week02/Average.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int count =0;
+        var sum=0;
+        while(scan.hasNextInt())
+        {
+            sum+=scan.nextInt();
+            count++;
+         }
+        // avg= (double)sum/count;
+        System.out.println((double)sum/count);
+
+
+
+    }
+}
