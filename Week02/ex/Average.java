504894b69ba06b6657e13243b13a6e5427dcf2ed
Rufiz Bayramov 16980 <rbayramov16980@ada.edu.az>
2023-02-07 08:55:03 +0300

    update solutions

---
+++ Week02/ex/Average.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Average {
+    public static void main(String[] args){
+        
+        try (Scanner in = new Scanner(System.in)) {
+        
+            System.out.print("Input number(s): ");
+              int n = in.nextInt();
+
+              if (n < 1){
+                System.out.println("error");
+                System.exit(1);
+            }
+
+        int[] nums = {n};
+         
+        float sum = 0;
+         
+        int i=0;
+        while(i < nums.length) {
+            sum += nums[i];
+            i++;
+        }
+         
+        float average = (sum / nums.length); 
+         
+        System.out.println("Average : "+average);
+
+    }
+}
+}
