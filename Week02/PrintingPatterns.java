70797a4539cd4e815cca9a07f79c57f53775d2cc
Turkana <113385549+turkanaaa@users.noreply.github.com>
2023-02-08 17:18:09 +0400

    PrintingPatterns.java
    Print the following pattern given the size: (the following example size = 5).
    *#*#* 
    #*#*# 
    *#*#* 
    #*#*#

---
+++ Week02/PrintingPatterns.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrintingPatterns {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int size = scan.nextInt();
+
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if ((i + j) % 2 == 0) {
+          System.out.print("*");
+        } else {
+          System.out.print("#");
+        }
+      }
+      
+      System.out.println();
+    }
+
+  }
+}
