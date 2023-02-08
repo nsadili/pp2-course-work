338148d90e40e96c5733d2a10f36158dbe27ed33
lamanasgarli <lasgarli17569@ada.edu.az>
2023-02-08 15:55:28 +0400

    adding code

---
+++ Week03/sum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner ;
+public class sum {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in) ;
+    var v=scan.nextInt();
+    var l=scan.nextInt() ;
+    System.out.println(v+l);
+
+}
+}
