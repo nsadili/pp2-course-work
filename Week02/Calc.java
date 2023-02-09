f789e1dbb27dadf679556010370b24fa6996a882
Suel Heybatova 13641 <123533417+suelheybatova1@users.noreply.github.com>
2023-02-09 22:36:44 +0400

    New ex.added

--- Week02/Calc.java
+++ Week02/Calc.java
@@ -4,13 +4,16 @@ public class Calc {
         public static void main(String[] args) {
             int num1 = Integer.parseInt(args[0]);
             int num2 = Integer.parseInt(args[1]);
-            System.out.println("The sum is: " + (num1 + num2));
-            System.out.println("The subtraction is: " + (num1 - num2));
-            System.out.println("The multiplication is: " + (num1 * num2));
-            System.out.println("The integer division number1 over number2 is: " + (num1 / num2));
-            System.out.println("The integer division number2 over number1 is: " + (num2 / num1));
-            System.out.println("The integer remainder of number2 over number1 is: " + (num2 % num1));
-            System.out.println("The integer remainder of number1 over number2 is: " + (num1 % num2));
+            int sum = num1 + num2;
+            int difference = num1 - num2;
+            int product = num1 * num2;
+            int quotient = num1 / num2;
+            int remainder = num1 % num2;
+            System.out.println("Sum: " + sum);
+            System.out.println("Difference: " + difference);
+            System.out.println("Product: " + product);
+            System.out.println("Quotient: " + quotient);
+            System.out.println("Remainder: " + remainder);
         }
     }
 
