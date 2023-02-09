b35177fff36f2476d81edd9a03ae9afa997f820e
Suel Heybatova 13641 <123533417+suelheybatova1@users.noreply.github.com>
2023-02-09 22:18:12 +0400

    New ex. added

--- Week02/Main.java
+++
@@ -1,7 +0,0 @@
-package Week02;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println();
-    }
-}
---
+++ Week02/Sum.java
@@ -0,0 +1,14 @@
+package Week02;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please add First number: ");
+        int num1 = sc.nextInt();
+        System.out.println("please add Second number: ");
+        int num2 = sc.nextInt();
+        System.out.println("the answer  is: " + (num1 + num2));
+    }
+}
