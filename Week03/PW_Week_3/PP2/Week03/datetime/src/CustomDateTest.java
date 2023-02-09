67c08e421c7c8c37311549552e132e21b5daedeb
Murad Hajiyev <mhaciyev17623@ada.edu.az>
2023-02-09 23:17:36 +0400

    Week 3 practical work CustomDate exercise added

---
+++ Week03/PW_Week_3/PP2/Week03/datetime/src/CustomDateTest.java
@@ -0,0 +1,16 @@
+public class CustomDateTest {
+    public static void main(String[] args) {
+
+        CustomDate date1 = new CustomDate(2,24,2023);
+        CustomDate date2 = new CustomDate(2,28,2023);
+
+
+        date1.displayFormatted();
+        date1.displayDate();
+        System.out.println(date1.difference(date2));
+        System.out.println(CustomDate.compare(date1, date2));
+
+    }
+
+}
+
