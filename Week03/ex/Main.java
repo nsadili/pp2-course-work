c8a2f50d866c537157cfd4069fe6c0dc37992f7f
Nasib Pashayev 16076 <npashayev16076@ada.edu.az>
2023-02-09 11:05:07 -0800

    Add a week03.

---
+++ Week03/ex/Main.java
@@ -0,0 +1,34 @@
+import pp2.week03.geometry.Point;
+import pp2.week03.geometry.Segment;
+public class Main {
+
+    public static void main(String[] args) {
+
+       Point p1 = new Point();
+
+       Point p2 = new Point(4, 5);
+
+       p1.translate(2, 3);
+
+       System.out.println(p1.getX() + ", " + p1.getY());
+       System.out.println(p2.getX() + ", " + p2.getY());
+       
+       System.out.println(p1.distance(p2));
+       System.out.println(p2.distance(p1));
+       
+       Segment s1 = new Segment(p1, p2);
+       Segment s2 = new Segment( 0, 0, 3, 4);
+       System.out.printf(
+        "p1: (%.2f, %.2f) and p2: (%.2f, %.2f)\n",
+        s2.getP1().getX(), s2.getP1().getY(),
+        s2.getP2().getX(), s2.getP2().getY());
+
+        s2.translate(1, 1);
+
+        System.out.printf(
+            "p1: (%.2f, %.2f) and p2: (%.2f, %.2f)\n",
+            s2.getP1().getX(), s2.getP1().getY(),
+            s2.getP2().getX(), s2.getP2().getY());
+   }
+    
+}
---
+++ Week03/ex/pp2/week03/geometry/Point.java
@@ -0,0 +1,41 @@
+package pp2.week03.geometry;
+
+public class Point {
+    
+        private float x;
+        private float y;
+
+        public Point(){ 
+           
+        
+        }
+   
+    public Point(float x, float y){
+
+        this.x = x;
+        this.y = y;
+
+    }
+    public float getX(){
+        return this.x;
+    }
+    public float getY(){
+        return this.y;
+    }
+    public void translate(float dX, float dY){
+        this.x += dX;
+        this.y += dY;
+    }
+    public float distance(Point p){
+     float dist =(float) Math.sqrt(  
+        Math.pow(this.x - p.x, 2)
+             + Math.pow(this.y - p.y, 2));
+        
+         return dist;
+    }
+public float length() {
+        return p1.distance(p2);
+
+    }
+
+}
