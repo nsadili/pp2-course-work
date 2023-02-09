5ad305d13911be77bd7a64c6d39f44aa2443020e
lamanasgarli <lasgarli17569@ada.edu.az>
2023-02-09 13:06:41 +0400

    add

---
+++ Week02/calc.java
@@ -0,0 +1,18 @@
+
+public class calc {
+    public static void main(String[] args) {
+    
+    int num1 = Integer.parseInt(args[0]) ;
+    int num2 = Integer.parseInt(args[1]) ;
+      int sum = num1+num2  ;
+         int difference = num1-num2  ;
+           int division = num1/num2  ;
+             int remainder = num1%num2  ;
+                int product = num1*num2  ;
+    System.out.println(sum) ;
+    System.out.println(difference) ;
+     System.out.println(division) ;
+      System.out.println(product) ;
+       System.out.println(remainder) ;
+}
+} 
