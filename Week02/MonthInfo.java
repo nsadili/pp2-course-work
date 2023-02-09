24d4a776c41b334f925c814887336d33a58876ef
Mammad Pashazade <mpashazade17469@ada.edu.az>
2023-02-09 10:47:23 +0400

    .

--- Week02/MonthInfo.java
+++ Week02/MonthInfo.java
@@ -1,8 +1,56 @@
-import.java.util.Scanner;
+import java.util.Scanner;
 public class MonthInfo {
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
-        
+        int a = sc.nextInt();
+        if (a==1) 
+        {
+            System.out.println("January");
+        }
+        if (a==2) 
+        {
+            System.out.println("February");
+        }
+        if (a==3) 
+        {
+            System.out.println("March");
+        }
+        if (a==4) 
+        {
+            System.out.println("April");
+        }
+        if (a==5) 
+        {
+            System.out.println("May");
+        }
+        if (a==6) 
+        {
+            System.out.println("June");
+        }
+        if (a==7) 
+        {
+            System.out.println("July");
+        }
+        if (a==8) 
+        {
+            System.out.println("August");
+        }
+        if (a==9) 
+        {
+            System.out.println("September");
+        }
+        if (a==10) 
+        {
+            System.out.println("October");
+        }
+        if (a==11) 
+        {
+            System.out.println("November");
+        }
+        if (a==12) 
+        {
+            System.out.println("December");
+        }
     }
 }
