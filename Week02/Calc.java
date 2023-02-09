91c7509e93697edc164dfcb25eb2f2821923dce0
Suel Heybatova 13641 <123533417+suelheybatova1@users.noreply.github.com>
2023-02-09 23:01:45 +0400

    New ex. added

--- Week02/Calc.java
+++ Week02/Calc.java
@@ -2,19 +2,21 @@ package Week02;
 
 public class Calc {
         public static void main(String[] args) {
-            int num1 = Integer.parseInt(args[0]);
-            int num2 = Integer.parseInt(args[1]);
-            int sum = num1 + num2;
-            int difference = num1 - num2;
-            int product = num1 * num2;
-            int quotient = num1 / num2;
-            int remainder = num1 % num2;
-            System.out.println("Sum: " + sum);
-            System.out.println("Difference: " + difference);
-            System.out.println("Product: " + product);
-            System.out.println("Quotient: " + quotient);
-            System.out.println("Remainder: " + remainder);
+
+            if (args.length < 2) {
+                System.out.println("Please provide some input");
+                System.exit(1);
+            }
+
+            int n1 = Integer.parseInt(args[0]);
+            int n2 = Integer.parseInt(args[1]);
+
+            System.out.println("Addition is " + (n1+n2));
+            System.out.println("Subtraction is " + (n1-n2));
+            System.out.println("Multiplication is " + (n1*n2));
+            System.out.println("Division is " + (n1/n2));
+            System.out.println("Remainder of division is " + (n1%n2));
+
+
         }
     }
-
-
--- Week02/Sum.java
+++ Week02/Sum.java
@@ -1,7 +1,6 @@
 package Week02;
 
 import java.util.Scanner;
-
 public class Sum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
