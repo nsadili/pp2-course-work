020d789995b71f786da3f42daf4ec7145d106088
Shamil Abbasov 16084 <sabbasov16084@ada.edu.az>
2023-02-10 18:04:37 +0400

    first version of Invoice ex

---
+++ Week03/ex/InvoiceTest.java
@@ -0,0 +1,14 @@
+
+import pp2.week3.ex2.Invoice;
+
+public class InvoiceTest {
+    public static void main(String[] args) {
+
+            Invoice i=new Invoice("ALfa","Beta",10,5);
+
+        System.out.println(i.getInvoiceAmount());
+        
+
+    
+    }
+}
---
+++ Week03/ex/pp2/week3/ex2/Invoice.java
@@ -0,0 +1,77 @@
+package pp2.week3.ex2;
+
+public class Invoice {
+
+    private String partNumber;
+
+    private String partDes;
+
+    private int quantity;
+
+    private double price;
+
+    public Invoice() {
+        // this.partDes = "Null";
+        // this.partNumber = "Null";
+        // this.quantity = 0;
+        // this.price = 0.0;
+        this("Null" , "Null", 0, 0.0);
+
+    }
+
+    public Invoice(String partDes, String partNumber, int quantity,double price){
+        this.partDes = partDes;
+        this.partNumber = partNumber;
+        this.quantity = quantity;
+        this.price = price;
+
+        if(quantity<0) this.quantity=0;
+        if(price<0) this.price=0.0;
+
+
+    }
+
+    public double getInvoiceAmount(){
+        return quantity*price;
+    }
+
+    public void setPartNumber(String partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    public void setPartDes(String partDes) {
+        this.partDes = partDes;
+    }
+
+    public void setQuantity(int quantity) {
+        if(quantity>0){
+        this.quantity = quantity;
+        }
+    }
+
+    public void setPrice(double  price) {
+        if(price>0){
+          this.price = price;
+    }
+    }
+
+    public String getPartNumber() {
+        return partNumber;
+    }
+
+    public String getPartDes() {
+        return partDes;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    
+
+    
+
+}
