45c2191861ccb058bdc69d41d67bcce306a1f86a
“Abov <aakhundov17192@ada.edu.az>
2023-02-10 16:21:39 +0400

    d

---
+++ Week02/PW/week 01&02/PolarityOfNumber.java
@@ -0,0 +1,13 @@
+public class PolarityOfNumber {
+    public static main void(String[]args);
+    var a =Integer.valueof(args[0]);
+    if (a<0){
+        System.out.println("negative");
+        else if(a==0){
+            System.out.println("neutral");
+            else{
+                System.out.println("positive");
+            }
+        }
+    }
+}
