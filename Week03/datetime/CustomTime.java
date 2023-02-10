67a4f1a10ac2d9715d38a6e7a80354f5067e3f91
zradj <zradj7@gmail.com>
2023-02-10 11:08:40 +0400

    upload solution to CustomTime exercise in Week 03

---
+++ Week03/datetime/CustomTime.java
@@ -0,0 +1,60 @@
+package Week03.datetime;
+
+public class CustomTime {
+    private int hour, minute, second;
+
+    public CustomTime(int hour, int minute, int second) {
+        this.hour = hour + minute / 60 + (minute % 60 + second / 60) / 60;
+        this.minute = (minute % 60 + second / 60) % 60;
+        this.second = second / 60;
+    }
+
+    public CustomTime() {
+        this(0, 0, 0);
+    }
+
+    public CustomTime(int hour) {
+        this(hour, 0, 0);
+    }
+
+    public CustomTime(int hour, int minute) {
+        this(hour, minute, 0);
+    }
+
+    public CustomTime(CustomTime time) {
+        this(time.getHour(), time.getMinute(), time.getSecond());
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public String toUniversalString() {
+        return String.format("%02d:%02d:%02d", this.hour, this.minute, this.second);
+    }
+
+    public String toStandardString() {
+        int hour = this.hour % 12;
+        return String.format("%02d:%02d:%02d %s", hour == 0 ? 12 : hour, this.minute, this.second, (this.hour % 24) / 12 == 0 ? "AM" : "PM");
+    }
+}
---
+++ Week03/datetime/CustomTimeTest.java
@@ -0,0 +1,16 @@
+package Week03.datetime;
+
+public class CustomTimeTest {
+    public static void main(String[] args) {
+        CustomTime time1 = new CustomTime();
+        CustomTime time2 = new CustomTime(12, 1, 9);
+        CustomTime time3 = new CustomTime(time2);
+        CustomTime time4 = new CustomTime(11);
+        CustomTime time5 = new CustomTime(100, 7);
+        System.out.println(time1.toStandardString());
+        System.out.println(time2.toStandardString());
+        System.out.println(time5.toStandardString());
+        System.out.println(time3.toUniversalString());
+        System.out.println(time4.toUniversalString());
+    }
+}
