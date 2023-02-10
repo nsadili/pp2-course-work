f5261eb2ea61395afa54e1b3d6bba0424e59afcc
Behruz Gurbanli <bgurbanli14129@ada.edu.az>
2023-02-10 08:57:45 +0400

    Exercise added.

---
+++ Week03/geometry/Point.java
@@ -0,0 +1,43 @@
+package Week03.geometry;
+
+public class Point {
+    private float x;
+    private float y;
+
+    public Point() {
+    }
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) {
+        this(p.x, p.y);
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void translate(float dX, float dY) {
+        this.x += dX;
+        this.y += dY;
+    }
+
+    public boolean equals(Point p) {
+        return this.x == p.x && this.y == p.y;
+    }
+}
