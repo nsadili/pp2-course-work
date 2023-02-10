c1366c8fd9b626c692042e72c777493d1ea572f0
Rahima_Karimova <rkarimova17347@ada.edu.az>
2023-02-10 15:54:35 +0400

    adding

--- Week03/EX/pp2/week03/ex02/InvoiceTest.java
+++ Week03/EX/pp2/week03/ex02/InvoiceTest.java
@@ -5,8 +5,8 @@ public class InvoiceTest {
     Invoice p = new Invoice("five","new items",10,2.5);
     p.setDesc("Buy our products!");
 
-System.out.printf("Number: %s\n,Description: %s\nQuantity: %d\nPrice: %.2f\n",p.getNumber(),p.getDesc(),p.getQuantity(),p.getPrice());
-System.out.printf("Invoice amount: %f",p.getInvoiceAmount());
+System.out.printf("Number: %s\nDescription: %s\nQuantity: %d\nPrice: %.2f\n",p.getNumber(),p.getDesc(),p.getQuantity(),p.getPrice());
+System.out.printf("Invoice amount: %.3f",p.getInvoiceAmount());
 
     }
 }
