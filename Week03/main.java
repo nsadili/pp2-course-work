c0654558650753931dfeb6ac8908c585288bd596
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-02-12 00:06:04 +0400

    Add files via upload

---
+++ Week03/main.java
@@ -0,0 +1,19 @@
+import pp2.week03.Point;
+import pp2.week03.Segment;
+
+public class Main {
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        Point p2 = new Point(4, 5);
+        Segment s1 = new Segment(p1, p2);
+        Segment s2 = new Segment(0, 0, 3, 4);
+        System.out.printf("p1: (%.2f, %.2f) and p2: (%.2f, %.2f)\n",
+            s2.getP1().getX(), s2.getP1().getY(),
+            s2.getP2().getX(), s2.getP2().getY());
+
+        s2.translate(1, 1);
+        System.out.printf("p1: (%.2f, %.2f) and p2: (%.2f, %.2f)\n",
+            s2.getP1().getX(), s2.getP1().getY(),
+            s2.getP2().getX(), s2.getP2().getY());
+    }
+}
