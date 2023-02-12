9b444c58cb81f8e385bdfd191b7e905e50bcd904
Taghi Mammadov 17503 <tmammadov17503@ada.edu.az>
2023-02-12 14:23:26 +0400

    New esercise - Prime.java

---
+++ Week02/Primes.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Primes {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Input the number: ");
+            int number = sc.nextInt();
+            int count = 0;
+            if (number==0 || number==1) 
+            System.out.println("The number is not prime");
+            else {
+                for (int i=2; i<=number/ 2; i++) {
+                    if (number %i == 0) {
+                        count++;
+                        break;
+                    }
+                }
+
+                if (count==1) 
+                System.out.println("Not prime number");
+                else 
+                System.out.println("Prime number");
+            }
+        }
+    }
+}
--- Week02/RandomPointsInCircle.java
+++ Week02/RandomPointsInCircle.java
@@ -9,27 +9,27 @@ public class RandomPointsInCircle {
 
         int n;
         double x,y;
-        while(n-- > 0){
-
-        
-
-        for (int i = 0; i < n;){
-        do{
-             x = 2 * Math.random() -1;
-             y = 2 * Math.random() -1;
-             System.out.printf("\tTry: (%f, %f)\n" ,x,y);
-
+        while(n-- > 0);{
+            for (int i = 0; i < n;){
+                do
+                     x = 2 * Math.random() -1;
+                     y = 2 * Math.random() -1;
+                     System.out.printf("\tTry: (%f, %f)\n" ,x,y);
+                
+                }
         }
-    }
-    } while(x *x + y*y > 1.0);
-        
     
-
+    while(x *x + y*y > 1.0);{
         if(x*x + y*y <= 1.0){
             System.out.printf("(%f, %f) is in the circle.\n", x,y);
         } else {
             System.out.printf("(%f, %f) is outside the circle.\n", x,y);
         } 
+    }
+        
+    
+
+       
 
             
     }
