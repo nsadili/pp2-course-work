ddc9a0b7edb58b1b00854fe6964e201e30f86836
Seljan Mamedova <118668057+SeljanMamedova@users.noreply.github.com>
2023-02-12 16:08:59 +0400

    wk3 exs

--- Week02/Main.java
+++ Week02/Main.java
@@ -1,14 +1,17 @@
-package pp2.week3.geometry;
+package Week03.geometry;
+
+import javax.sound.midi.Soundbank;
 
 public class Main {
-    public static void main(String[]args){
-        Point p1= new Point(4,6);
-        Point p2= new Point (7,9);
+    public static void main(String[] args) {
+        Point p1 = new Point(4, 6);
+        Point p2 = new Point(7, 9);
 
-    p1.translate(3,9);
+        p1.translate(3, 9);
+
+        Segment s1 = new Segment(p1, p2);
+        System.out.println(s1.length());
 
 
-    Segment s1= new Segment(p1,p2);
-    System.out.println(s1.length());
-}
+    }
 }
--- Week02/Point.java
+++ Week02/Point.java
@@ -1,60 +1,55 @@
-package pp2.week3.geometry;
-
+package Week03.geometry;
 
 public class Point {
-     private float x;
+    private float x;
     private float y;
-    public Point(){
+
+    public Point () {
 
     }
-
-public Point (float x,float y)
+    public Point(float x, float y)
     {
-        this.x=x;
-        this.y=y;
-
+        this.x = x;
+        this.y = y;
     }
     public Point(Point p)
-
-
     {
-        this.x=p.x;
-        this.y=p.y;
+        this.x = p.x;
+        this.y = p.y;
+    }
 
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void translate(float dX, float dY)
+    {
+        this.x += dX;
+        this.y += dY;
+    }
+
+    public float distance(Point p)
+    {
+        float dist = (float) Math.sqrt( Math.pow( (p.x - this.x), 2) + Math.pow((p.y - this.y), 2));
+        return dist;
 
     }
 
+    public boolean equals(Point p)
+    {
+        return p.x == this.x && this.y == p.y;
 
-public float getX(){
-    return x;
+    }
 }
-public float getY(){
-    return y;
-}
-public void setX(float x){
-    this.x=x;
-}
-
-public void setY(float y){
-    this.y=y;
-}
-
-
-public void translate (float dx,float dy){
-    this.x+=dx;
-    this.y+=dy;
-}
-
-public float distance(Point p){
-
-float dist=(float) Math.sqrt(Math.pow((p.x-this.x),2) +Math.pow((p.y-this.y),2));
-return dist;
-}
-
-public boolean equals(Point p)
-{
-    return p.x==this.x && this.y == p.y;
-}
-
-}
-
