d068ebd8aeb01cae5ac8967eb8ca44efcc85979a
ravanabdinli <118709878+ravanabdinli@users.noreply.github.com>
2023-02-12 23:37:40 +0400

    Week 3 exercises

---
+++ Week03/Invoice.java
@@ -0,0 +1,44 @@
+public Bill() {}
+
+public Bill(String itemCode, String itemDescription, int itemCount, double itemPrice) {
+    this.itemCode = itemCode;
+    this.itemDescription = itemDescription;
+    this.itemCount = itemCount;
+    this.itemPrice = itemPrice;
+}
+
+public String getItemCode() {
+    return itemCode;
+}
+
+public void setItemCode(String itemCode) {
+    this.itemCode = itemCode;
+}
+
+public String getItemDescription() {
+    return itemDescription;
+}
+
+public void setItemDescription(String itemDescription) {
+    this.itemDescription = itemDescription;
+}
+
+public int getItemCount() {
+    return this.itemCount;
+}
+
+public void setItemCount(int itemCount) {
+    this.itemCount = Math.max(0, itemCount);
+}
+
+public double getItemPrice() {
+    return itemPrice;
+}
+
+public void setItemPrice(double itemPrice) {
+    this.itemPrice = Math.max(0.0, itemPrice);
+}
+
+public double getTotal() {
+    return this.itemCount * this.itemPrice;
+}
