278cb906120d861b47eea46f02a8306323dccf20
Ravan Mehraliyev 16226 <rmehraliyev16226@ada.edu.az>
2023-02-12 23:36:23 +0400

    Custom Date is done !

---
+++ Week03/pw/datetime/CustomDate.java
@@ -0,0 +1,68 @@
+public class CustomDate {
+
+    private int month;
+    private int day;
+    private int year;
+
+    public CustomDate(int month, int day, int yaer) {
+
+        this.month = month;
+        this.day = day;
+        this.year = yaer;
+
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void displayDate() {
+        System.out.println(month + "/" + day + "/" + year);
+    }
+
+    public int difference(CustomDate date) {
+
+        int conversion = (date.year - year) * 365 + (date.month - month) * 31 + (date.day - day);
+        return Math.abs(conversion);
+
+    }
+
+    public static int compare(CustomDate date1, CustomDate date2) {
+
+        if ((date1.year == date2.year && date1.month == date2.month && date1.day < date2.day)
+                || date1.year == date2.year && date1.month < date2.month || date1.year < date2.year)
+            return 1;
+        else if ((date1.year == date2.year && date1.month == date2.month && date1.day > date2.day)
+                || date1.year == date2.year && date1.month > date2.month || date1.year > date2.year)
+            return -1;
+        else
+            return 0;
+
+    }
+
+    public void displayFormatted() {
+        String[] monthNames = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+        System.out.println(day + " " + monthNames[month - 1] + " " + year);
+    }
+
+}
---
+++ Week03/pw/datetime/CustomDateTest.java
@@ -0,0 +1,30 @@
+public class CustomDateTest {
+    public static void main(String[] args) {
+        
+
+        CustomDate date1 = new CustomDate(1, 13, 2020);
+        CustomDate date2 = new CustomDate(5, 20, 2016);
+        System.out.println("Displaying date1 : ");
+        date1.displayDate();
+        System.out.println();
+    
+        System.out.println("Displaying date2 : ");
+        date2.displayFormatted();
+        System.out.println();
+    
+        System.out.println(" difference between date1 and date2: " + date1.difference(date2));
+        System.out.println();
+    
+        System.out.println("Comparing date1 and date2: ");
+        int comparisonResult = CustomDate.compare(date1, date2);
+        if (comparisonResult > 0) {
+          System.out.println("date1 is earlier");
+        } else if (comparisonResult < 0) {
+          System.out.println("date2 is earlier");
+        } else {
+          System.out.println("date1 and date2 are the same");
+        }
+      }
+    }
+
+ 
