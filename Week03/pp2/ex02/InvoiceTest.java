b295d71a883878ddc29bb8a7e730fc969bf8aa52
Suel Heybatova 13641 <123533417+suelheybatova1@users.noreply.github.com>
2023-02-12 18:45:45 +0400

    new ex.

--- Week03/pp2/ex02/InvoiceTest.java
+++ Week03/pp2/ex02/InvoiceTest.java
@@ -4,7 +4,7 @@ public class InvoiceTest {
     public static void main(String[] args) {
         Invoice invoice = new Invoice("556", "books", 18, 9.99);
         System.out.println("Part Number: " + invoice.getPartNumber());
-        System.out.println("Part Description: " + invoice.getPartDescription());
+        System.out.println("Part Description: " + invoice.getPartDesc());
         System.out.println("Quantity: " + invoice.getQuantity());
         System.out.println("Price Per Item: " + invoice.getPricePerItem());
         System.out.println("Invoice Amount: " + invoice.getInvoiceAmount());
