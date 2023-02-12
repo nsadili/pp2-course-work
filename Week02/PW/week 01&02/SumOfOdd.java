263edd5e49f1d04f2649113340d03583b1d6b78c
“Abov <aakhundov17192@ada.edu.az>
2023-02-13 02:07:16 +0400

    pw

---
+++ Week02/PW/week 01&02/SumOfOdd.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SumOfOdd {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        var a = Integer.parseInt(args[0]);
+        var b = Integer.parseInt(args[1]);
+
+        var sum = 0;
+
+             a++;
+        while(a < b) {
+
+            if(a % 2 != 0)
+            sum += a;
+            a++;
+                     
+        }
+
+        System.out.println(sum);
+    }
+    
+}
