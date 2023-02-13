06eb53bb7745a38426fe853f2a518d7f57d9af30
Ibrahim Ashrafov 17505 <iashrafov17505@ada.edu.az>
2023-02-13 22:41:32 +0400

    PW tasks done!

---
+++ Week03/CustomTimeTest.java
@@ -0,0 +1,9 @@
+import datetime.CustomTime;
+
+public class CustomTimeTest {
+    public static void main(String[] args) {
+        CustomTime time = new CustomTime(13, 37, 2);
+
+        System.out.println(time.String());
+    }
+}
