7789f125d3c97a902319fb4fd3a500e561c68b78
Aysel Panahova 16022 <apanahova16022@ada.edu.az>
2023-02-13 23:04:54 +0400

    ex from week3

--- Week03/pw/Point.java
+++ Week03/pw/Point.java
@@ -1,4 +1,22 @@
 
 public class Point {
 
+    public void setX(float x1) {
+    }
+
+    public float distance(Point p2) {
+        return 0;
+    }
+
+    public int getX() {
+        return 0;
+    }
+
+    public int getY() {
+        return 0;
+    }
+
+    public void setX(float x1) {
+    }
+
 }
