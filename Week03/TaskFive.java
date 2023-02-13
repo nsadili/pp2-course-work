f0253adf6271c22d98ed8e1d47fb5144d8e3fb20
Elnar16260 <118639055+Elnar42@users.noreply.github.com>
2023-02-13 23:24:03 +0400

    Create TaskFive.java

---
+++ Week03/TaskFive.java
@@ -0,0 +1,55 @@
+package pp2.week03.math;
+
+public class ComplexNumber {
+    private double x; //real
+    private double y; //imaginary
+
+    public ComplexNumber(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean equals(ComplexNumber number){
+         if((this.x == number.x) && (this.y == number.y)) {
+             return true;
+         } else{
+             return false;
+         }
+    }
+
+    public String toString(){
+        return this.x + "+" + this.y + "i";
+    }
+    public double re(){
+        return this.x;
+    }
+
+    public double imagin(){
+        return this.y;
+    }
+
+    public ComplexNumber conjug(){
+        return new ComplexNumber(this.x, -this.y);
+    }
+
+    public double abs(){
+        return Math.sqrt(this.x * this.y + this.imagin() * this.imagin());
+    }
+
+    public ComplexNumber add(ComplexNumber number){
+        return new ComplexNumber(this.x + number.x, this.y * this.x);
+    }
+
+    public ComplexNumber sub(ComplexNumber number){
+        return new ComplexNumber(this.x - number.x, this.y - number.y);
+    }
+
+    public ComplexNumber multiply(ComplexNumber number){
+        return new ComplexNumber(this.x * number.x - number.y * this.y, this.x * number.y - number.x + this.y);
+    }
+  
+}
+
+
+
+//TEST OF COMPLEX NUMBERS
