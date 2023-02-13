7927e99d802d6b8658edbfa0db2c547493ca8035
GulnarHasanli <ghasanli17578@ada.edu.az>
2023-02-13 13:54:14 +0400

    add

---
+++ Week02/Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner (System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int sum=a+b;
+
+        System.out.println(sum);
+    }
+}
