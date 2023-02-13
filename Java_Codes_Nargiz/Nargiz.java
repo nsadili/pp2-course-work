23202d674547a022f9bf8f5cc43b9cb0bea5ca94
namiraslanli12636 <119003206+namiraslanli12636@users.noreply.github.com>
2023-02-13 14:55:00 +0400

    add

---
+++ Java_Codes_Nargiz/Nargiz.java
@@ -0,0 +1,8 @@
+package Java_Codes_Nargiz;
+
+
+public class Nargiz{
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ Week03/Invoice.java
@@ -0,0 +1,8 @@
+package Week02;
+
+public class Invoice{
+
+    public static void main(String[] args) {
+        
+    }
+}
