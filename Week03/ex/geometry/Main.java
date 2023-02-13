4be0e1e39c322a0d5b883ae360951cf4c774d3a2
Naik Pashayev 13148 <naikpasha1994@gmail.com>
2023-02-13 15:32:39 +0400

    Updated my files

--- Week03/ex/geometry/Main.java
+++ Week03/ex/geometry/Main.java
@@ -3,13 +3,13 @@ package Week03.ex.geometry;
 public class Main {
     public static void main(String[] args) {
         Point p1 = new Point(3, 4);
-        Point p2 = new Point(5, 7);
+        Point p2 = new Point(4, 7);
 
         System.out.println(p1.getX());
         System.out.println(p2.getY());
         System.out.println(p1.equals(p2));
 
-        Segment s1 = new Segment(6,9);
+        Segment s1 = new Segment(6.0f,9.0f, 10.0f, 20.0f);
         System.out.println(s1.getIntercept()); 
         System.out.println(s1.getSlope());
     }
