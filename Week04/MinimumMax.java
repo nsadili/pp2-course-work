3d5da07d88df52698505109938d6fa4eb629ab1a
RamilKhaspoladov <rkhaspoladov13582@ada.edu.az>
2023-02-15 11:22:26 +0400

    ex

---
+++ Week04/MinimumMax.java
@@ -0,0 +1,20 @@
+public class MinimumMax {
+    public static void main(String[] args) {
+        int[] a = { -45, 3, 7, 112, 4, -5, 27, 33, 9, 0, 12, 0 };
+
+        int min = a[0];
+        int max = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+
+        System.out.println("Minimum value: " + min);
+        System.out.println("Maximum value: " + max);
+    }
+}
