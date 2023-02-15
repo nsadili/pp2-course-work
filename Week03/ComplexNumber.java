7baa1bfbd2be29d0ae7d5d0dd3c3d6af28103648
Rahida <rasadli16055@ada.edu.az>
2023-02-15 20:24:54 +0400

    week 3
    pw 's and average

---
+++ Week03/ComplexNumber.java
@@ -0,0 +1,70 @@
+package Week03;
+
+public class ComplexNumber {
+    private double x; //r
+    private double y; //i
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public ComplexNumber(double x, double y) {
+        setX(x);
+        setY(y);
+    }
+
+    public boolean equals(ComplexNumber number) {
+        if (this.x == number.getX() && this.y == number.getY()) return true;
+        else return false;
+    }
+
+    public String toString() {
+        return getX() + " + " + getY() + "i";
+    }
+
+    public double re() {
+        return getX();
+    }
+
+    public double imag() {
+        return getY();
+    }
+
+    public ComplexNumber conjugate() {
+        return new ComplexNumber(x, -y);
+    }
+
+    public double abs(ComplexNumber number) {
+        return Math.sqrt(number.x*number.x+number.y*number.y);
+    }
+
+    public ComplexNumber add(ComplexNumber number) {
+        double newx = this.x + number.re();
+        double newy = this.y + number.imag();
+        return new ComplexNumber(newx, newy);
+    }
+
+    public ComplexNumber sub(ComplexNumber number) {
+        double newx = this.x - number.re();
+        double newy = this.y - number.imag();
+        return new ComplexNumber(newx, newy);
+    }
+
+    public ComplexNumber mult(ComplexNumber number) {
+        double newx = this.x - number.re();
+        double newy = this.y - number.imag();
+        return new ComplexNumber(newx, newy);
+    }
+}
---
+++ Week04/Avarage.java
@@ -0,0 +1,21 @@
+package Week04;
+
+import java.util.Scanner;
+
+public class Avarage {
+    public static void main(String[] args) {
+        int [] test = new int[10];
+    Scanner scn= new Scanner(System.in);
+    int sum = 0;
+    int cnt = 0;
+    for (int i=0; i<test.length;i++)
+    {
+        test[i]=scn.nextInt();
+         sum += test[i];
+         cnt++;
+    }
+    double ave= 1.0* sum/cnt;
+    System.out.println(ave);
+    }    
+}
+
