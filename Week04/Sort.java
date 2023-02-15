0525eb52cac33fe10e7db06996984cb63fa0e25b
Behruz Gurbanli <bgurbanli14129@ada.edu.az>
2023-02-15 23:13:08 +0400

    Sort modified

--- Week04/Sort.java
+++ Week04/Sort.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Sort {
     public static void main(String[] args) {
-        int[] array = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] array = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 9, 9, 5, 4, 3, 3, 4, 5, 6, 3, 1, 3, 5, 3};
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < array[i]) {
@@ -16,11 +16,21 @@ public class Sort {
             }
         }
         System.out.println(Arrays.toString(array));
+        System.out.println("The most frequent element in the array is: " + mostFrequentElement(array));
     }
-    public static int mostRepeatedElement(int[] array) {
+    public static int mostFrequentElement(int[] array) {
         int max = 0;
-        int element;
-
+        int element = 0;
+        for (int i = 0; i < array.length; i++) {
+            int count = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) count++;
+            }
+            if (count > max) {
+                max = count;
+                element = array[i];
+            }
+        }
         return element;
     }
 }
