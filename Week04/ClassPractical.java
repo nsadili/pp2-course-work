b79c0b1a4c115d721eebfe7d4002c5739ec949b7
Suel Heybatova 13641 <123533417+suelheybatova1@users.noreply.github.com>
2023-02-15 09:17:59 +0400

    Updated ex.

--- Week04/ClassPractical.java
+++ Week04/ClassPractical.java
@@ -9,6 +9,23 @@ public class ClassPractical {
             n = n / 10;
         }
         System.out.println(Arrays.toString(arr));
+        int max = arr[0];
+        for (int i = 0; i <arr.length; i++){
+            if (arr[i]>max){
+            }
+        }
+        System. out.println(max);
+        System.out.println(indexOf(arr,max));
     }
+    public static int indexOf(int arr[],int element){
+        int index = -1;
+        for (int i = 0; i <arr.length; i++){
+            if(arr[i] == element){
+                 index = i;
+            }
+        }
+        return index;
+    }
+
 }
 
---
+++ Week04/Ex02.java
@@ -0,0 +1,5 @@
+package Week04;
+
+public class Ex02 {
+
+}
