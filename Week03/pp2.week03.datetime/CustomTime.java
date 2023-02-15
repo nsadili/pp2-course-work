2a645ba75329017f1c654e1c4bfa8181b4f29a9e
Pasha <pzulfugarli13731@ada.edu.az>
2023-02-15 21:10:54 +0400

    PW

---
+++ Week03/pp2.week03.datetime/CustomTime.java
@@ -0,0 +1,49 @@
+public class CustomTime {
+    int hour = 0;
+    int minute = 0;
+    int second = 0;
+    public CustomTime(int hour){
+        this.setHour(hour);
+    }
+    public CustomTime(int hour, int minute){
+        this.setHour(hour);
+        this.setMinute(minute);
+    }
+    public CustomTime(){
+    }
+    public CustomTime(int hour, int minute, int second){
+        this.setHour(hour);
+        this.setMinute(minute);
+        this.setSecond(second);
+    }
+    public CustomTime(CustomTime time){
+        time.hour = this.hour;
+        time.minute = this.minute;
+        time.second = this.second;
+    }
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+    public void setSecond(int second) {
+        this.second = second;
+    }
+    public int getHour() {
+        return hour;
+    }
+    public int getMinute() {
+        return minute;
+    }
+    public int getSecond() {
+        return second;
+    }
+    public void toUniversalString(String note, CustomTime time){
+        System.out.printf("%s %02d:%02d:%02d\n", note, time.hour, time.minute, time.second);
+    }
+    public void toStandardString(String note, CustomTime time){
+        System.out.printf("%s %02d:%02d:%02d %s\n", note, (time.hour > 12 ? time.hour - 12 : time.hour),
+        time.minute, time.second, (time.hour > 12 ? "PM" : "AM"));
+    }
+}
---
+++ Week03/pp2.week03.datetime/CustomTimeTest.java
@@ -0,0 +1,15 @@
+public class CustomTimeTest {
+    public static void main(String[] args) {
+        CustomTime fullTime = new CustomTime(15, 21, 12);
+        CustomTime onlyHoursTime = new CustomTime(15);
+        CustomTime timeWithoutSeconds = new CustomTime(15, 21);
+        CustomTime zeroTime = new CustomTime();
+        
+        System.out.println();
+        fullTime.toUniversalString("Full time:", fullTime);
+        onlyHourTime.toUniversalString("Only hours time:", onlyHourTime);
+        timeWithoutSeconds.toStandardString("Time without seconds:", timeWithoutSeconds);
+        zeroTime.toStandardString("Zero time:", zeroTime);
+        System.out.println();
+    }
+}
