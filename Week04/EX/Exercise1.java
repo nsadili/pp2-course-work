e09b31669ee2c625ef7f8e2e49463749bfac26a8
Madina Alili 15406 <malili15406@ada.edu.az>
2023-02-15 22:31:09 +0400

    15 feb

---
+++ Week04/EX/Exercise1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        if(args.length==0){
+            System.out.println("Please, enter at least an integer: ");
+            System.exit(1);
+        }
+
+        int numbers[]=new int[args.length];
+        for(int i=0; i<args.length; i++){
+            numbers[i] = Integer.parseInt(args[i]);
+
+        }
+
+        printArr(numbers);
+        System.out.println(sumArr(numbers));
+    }
+
+    private static void printArr(int[] arr) {
+         for(var el : arr)
+            System.out.print(el+" ");
+        System.out.println();
+            
+        }
+
+
+    private static int sumArr(int[] arr){
+        var sum=0;
+        for(var el : arr)
+            sum+=el;
+        return sum;
+
+
+
+    }
+    }
