10491d76d6fdc4def2fde8c06e5e205ebfc56738
nibrahimli16079 <nibrahimli16079@ada.edu.az>
2023-02-15 08:12:07 +0400

    pw

---
+++ Week03/CustomTimeTest.java
@@ -0,0 +1,8 @@
+package Week03;
+
+public class CustomTimeTest {
+    public static void main(String[] args) {
+        CustomTime time1 = new CustomTime(14, 5, 6);
+        System.out.println("time 1: " + time1.toStandardString());
+    }
+}
