c8b02d15b5b4cdbc4ec1c5de94b63bc3cd34e96b
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-02-16 17:51:50 +0400

    Add files via upload

---
+++ Week03/CustomTime.java
@@ -0,0 +1,68 @@
+package pp2.week3.datetime;
+
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+    private boolean isAM;
+
+    public Time(int hours, int minutes, int seconds) {
+        setHours(hours);
+        setMinutes(minutes);
+        setSeconds(seconds);
+    }
+
+    public Time() {
+        this(0, 0, 0);
+    }
+
+    public Time(int hours) {
+        this(hours, 0, 0);
+    }
+
+    public Time(int hours, int minutes) {
+        this(hours, minutes, 0);
+    }
+
+    public Time(Time time) {
+        this(time.hours, time.minutes, time.seconds);
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        if (hours >= 0 && hours <= 23) {
+            this.hours = hours;
+            this.isAM = hours < 12;
+        } else {
+            this.hours = 0;
+        }
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes >= 0 && minutes <= 59 ? minutes : 0;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds >= 0 && seconds <= 59 ? seconds : 0;
+    }
+
+    public String toUniversalString() {
+        return String.format("%02d:%02d:%02d", getHours(), getMinutes(), getSeconds());
+    }
+
+    public String toStandardString() {
+        return String.format("%d:%02d:%02d %s", (getHours() == 0 || getHours() == 12) ? 12 : getHours() % 12,
+                getMinutes(), getSeconds(), isAM ? "AM" : "PM");
+    }
+}
