c1c81269dec73b1902d33ce28f67d2b73cb4c27f
nilufar_babayeva_16230 <nbabayeva16230@ada.edu.az>
2023-02-16 16:01:18 +0400

    Arrays

---
+++ Week04/SortingArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class SortingArray {
+    public static  void main(String[] args){
+        int [] arr= new int[]{1,2,3,4,5,6,7,8,9};
+
+        for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr.length;j++){
+           if(arr[i]>arr[j]){
+            arr[i]=arr[j]+arr[i];
+            arr[j]=arr[i]-arr[j];
+            arr[i]=arr[i]-arr[j];
+           }
+
+    }
+}
+
+    System.out.println(Arrays.toString(arr));    
+    }
+    
+}
