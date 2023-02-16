f773cf81b27a62f86987e799d0c61c1c9046bf7a
zakir ashirli 17640 <asirlizakir@gmail.com>
2023-02-16 23:59:24 +0400

    week04 PW and EX

---
+++ Week04/src/practicalworks/Histogram.java
@@ -0,0 +1,97 @@
+package practicalworks;
+
+import java.util.Arrays;
+
+public class Histogram {
+	public static void main(String[] args) {
+
+		int[] grades = { 1, 2, 9, 10, 7, 6, 4, 3, 8, 1, 1 };
+		int[] histogram = new int[11];
+
+		// Count number of each grade
+		for (int i = 0; i < grades.length; i++) {
+			histogram[grades[i]]++;
+		}
+
+		// Display histogram
+		for (int i = 0; i < histogram.length; i++) {
+			System.out.println(i + ": " + histogram[i]);
+		}
+
+		// Calculate mean, median, and mode
+		int mean = calculateMean(grades);
+		int median = calculateMedian(grades);
+		int mode = calculateMode(grades);
+
+		// Display results
+		System.out.println("Mean: " + mean);
+		System.out.println("Median: " + median);
+		System.out.println("Mode: " + mode);
+	}
+
+	public static int calculateMean(int[] grades) {
+
+		int sum = 0;
+
+		for (int i = 0; i < grades.length; i++) {
+
+			sum += grades[i];
+		}
+
+		return sum / grades.length;
+	}
+
+	public static int calculateMedian(int[] grades) {
+		Arrays.sort(grades);
+
+		int middle = grades.length / 2;
+
+		if (grades.length % 2 == 0) {
+
+			return (grades[middle - 1] + grades[middle]) / 2;
+
+		} else {
+
+			return grades[middle];
+
+		}
+	}
+
+	public static int calculateMode(int[] grades) {
+
+		Arrays.sort(grades);
+
+		int mode = grades[0];
+
+		int maxCount = 1;
+
+		int currentCount = 1;
+
+		for (int i = 1; i < grades.length; i++) {
+
+			if (grades[i] == grades[i - 1]) {
+
+				currentCount++;
+
+			} else {
+
+				if (currentCount > maxCount) {
+
+					maxCount = currentCount;
+
+					mode = grades[i - 1];
+				}
+
+				currentCount = 1;
+			}
+		}
+
+		if (currentCount > maxCount) {
+
+			mode = grades[grades.length - 1];
+
+		}
+
+		return mode;
+	}
+}
