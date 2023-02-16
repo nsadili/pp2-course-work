9f010fdb36794b0ad99de9012832c097ec26f1a1
Nazrin Muradova 16062 <nnmuradova16062@ada.edu.az>
2023-02-16 10:19:35 +0400

    Add new file

---
+++ Week04/homework1.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class homework1 {
+    public static void main(String[] args) {
+        int array[] = { -45, 3, 7, 112, 4, -5, 27, 33, 9, 0, 12, 0};
+        Arrays.sort(array);
+
+    //print the minimum value 
+    System.out.println("Minimum value: " + array[0]);
+
+    //print the maximum value 
+    System.out.println("Maximum value: " + array[3]);
+
+  }
+    }
