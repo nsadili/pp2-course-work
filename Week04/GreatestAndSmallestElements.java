314c7d1d806840ce07a205cc8e0426de403223c1
Suel Heybatova 13641 <123533417+suelheybatova1@users.noreply.github.com>
2023-02-16 22:56:15 +0400

    New ex.

---
+++ Week04/GreatestAndSmallestElements.java
@@ -0,0 +1,65 @@
+package Week04;
+import java.util.Scanner;
+public class GreatestAndSmallestElements {
+        static int[] arr = new int[999];
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int i = 0;
+            while (sc.hasNextInt()) {
+                arr[i++] = sc.nextInt();
+            }
+            System.out.println("min value from scn input is... " + minimum(arr));
+            System.out.println("max value from scn input is... " + maximum(arr));
+            if (args.length != 0) {
+                System.out.println("min value from argument input is... " + minimum(args));
+                System.out.println("max value from argument input is... " + maximum(args));
+            }
+            System.out.println("The min and max value from scn input are... " + minandmax(arr));
+        }
+        public static int minimum(int[] array) {
+            int min = array[0];
+            for(int x:array) {
+                if(x < min)
+                    min = x;
+            }
+            return min;
+        }
+        public static int maximum(int[] array) {
+            int max = array[0];
+            for(int x:array) {
+                if(x > max)
+                    max = x;
+            }
+            return max;
+        }
+        public static int minimum(String[] array) {
+            int min = Integer.parseInt(array[0]);
+            for(String x:array) {
+                if(Integer.parseInt(x) < min)
+                    min = Integer.parseInt(x);
+            }
+            return min;
+        }
+        public static int maximum(String[] array) {
+            int max = Integer.parseInt(array[0]);
+            for(String x:array) {
+                if(Integer.parseInt(x) > max)
+                    max = Integer.parseInt(x);
+            }
+            return max;
+        }
+        public static String minandmax(int[] array) {
+            int[] answer = new int[2];
+            int min = array[0];
+            int max = array[0];
+            for(int x:array) {
+                if(x < min)
+                    min = x;
+                if(x > max)
+                    max = x;
+            }
+            return "min: " + min + ". max: " + max;
+        }
+    }
+
+
