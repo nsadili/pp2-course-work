e18c0cb99f4fdb2a9b2ca9fc0077b6bb5e9ca04c
Sadig Mammadov 14065 <sqmammadov@gmail.com>
2023-02-16 17:13:42 +0400

    new updates

---
+++ Week04/ex07/StringReversed.java
@@ -0,0 +1,48 @@
+package Week04.ex07;
+
+import java.util.Scanner;
+
+public class StringReversed {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        //System.out.println(reverse1(s));
+        //System.out.println(reverse2(s));
+        System.out.println(reverse3(s));
+        sc.close();
+    }
+
+    static String reverse1 (String s) {
+        String temp = "";
+
+       for (int i = s.length() - 1; i >= 0; i-- ) {
+            temp += s.charAt(i);
+       } 
+       return temp;
+    }
+
+    static String reverse2 (String s) { // memory efficient
+        char[] chars = s.toCharArray();
+
+        int l = chars.length;
+
+        for (int i = 0; i < l / 2; i++) {
+            char temp = chars[i];
+            chars[i] = chars[l - 1 - i];
+            chars[l - 1 - i] = temp;
+        }
+
+        return new String (chars);
+    }
+
+    static String reverse3 (String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = s.length() - 1; i >= 0; i-- ) {
+            sb.append(s.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
