60b9eb23abc612e99ccddf30d0f8bf46ba9d9921
Fatid-Ibadov <fibadov17954@ada.edu.az>
2023-02-16 17:26:46 +0400

    cm

--- Week02/LearnScanner.java
+++ Week02/LearnScanner.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 public class LearnScanner {
     public static void main(String[] args){
 
-        Scanner x = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name");
-
-        String yourName = x.nextLine();
+        String yourName = sc.nextLine();
+        
+        System.out.println("Enter how old are you");
+        int yourAge = sc.nextInt();
+        
+        System.out.println("Enter how tall are you");
+        double yourHeight = sc.nextDouble();
+        
         System.out.println("Username is: " + yourName);
+        System.out.println("Your age is: " + yourAge);
+        System.out.println("Your height is: " + yourHeight);
+        
+
+
     }
 
 }
