3bd6b6e72d1617a65aa883a2e5e6b1d0ca87420e
“Abov <aakhundov17192@ada.edu.az>
2023-02-17 12:08:18 +0400

    sum

---
+++ Week04/ex/Exercise1.java
@@ -0,0 +1,34 @@
+package Week04.ex;
+
+import java.util.Arrays;
+
+public class Exercise1{
+    public static void main(String[]args){
+        if (args.length==0){
+            System.out.println("Please provide at least a number");
+            System.exit(1);
+        }
+        int numbers[]=new int[args.length];
+        for (int i=0; i<args.length;i++){
+            numbers[i]=Integer.parseInt(args[i]);
+        }
+        printArr(numbers);
+        System.out.println(sumArr(numbers));
+    }
+  
+    private static void printArr(int[]arr){
+            for (var el : arr)
+                System.out.print(el + " ");
+            System.out.println();
+            
+        }
+            
+    private static int sumArr(int[] arr){
+        var sum = 0;
+        for (var el : arr)
+            sum+=el;
+            return sum;
+            
+        
+    }
+}
