45f34f4b1cda4f2b42ba653787203e714edef8b8
Pasha <pzulfugarli13731@ada.edu.az>
2023-02-17 11:41:38 +0400

    PW

---
+++ Week04/ExplodedString.java
@@ -0,0 +1,18 @@
+package Week04;
+
+import.java.util.Scanner;
+
+public class ExplodedString{
+    public class void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextline();
+        String[]split = str.split("");
+        StringBuilder ab = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < split.length; i++){
+            sb.append(split[i]);
+            result append(sb);
+        }
+        System.out.println(result);
+    }
+}
