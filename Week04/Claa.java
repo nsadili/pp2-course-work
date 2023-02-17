e4497bb925555af22aa29c5b05766bb5857b42dc
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-02-17 15:22:49 +0400

    Add files via upload

---
+++ Week04/Claa.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Claa {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        System.out.println("The elements of the array are: ");
+        printArray(arr);
+        System.out.println("The sum of the elements of the array is: " + sumArray(arr));
+        System.out.println("The average of the elements of the array is: " + (sumArray(arr) / n));
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int sumArray(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
+
+
+
+
