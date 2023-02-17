18c48e1035abfeed9cec65d97701a1e169a853aa
Behruz Gurbanli <bgurbanli14129@ada.edu.az>
2023-02-18 01:56:30 +0400

    Slices added.

---
+++ Week04/Slices.java
@@ -0,0 +1,20 @@
+package Week04;
+
+import java.util.Scanner;
+
+public class Slices {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please give the input: ");
+        String word = sc.nextLine();
+        slices(word);
+//        System.out.println(word.substring(0, 2));
+    }
+
+    public static void slices(String word) {
+        String[] split = word.split("");
+        for (int i = 1; i < split.length; i++) {
+            System.out.println(word.substring(0, i) + " " + word.substring(i, split.length));
+        }
+    }
+}
