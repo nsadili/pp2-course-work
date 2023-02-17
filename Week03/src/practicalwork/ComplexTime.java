1d374981a7b875c71f36f084c197c9d2e222fb52
HuseynSadatkhanov <hsadatkhanov13770@ada.edu.az>
2023-02-17 12:17:11 +0400

    Week3 added code

---
+++ Week03/src/practicalwork/ComplexTime.java
@@ -0,0 +1,41 @@
+package practicalwork;
+
+class ComplexNumbers {
+	private double real;
+	private double imaginary;
+
+	public ComplexNumbers(double real, double imaginary) {
+		this.real = real;
+		this.imaginary = imaginary;
+	}
+
+	public double re() {
+		return real;
+	}
+
+	public double getImaginary() {
+		return imaginary;
+	}
+
+	public ComplexNumbers add(ComplexNumbers other) {
+		return new ComplexNumbers(real + other.real, imaginary + other.imaginary);
+	}
+
+	public ComplexNumbers subtract(ComplexNumbers other) {
+		return new ComplexNumbers(real - other.real, imaginary - other.imaginary);
+	}
+
+	public ComplexNumbers multiply(ComplexNumbers other) {
+		double realPart = real * other.real - imaginary * other.imaginary;
+		double imaginaryPart = real * other.imaginary + imaginary * other.real;
+		return new ComplexNumbers(realPart, imaginaryPart);
+	}
+
+	public ComplexNumbers conjugate() {
+		return new ComplexNumbers(real, -imaginary);
+	}
+
+	public String toString() {
+		return real + " + " + imaginary + "i";
+	}
+}
