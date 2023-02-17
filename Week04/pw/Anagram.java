46453e91a6efbede6a77cf713a0cb07a2921b32c
NarimanMammadov <nmammadov16237@ada.edu.az>
2023-02-17 21:08:57 +0400

    anagram added

--- Week04/pw/Anagram.java
+++ Week04/pw/Anagram.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Anagram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -6,11 +7,25 @@ public class Anagram {
         String std1 = sc.next();
         System.out.println("Please enter the 2nd string!");
         String std2 = sc.next();
-        if (std1!=std2) System.out.println(false);
-        char[] chars = new char[std1.length()];
-        for (int i=0;i<std1.length();i++){
 
-        }
-        }
+        System.out.printf("Is being these strings anagram false or true? --> %b", checkingAnagram(std1, std2));
+
+
+
+    }
+
+    private static boolean checkingAnagram(String str01,String str02){
+        str01 = str01.toLowerCase();
+        str02 = str02.toLowerCase();
+
+        if (str01.length() != str02.length()) return false;
+
+        char[] str01A = str01.toCharArray();
+        char[] str02A = str02.toCharArray();
+
+        Arrays.sort(str01A);
+        Arrays.sort(str02A);
+
+        return Arrays.equals(str01A,str02A);
     }
 }
