63ced91ae57f2d0abe90de7a2ab1ce6b93a3bddb
Bahruz Zeynalov <bzeynalov16139@ada.edu.az>
2023-02-17 10:00:11 +0400

    week 4

---
+++ Week04/ex/Exercise1.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+import javax.security.auth.PrivateCredentialPermission;
+
+public class Exercise1{
+   public static void main(String[] args) {
+    
+    if (args.length == 0) {
+        System.out.println("Please,enter at least an integer") ;
+        System.exit(1);
+    }
+     
+    int numbers[] = new int[args.length];
+    for(int i=0 ; i<args.length ; i++) {
+        numbers[i] = Integer.parseInt(args[i]);
+    }
+
+    //System.out.println(Arrays.toString(args));
+    //System.out.println(Arrays.toString(numbers));
+
+     printArr(numbers);
+     System.out.println(sumArr(numbers));
+   } 
+
+   private static void printArr(int[] arr) {
+    for(var el : arr)
+    System.out.print(el + " ");
+    System.out.println();
+   }
+
+   private static int sumArr(int[] arr) { 
+
+
+    
+
+    var sum = 0;
+    for ( var el: arr)
+    sum += el;
+
+    return sum;
+   }
+    
+}
