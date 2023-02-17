2ba133b5fa1c0b5bf55b02ef4fc6bf783ca8592f
leylaneymat <lneymat17256@ada.edu.az>
2023-02-17 11:10:15 +0400

    histogram exercise

---
+++ Week04/EX/ex1.java
@@ -0,0 +1,79 @@
+package EX;
+import java.util.Arrays;
+
+public class ex1 {
+    // main
+    public static void main(String[] args){
+        if (args.length == 0) {
+            System.out.println("Please, print a value.");
+            System.exit(1);
+        }
+
+        System.out.println(Arrays.toString(args));
+
+        int numbers[] = new int[args.length];
+        for (int i = 0; i < args.length; i++){
+            numbers[i] = Integer.parseInt(args[i]);
+        }
+      
+        printArray(numbers);
+        sum(numbers);
+        returnMax(numbers);
+        returnMin(numbers);
+    }
+
+    // classes
+    public class minMax {
+        int min;
+        int max;
+
+        public minMax(int min, int max){
+            setMax(max);
+            setMin(min);
+        }
+
+        public void setMin(int min) {
+            this.min = min;
+        }
+
+        public void setMax(int max) {
+            this.max = max;
+        }
+        
+    }
+
+    // methods
+    static void printArray(int[] arr){
+        for (int el: arr){
+            System.out.println(el + " ");
+        }
+    }
+
+    private static int sum(int[] arr){
+        var sum = 0;
+        for (var el: arr){
+            sum += el;
+        }
+        return sum;
+    }
+
+    public static int returnMax(int[] arr){
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] > max) max = arr[i];
+        }
+        return max;
+    }
+
+    public static int returnMin(int[] arr){
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] < min) min = arr[i];
+        }
+        return min;
+    }
+
+    public minMax returnMaxAndMin(int[] arr){
+        return new minMax(returnMin(arr), returnMax(arr));
+    }
+}
---
+++ Week04/EX/stringReversed.java
@@ -0,0 +1,53 @@
+package EX;
+import java.util.Scanner;
+
+public class stringReversed {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        System.out.println(explodedString(input));
+        scan.close();
+    }
+
+    static String reverseV1(String str){
+        String res = "";
+        for (int i = str.length() - 1; i >= 0; i--){
+            res += str.charAt(i);
+        }
+        return res;
+}
+
+    static String reverseV2(String str){
+        char[] chars = str.toCharArray();
+        int len = chars.length;
+        for (int i = 0; i < len / 2; i++){
+            char tmp = chars[i];
+            chars [i] = chars[len - 1 - i];
+            chars [len - 1 - i] = tmp;
+        }
+        return new String(chars);
+    }
+
+    static String reverseV3(String str){
+        StringBuilder sb = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--){
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    static String reverseV4(String str){
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    private static String explodedString(String str){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                sb.append(str.charAt(j));
+            }
+        }
+        return sb.toString();
+    }
+}
+
