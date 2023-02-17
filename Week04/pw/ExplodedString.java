c26571222e9a3d1f1852846e831e3f53a76a98b5
rufat_hajizada_16231 <rufeth04@gmail.com>
2023-02-17 14:59:07 +0400

    Updated

--- Week04/pw/ExplodedString.java
+++ Week04/pw/ExplodedString.java
@@ -4,8 +4,19 @@ public class ExplodedString {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
         String str = sc.nextLine();
-        sc.close();       
-    }
+        sc.close(); 
 
+        System.out.println(explode(str));
+
+    }
     
+    public static String explode(String str){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<str.length(); i++){
+            for (int j=0; j<=i; j++){
+                sb.append(str.charAt(j));
+            }
+        }             
+        return sb.toString();
+    }
 }
