92480ee39ffd28d6139e0b25fb1341bf22f707e5
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-02-19 00:04:30 +0400

    Add files via upload

---
+++ Week04/AnagRef.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class AnagRef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first string: ");
+        String str1 = sc.nextLine();
+        System.out.println("Enter the second string: ");
+        String str2 = sc.nextLine();
+        if (isAnagram(str1, str2)) {
+            System.out.println("The two strings are anagrams");
+        } else {
+            System.out.println("The two strings are not anagrams");
+        }
+    }
+
+    public static boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        int[] count = new int[256];
+        for (int i = 0; i < str1.length(); i++) {
+            count[str1.charAt(i)]++;
+            count[str2.charAt(i)]--;
+        }
+        for (int i = 0; i < 256; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+
