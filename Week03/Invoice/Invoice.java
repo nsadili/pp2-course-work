e4ea4e8c778e3596b0fc40d619e33cd3cec4d736
“Abov <aakhundov17192@ada.edu.az>
2023-02-19 22:20:38 +0400

    invoice

---
+++ Week03/Invoice/Invoice.java
@@ -0,0 +1,58 @@
+package Invoice;
+
+public class Invoice {
+    private String number;
+    private String description;
+    private int quantity;
+    private double price;
+
+    public Invoice(String number, String description, int quantity, double price) {
+        if(quantity < 0) {
+            quantity = 0;
+        }
+        if(price < 0) {
+            price = 0;
+        }
+
+        this.number = number;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getInvoiceAmount() {
+        return this.price * this.quantity;
+    }
+}
