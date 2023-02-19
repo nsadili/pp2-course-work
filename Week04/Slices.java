984117e0f8f9d78ab0f67315a26138eb1d84b41a
Seljan Mamedova <118668057+SeljanMamedova@users.noreply.github.com>
2023-02-19 13:57:36 +0400

    slices added

---
+++ Week04/Slices.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Slices {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please give the input: ");
+        String word = sc.nextLine();
+        slices(word);
+    }
+
+    public static void slices(String word) {
+        String[] split = word.split("");
+        for (int i = 1; i < split.length; i++) {
+            System.out.println(word.substring(0, i) + " " + word.substring(i, split.length));
+        }
+    }
+}
