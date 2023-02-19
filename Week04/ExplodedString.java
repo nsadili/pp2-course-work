bdc09b525c27bf8b495bbcf9063d0d5db9339303
Seljan Mamedova <118668057+SeljanMamedova@users.noreply.github.com>
2023-02-19 13:58:49 +0400

    PW's added

---
+++ Week04/ExplodedString.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class ExplodedString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("give the string: ");
+        String str = sc.nextLine();
+        System.out.println(explodeString(str));
+    }
+
+    public static StringBuilder explodeString(String string) {
+        char[] split = string.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder exploded = new StringBuilder();
+        for (int i = 0; i < split.length; i++) {
+            sb.append(split[i]);
+            exploded.append(sb);
+        }
+        return exploded;
+    }
+}
