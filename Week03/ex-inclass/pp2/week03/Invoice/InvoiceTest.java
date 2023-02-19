80b579db1f56a4960eee5180584d0606f268e665
Laman Panakhova 16882 <lamanpanakhova16882@ada.edu.az>
2023-02-19 18:01:07 +0400

    Instructor's example codes for understanding

---
+++ Week03/ex-inclass/pp2/week03/Invoice/InvoiceTest.java
@@ -0,0 +1,8 @@
+package pp2.week03.Invoice;
+
+public class InvoiceTest {
+    public static void main(String[] args) {
+        Invoice a = new Invoice("1","iPhone X", 2, 405);
+System.out.println(a.getInvoiceAmount());
+    }
+}
