fc280c5294eca14d2c605063847822a7f35de5f8
jala-asadli-16143 <jasadli16143@ada.edu.az>
2023-02-19 22:09:05 +0400

    Add week04 ex04

---
+++ Week04/ex04/Anagrams.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagrams {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please, enter the first input: ");
+        String str1 = sc.nextLine();
+
+        System.out.println("Please, enter the second input: ");
+        String str2 = sc.nextLine();
+
+        boolean areAnagrams = ifAnagrams(str1, str2);
+        if (areAnagrams) { // are anagrams
+            System.out.println("The two strings are anagrams.");
+        } else { // are not anagrams
+            System.out.println("The two strings are not anagrams.");
+        }
+        sc.close();
+    }
+    public static boolean ifAnagrams(String str1, String str2) {
+        //makes inputs similar by making all lower case, removes spaces
+        str1 = str1.replaceAll(" ", "");
+        str2 = str2.replaceAll(" ", "");
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        if(str1.length() == str2.length()) {
+            char[] charArray1 = str1.toCharArray();
+            char[] charArray2 = str2.toCharArray();
+
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            return Arrays.equals(charArray1, charArray2);
+        }else {
+            return false;
+        }
+    }
+}
