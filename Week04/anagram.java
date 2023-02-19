826f76fc28be2bd400739e9d4ff97eac782e3d35
Nazrin Muradova 16062 <nnmuradova16062@ada.edu.az>
2023-02-19 22:53:08 +0400

    Add new file

---
+++ Week04/anagram.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class anagram{
+
+    public static boolean areAnagrams(String str1, String str2) {
+        // Check if the lengths of the strings are equal
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        
+        // Convert the strings to char arrays and sort them
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        
+        // Compare the sorted char arrays
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
