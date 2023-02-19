a621ba68c968ccbacb053d5452660d55d0105fad
Murad Mammadov 17382 <mamedovmurad000@gmail.com>
2023-02-19 17:14:14 +0400

    Week 02 ex

--- Week02/ex/CoinToss.java
+++ Week02/ex/CoinToss.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CoinTossing {
+public class CoinToss {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), heads = 0, tails = 0;
