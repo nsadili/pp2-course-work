14aa35c36ad6599b9efb3d30c81e88cf18675c18
Nijat Alisoy 17366 <nalisoy17366@ada.edu.az>
2023-02-19 17:44:41 +0400

    datetime

--- Week03/ex/pp2/week03/datetime/CustomDateTest.java
+++ Week03/ex/pp2/week03/datetime/CustomDateTest.java
@@ -1,8 +1,12 @@
-package pp2.week03.datetime;
+package pp2.week03;
+
+import pp2.week03.datetime.CustomDate;
 
 public class CustomDateTest {
     public static void main(String[] args){
-        
+        CustomDate date = new CustomDate(30, 11, 2005);
+        date.displayDate();
+        date.displayFormatted();
     }
     
 }
--- Week03/ex/pp2/week03/datetime/CustomTimeTest.java
+++ Week03/ex/pp2/week03/datetime/CustomTimeTest.java
@@ -1,5 +1,10 @@
 package pp2.week03.datetime;
 
 public class CustomTimeTest {
+    public static void main(String[] args) {
+        CustomTime time = new CustomTime(5,36);
+        System.out.println(time.toUniversalString());
+        System.out.println(time.toStandardString());
+    }
     
 }
