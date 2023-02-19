df5ed6abb346617098ea0057925d6781d5008f1b
“Abov <aakhundov17192@ada.edu.az>
2023-02-19 22:21:46 +0400

    invoicetest

---
+++ Week03/Invoice/InvoiceTest.java
@@ -0,0 +1,8 @@
+package Invoice;
+
+public class InvoiceTest {
+    public static void main(String[] args) {
+        Invoice a = new Invoice("1","iPhone X", 2, 405);
+System.out.println(a.getInvoiceAmount());
+    }
+}
