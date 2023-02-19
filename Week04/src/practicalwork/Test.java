d483f5eb9be40607e6b66897dec29faaab79a4e2
HuseynSadatkhanov <hsadatkhanov13770@ada.edu.az>
2023-02-19 22:39:07 +0400

    Week4 Practical Work

--- Week04/src/practicalwork/Test.java
+++ Week04/src/practicalwork/Test.java
@@ -1,10 +1,24 @@
 package practicalwork;
 
+import java.util.Arrays;
+
 public class Test {
-
 	public static void main(String[] args) {
-		System.out.println("hello");
-
+		
+		String input = "Huseyn";
+		
+		String sortedString = sortString(input);
+		
+		System.out.println(sortedString);
 	}
+	
 
+	public static String sortString(String input) {
+		
+		char[] chars = input.toCharArray();
+		
+		Arrays.sort(chars);
+		
+		return new String(chars);
+	}
 }
