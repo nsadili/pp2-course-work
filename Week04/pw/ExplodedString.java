f41b2eaaed5a4a5565810d37ebc56b05aa28d54c
Aliya Zulfugarli 13080 <azulfugarli13080@ada.edu.az>
2023-02-19 23:22:43 +0400

    Added my java files

---
+++ Week04/pw/ExplodedString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExplodedString {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    String[] split = str.split("");
+    StringBuilder sb = new StringBuilder();
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < split.length; i++) {
+      sb.append(split[i]);
+      result.append(sb);
+    }
+    System.out.println(result);
+  }
+}
