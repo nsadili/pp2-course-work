033ea92a974b462a9b789203772c06031df48cad
Behruz Gurbanli <bgurbanli14129@ada.edu.az>
2023-02-19 12:15:51 +0400

    Anagrams.java Modified

--- Week04/Anagrams.java
+++ Week04/Anagrams.java
@@ -14,8 +14,8 @@ public class Anagrams {
     }
 
     public static void anagrams(String word1, String word2) {
-        String[] firstWord = sort(word1.split(""));
-        String[] secondWord = sort(word2.split(""));
+        char[] firstWord = sort(word1.toCharArray());
+        char[] secondWord = sort(word2.toCharArray());
 
         System.out.println(Arrays.toString(firstWord));
         System.out.println(Arrays.toString(secondWord));
@@ -24,10 +24,10 @@ public class Anagrams {
         else System.out.println("They are not anagrams");
     }
 
-    public static String[] sort(String[] ch) {
+    public static char[] sort(char[] ch) {
         for (int i = 0; i < ch.length; i++) {
             for (int j = i + 1; j < ch.length; j++) {
-                if (ch[j].compareTo(ch[i]) < 0) {
+                if (ch[j] < ch[i]) {
                     swap(ch, i, j);
                 }
             }
@@ -35,8 +35,8 @@ public class Anagrams {
         return ch;
     }
 
-    public static void swap(String[] ch, int i, int j) {
-        String temp = ch[j];
+    public static void swap(char[] ch, int i, int j) {
+        char temp = ch[j];
         ch[j] = ch[i];
         ch[i] = temp;
     }
