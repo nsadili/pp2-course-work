325b735f335491cd3a4dcee7dbf1f02a8ece6a8d
Madina Alili 15406 <malili15406@ada.edu.az>
2023-02-19 22:22:24 +0400

    week3

---
+++ Week03/geometry/Main.java
@@ -0,0 +1,18 @@
+package Week03.geometry;
+
+public class Main {
+    public static void main(String[] args) {
+        Point p1 = new Point(1, 5);
+        Point p2 = new Point(9, 12);
+        Segment segment = new Segment(p1, p2);
+
+        System.out.println("P1 is: (" + p1.getX() + "; " + p1.getY() + ").");
+        System.out.println("P2 is: (" + p2.getX() + "; " + p2.getY() + ").");
+        System.out.println("P1 equals P2: " + p1.equals(p2));
+        System.out.println("Distance between P1 and P2 is: " + p1.distance(p2));
+        System.out.println("The segment is on line with P1: " + segment.isOnLine(p1));
+        System.out.println("The segment is on line with P2: " + segment.isOnLine(p2));
+        System.out.println("The intercept of segment is: " + segment.getIntercept());
+        System.out.println("The slope of the segment: " + segment.getSlope());
+    }
+}
