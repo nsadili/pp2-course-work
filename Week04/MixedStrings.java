71e9a6b0494890528cf12929d6687a4b68cb4795
Suel Heybatova 13641 <123533417+suelheybatova1@users.noreply.github.com>
2023-02-19 18:36:57 +0400

    New ex.

---
+++ Week04/MixedStrings.java
@@ -0,0 +1,29 @@
+package Week04;
+public class MixedStrings {
+        public static String swapWords(String sentence) {
+            String[] words = sentence.split(" ");
+            StringBuilder sb = new StringBuilder();
+            for(String word : words) {
+                if(word.length() <= 1) {
+                    sb.append(word);
+                }
+                else {
+                    char first = word.charAt(0);
+                    char last = word.charAt(word.length() - 1);
+                    String middle = word.substring(1, word.length() - 1);
+                    sb.append(last).append(middle).append(first);
+                }
+                sb.append(" ");
+            }
+            return sb.toString().trim();
+        }
+
+        public static void main(String[] args) {
+            String sentence = "this is PP2 Fall 2021 :)";
+            String mixed = swapWords(sentence);
+            System.out.println("original sentence:) " + sentence);
+            System.out.println("mixed sentence:) " + mixed);
+        }
+    }
+
+
