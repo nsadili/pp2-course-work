052a1e6d819307ff1a7786e1c199e75afbf61f7f
narminasuleymanova <nsuleymanova17999@ada.edu.az>
2023-02-19 14:51:58 +0400

    New tasks

---
+++ Week03/CustomTimeTest.java
@@ -0,0 +1,7 @@
+public class CustomTimeTest {
+    public static void main(String[] args) {
+        CustomTime time = new CustomTime(14,54);
+        System.out.println(time.toUniversalString());
+        System.out.println(time.toStandardString());
+    }
+}
