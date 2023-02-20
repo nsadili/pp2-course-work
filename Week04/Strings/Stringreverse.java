61de345418dde8efb96d6049e2c8e982d1a0be3c
Alisadiq Shukurov 14234 <ashukurov14234@ada.edu.az>
2023-02-20 04:16:56 -0800

    String reversed

--- Week04/Strings/Stringreverse.java
+++ Week04/Strings/Stringreverse.java
@@ -15,4 +15,28 @@ public class Stringreverse {
         }
         return reversedString;
     }
+    private static String reverse2(String str){
+        char [] chars = str.toCharArray();
+        var len= chars.length;
+        for(int i=0; i<chars.length/2;i++){
+            char temp= chars[i];
+            chars[i]= chars[len-1-i];
+            chars[len-1-i]= temp;
+        }
+        return new String(chars);
+
+    }
+    private  static String reverse3(String str){
+        StringBuilder sb= new StringBuilder();
+        for(int i= str.length()-1;i>=0;i--){
+            sb.append(str.charAt(i));
+
+
+        }
+        return sb.toString();
+    }
+    private static String reverse4(String str){
+        return new StringBuilder(str).reverse().toString();
+    }
+
 }
