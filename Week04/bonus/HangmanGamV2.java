60323c712607460d02a0c6bf143f39fd5a057ecd
rufat_hajizada_16231 <rufeth04@gmail.com>
2023-02-20 11:04:51 +0400

    Hangman version 2 added

--- Week04/bonus/HangmanGamV2.java
+++ Week04/bonus/HangmanGamV2.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class HangmanGame {
+public class HangmanGameCopy {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         String str = sc.next().toLowerCase();
         //sc.close();
 
-        String secretword="";
+        StringBuilder sb = new StringBuilder();
         for (int i=0; i<str.length(); i++){
-            secretword+="_";
+            sb.append("_");
         }
 
         char[] charstr = str.toCharArray();
-        char[] charsw = secretword.toCharArray();
+        char[] charsw = sb.toString().toCharArray();
         
         for (int i=0; i<charsw.length; i++){
             System.out.print(charsw[i]+" ");
@@ -34,15 +34,13 @@ public class HangmanGame {
             }
             guessed.add(letter);
             for (int i=0; i<str.length();i++){
-                if (charsw[i] == letter) {
-                    cnttrue--;
-                }
                 if (charstr[i]==letter){
                     charsw[i]=letter;
                     cnt++;
                     cnttrue++;
                 }
             }
+
             if (cnt>=1) {
                 System.out.println("True Guess!!!");
             }
@@ -52,19 +50,23 @@ public class HangmanGame {
                 System.out.println("False Guess!!!");
                 System.out.println("False guessed letters: " + falseguess);
             }
+
             System.out.println("Left guesses: "+ (6-cntman));
-            //System.out.println("The word: "+ Arrays.toString(charsw));
+            
             for (int i=0; i<charsw.length; i++){
                 System.out.print(charsw[i]+" ");
             }
+
             if(cnttrue==charstr.length) {
                 System.out.println("\nYou Won! Congratulations!");
                 break;
             }
+
             if(cntman==6) {
-                System.out.println("You Lost! Game Over!");
+                System.out.println("\nYou Lost! Game Over!");
                 break;
             }
+
             System.out.println("\n");
             
         }
---
+++ Week04/bonus/TheEratosthenes.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TheEratosthenes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //System.out.println(findPrime(n));
+        int [] res=findPrime(n);
+        for (int i=1; i<=n; i++){
+            if (res[i]==1) System.out.println(i);
+        }
+
+    }
+
+    public static int[] findPrime(int n){
+        int[] ones= new int[n+1];
+
+        for (int i=2; i<=n; i++){
+            ones[i]=1;
+        }
+
+        for (int i=2; i<=n; i++){
+            if(ones[i]==1) {
+                for (int j=i*2; j<=n; j+=i){
+                    ones[j]=0;
+                }
+            }
+        }
+        
+        return ones;
+    }
+}
+
