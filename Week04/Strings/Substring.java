b7590506e901a9f19eadef72b0090b1adbefcf6a
Alisadiq Shukurov 14234 <ashukurov14234@ada.edu.az>
2023-02-20 06:10:55 -0800

    Substring added

--- Week04/Strings/Substring.java
+++ Week04/Strings/Substring.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Substring {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter String :");
+        System.out.println("Enter String : ");
         String string = sc.nextLine();
-        String sub = sc.nextLine();
-        System.out.println(substring(string, sub));
+        String substring = sc.nextLine();
+        System.out.println(substring(string, substring));
     }
 
     public static int substring(String string, String sub) {
-        char[] split = string.toCharArray();
-        char[] splitsub = sub.toCharArray();
+        char[] splitstring = string.toCharArray();
+        char[] splitsubstring = sub.toCharArray();
         int j = 0;
-        for (int i = 0; i < split.length; i++) {
-            if (splitsub[j] == split[i]) {
-                if (j == splitsub.length - 1) return i - j + 1;
+        for (int i = 0; i < splitstring.length; i++) {
+            if (splitsubstring[j] == splitstring[i]) {
+                if (j == splitsubstring.length - 1) return i - j + 1;
                 j++;
             } else j = 0;
         }
