5bb53933a959ffe61c5a6d6b0949323fb9c64e4d
Rahida <rasadli16055@ada.edu.az>
2023-02-20 22:06:24 +0400

    week4
    pw

---
+++ Week04/slices.java
@@ -0,0 +1,17 @@
+package Week04;
+
+public class slices {
+    public static void main(String[] args) {
+     String s= "SITE"; 
+     int c=0;
+     for (int i=0; i<s.length(); i++){ 
+       
+        System.out.print(s.substring(0, c++));
+        System.out.print(" ");
+        System.out.print(s.substring(i));
+        System.out.println();
+     }
+     
+    }
+   
+}
