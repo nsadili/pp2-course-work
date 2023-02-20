aa26d869c9135ab5899db9da4c3275821ce66f69
Alisadiq Shukurov 14234 <ashukurov14234@ada.edu.az>
2023-02-20 06:08:36 -0800

    Anagrams added

---
+++ Week04/Strings/Substring.java
@@ -0,0 +1,26 @@
+package Week04.Strings;
+
+import java.util.Scanner;
+
+public class Substring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String :");
+        String string = sc.nextLine();
+        String sub = sc.nextLine();
+        System.out.println(substring(string, sub));
+    }
+
+    public static int substring(String string, String sub) {
+        char[] split = string.toCharArray();
+        char[] splitsub = sub.toCharArray();
+        int j = 0;
+        for (int i = 0; i < split.length; i++) {
+            if (splitsub[j] == split[i]) {
+                if (j == splitsub.length - 1) return i - j + 1;
+                j++;
+            } else j = 0;
+        }
+        return -1;
+    }
+}
