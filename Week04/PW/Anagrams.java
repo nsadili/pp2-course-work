dc6ea472da9a777d8aca0ed681ead41ecbd17a27
Laman Jafarli 18018 <ljafarli18018@ada.edu.az>
2023-02-21 23:10:53 +0400

    .\PW\

---
+++ Week04/PW/Anagrams.java
@@ -0,0 +1,12 @@
+public class Anagrams {
+    public static void main(String[] args) {
+        boolen isAnagrams {
+            StringSorting ss = new StringSorting();
+            return ss.sort(s1).equals(ss.sort(s2));
+        }
+        public static void main(String[] args) {
+            Anagrams an = new Anagrams();
+            system.out.println(an.isAnagram("eat", "ate"));
+        }
+        }
+    }
