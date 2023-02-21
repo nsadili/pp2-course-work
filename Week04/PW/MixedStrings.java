eeb5cb14b3839a650fe0134140a2971ad8686a70
Laman Jafarli 18018 <ljafarli18018@ada.edu.az>
2023-02-21 23:33:35 +0400

    .\PW\

---
+++ Week04/PW/MixedStrings.java
@@ -0,0 +1,24 @@
+public class MixedStrings {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                System.out.print("Print a st: ");
+                String str = scan.nextLine();
+                System.out.println("Changed st: " + changedString(str) + '\n');
+                scan.close();
+            }
+            public static String changedString(String str){
+                String[] words = str.split(" ");
+                for (int i = 0; i < words.length; i++){
+                    char[] charArray = words[i].toCharArray();
+                    int len = charArray.length - 1;
+                    char temp = charArray[0];
+                    charArray[0] = charArray[len];
+                    charArray[len] = temp;
+                    String changedString = new String (charArray);
+                    words[i] = changedString;
+                }
+                return String.join(" ", words);
+            }
+        } 
+    }
