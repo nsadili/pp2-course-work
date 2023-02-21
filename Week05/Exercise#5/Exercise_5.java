7f101f76c0388c10af8209b824dc9d8d72d74563
RamilKhaspoladov <rkhaspoladov13582@ada.edu.az>
2023-02-21 21:27:58 +0400

    Adding Exercise_5

---
+++ Week05/Exercise#5/Exercise_5.java
@@ -0,0 +1,7 @@
+// Single - is the easiest type to extend one class from another. We call this inheritance as a single, when we have only one class to extend 
+// Multilevel - multilevel inheritance is the same as a single, but there are more classes to be extended. For example, let we have class A, which
+//inherit from class B. Moreover, we have C which inherit from class A, which entitle as to call this type of inheritance as a multilevel one
+//Hierarchical - this type of inheritance relates to codes, which have two or more classes, which are extended by one class. 
+//Multiple - multiple type of inheritance is an "impossible" one. We can not apply it by writing a code, however can access to it with the usage of interface.
+//Also we can say that multiple type is antonymous to the hierarchical one, because there we refer one class to two, not vice a verse.
+//Hybrid - a hybrid of hierarchical and multiple types of inheritance. It can be considered as the most complicated one
