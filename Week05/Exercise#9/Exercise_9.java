ae90bc5abe9b92c39ce4f26c7730aa54866d3dc4
RamilKhaspoladov <rkhaspoladov13582@ada.edu.az>
2023-02-22 10:24:20 +0400

    Changings in exercise_9

--- Week05/Exercise#9/Exercise_9.java
+++ Week05/Exercise#9/Exercise_9.java
@@ -25,4 +25,37 @@ public class Customer{
         return this.name + "(" + this.ID + ")";
              
     }
+
+    public class Account extends Customer {
+        private int id; 
+        private Customer customer; 
+        private double balance;
+    }
+ 
+    public Account(int id, Customer customer, double balance) {
+
+    }
+
+    public Account(int id, Customer customer, double balance) {
+       this.id = id;
+       this.customer = customer;
+       this.balance = balance;
+    }
+
+    public int getID() {
+        return this.id;
+    }
+
+    public String getCustomer() {
+        return this.customer; 
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void 
+    r  public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
 }
