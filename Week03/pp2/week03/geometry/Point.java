51ce4efaa9ecf655398829e85d67866211b0020a
fbakirli <113505730+fbakirli@users.noreply.github.com>
2023-02-22 13:39:59 +0400

    commit changes

--- Week03/pp2/week03/geometry/Point.java
+++ Week03/pp2/week03/geometry/Point.java
@@ -5,9 +5,9 @@ public class Point{
         private float y;
         
         public Point(){
-
+         
         }
-        public Point(float x, float y){
+     public Point(float x, float y){
           this.x=x;
           this.y=y;
      } 
@@ -18,9 +18,29 @@ public class Point{
      public float getX(){
       return this.x;
      }
+     public void setX(float x){
+        this.x=x;
+     }
     public float getY()   {
         return this.y;
     } 
+    public void setY(float y){
+        this.y=y;
+     }
+     public void translate(float dX, float dY){
+        this.x += dX;
+        this.y += dY;
+    }
+
+    public float distance(Point p) {
+        float dX=this.x-p.x;
+        float dY=this.y-p.y;
+        return (float) Math.sqrt(dX*dX+dY*dY);
+    }
+
+    public boolean equals(Point p) {
+        return this.x == p.getX() && this.y == p.getY();
+    }
 
 }
 
