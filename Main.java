5f8650e42e9f23a2a19b460920cc4854096d048e
Behruz Gurbanli <bgurbanli14129@ada.edu.az>
2023-02-22 14:03:11 +0400

    Main modified

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
--- Week04/Main.java
+++
@@ -1,8 +0,0 @@
-package Week04;
-
-public class Main {
-    public static void main(String[] args) {
-        String str = new String("Main");
-        System.out.println(str.charAt(4));
-    }
-}
