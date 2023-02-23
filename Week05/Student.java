cbf59cbb14c139e79b86cd4092aa7cc866a1d134
Tofiq041 <119070294+Tofiq041@users.noreply.github.com>
2023-02-23 22:30:46 +0400

    Create Student.java

---
+++ Week05/Student.java
@@ -0,0 +1,29 @@
+package Week05;
+
+import java.util.Arrays;
+
+public class Student extends Person {
+    private int studentId;
+
+    public Student(String firstName, String lastName, String gender, int studentId) {
+        super(firstName, lastName, gender);
+        this.studentId = studentId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String toString() {
+        return "Student studentId is" + studentId + ", firstName is " + getFirstName() + ", lastName is " + getLastName() + ", gender is " + getGender() + ".";
+    }
+
+    public boolean equals(Student s) {
+        return super.equals(s) &&
+                this.studentId == s.getStudentId();
+    }
+}
