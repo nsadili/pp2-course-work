e9b53a081dde036cde477f0ed9a74301b225ac19
Pasha <pzulfugarli13731@ada.edu.az>
2023-02-23 21:58:59 +0400

    PW

---
+++ Week05/Circleclasses.java
@@ -0,0 +1,30 @@
+package Week05;
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false
