b085022130b99acfb9fdd0088f4e8bc51dc6d47e
Pasha <pzulfugarli13731@ada.edu.az>
2023-02-23 21:58:07 +0400

    PW

---
+++ Week05/MathDemo.java
@@ -0,0 +1,30 @@
+package Week05;
+
+public class MathDemo {
+    public static int min(int a, int b) {
+        return (a < b) ? a : b;
+    }
+
+    public static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    public static int sum(int[] args) {
+        int sum = 0;
+        for (int x: args) {
+            sum += x;
+        }
+        return sum;
+    }
+
+    public static float mean(int[] args) {
+        return (float) sum(args) / args.length;
+    }
+
+    public static int factorial(int n) {
+        int factorial = 1;
+        for (int i = 2; i <= n; i++)
+            factorial *= i;
+        return factorial;
+    }
+}
