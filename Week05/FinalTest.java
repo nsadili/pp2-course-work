94d561d146f356798aad4d0a51039d49e203d71f
UlviMirzayev <umirzayev17452@ada.edu.az>
2023-02-23 09:27:30 +0400

    Test

---
+++ Week05/FinalTest.java
@@ -0,0 +1,21 @@
+public class FinalTest {
+    public static void main(String[] args) {
+        GeneralType gen=new GeneralType();
+    }
+}
+
+class A {
+    static { System.out.println("AS"); }
+     System.out.println("A"); 
+    public A() { System.out.println("AC"); }
+    public A(int x) { System.out.println(x + " AC"); }
+}
+
+class GeneralType extends A {
+        static { System.out.println("GTS"); }
+        { System.out.println("GT"); }
+        public GeneralType() {
+        super(5);
+        System.out.println("GTC");
+        }
+}
