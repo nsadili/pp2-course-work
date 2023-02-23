c6541565a4813cf601dfa336ac6811e8b5862b75
Ilqar Malikov13943 <102352450+imalikov13943@users.noreply.github.com>
2023-02-23 22:03:53 +0400

    Add files via upload
    Exercise 5

---
+++ Week05/Ex5.java
@@ -0,0 +1,11 @@
+package Week05;
+
+public class Ex5 {
+    /* Single Inheritance: It enables a derived class to inherit properties and behavior from a single parent class.
+     * Multilevel Inheritance: It is making that derived class the base class for the new class.
+     * Hierarchical Inheritance: When several classes are derived from same base class.
+     * Multiple Inheritance: When an object or a class can inherit characteristics and features from more than one parent class.
+     * Hybrid Inheritance: It is a combination of multiple inheritance and multilevel inheritance. A class is derived from two classes as in multiple inheritance.
+     */
+    
+}
