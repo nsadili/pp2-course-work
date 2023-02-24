c6d829d6579bc49a41cf6d02f0d180d7737a82d7
Elnar16260 <118639055+Elnar42@users.noreply.github.com>
2023-02-24 11:22:06 +0400

    Delete Week05/TaskSeven directory

--- Week05/TaskSeven/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int m[] = {1, 2,3, 4,5, 6};
-        double sum = MathDemo.mean(m);
-        int fact = MathDemo.factorial(0);
-        System.out.println(fact);
-    }
-}
