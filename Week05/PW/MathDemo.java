9d4ba60ea77bc636ce9abb2c106f2a1c7fc7f666
itsmurad <118719384+itsmurad@users.noreply.github.com>
2023-02-24 12:37:00 +0400

    Update MathDemo.java

--- Week05/PW/MathDemo.java
+++ Week05/PW/MathDemo.java
@@ -24,6 +24,8 @@ public class MathDemo {
         return sum(args) / args.length;
     }
     public static int factorial(int n){
+        if(n == 0) return 1;
+        if(n == 1) return 1;
         return n * factorial(n-1);
     }
 
