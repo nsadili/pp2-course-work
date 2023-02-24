03954cc289c918bf62e5d68a4fc26aa6cee82316
Elnar Ashrafov, ID - 16260 <eashrafov16260>
2023-02-24 11:20:53 +0400

    Task 7

---
+++ Week05/Task7/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int m[] = {1, 2,3, 4,5, 6};
+        double sum = MathDemo.mean(m);
+        int fact = MathDemo.factorial(0);
+        System.out.println(fact);
+    }
+}
