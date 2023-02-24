9be8d11ec00f958bbe307d777b1bc6926869c8c0
Nazrin Muradova 16062 <nnmuradova16062@ada.edu.az>
2023-02-24 10:07:30 +0400

    Add file

---
+++ Week05/Inheritance.java
@@ -0,0 +1,58 @@
+public class Inheritance{
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String gender;
+
+    public Person(String firstName, String lastName, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+  
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+        if (!(object  instanceof Person)) {
+            return false;
+        }
+        Person p = (Person) object;
+        return this.firstName.equals(p.firstName) && this.lastName.equals(p.lastName) && this.gender.equals(p.gender);
+    }
+}
+}   
