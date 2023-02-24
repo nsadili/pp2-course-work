087161f9c327595073006539be02457d45341a7e
Aliya Zulfugarli 13080 <azulfugarli13080@ada.edu.az>
2023-02-24 13:23:12 +0400

    Added my java files

---
+++ Week05/ex/MathDemo.java
@@ -0,0 +1,31 @@
+public class MathDemo {
+    
+    public int min(int a, int b) {
+        return Math.min(a, b);
+    }
+    
+    public int max(int a, int b) {
+        return Math.max(a, b);
+    }
+    
+    public int sum(int[] args) {
+        int sum = 0;
+        for (int arg : args) {
+            sum += arg;
+        }
+        return sum;
+    }
+    
+    public float mean(int[] args) {
+        int sum = sum(args);
+        return (float) sum / args.length;
+    }
+    
+    public int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
+
