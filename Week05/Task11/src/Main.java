4991cf6e8a0b5433e59283b11081da5f12742d85
Elnar Ashrafov, ID - 16260 <eashrafov16260>
2023-02-26 09:13:22 +0400

    Task 11

---
+++ Week05/Task11/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+         Rectangle r1 = new Rectangle(5, 10);
+         Rectangle r2 = new Rectangle(15, 10);
+         Rectangle r3 = new Rectangle(5, 10);
+        System.out.println(r1.equals(r2));
+        System.out.println(r1.equals(r3));
+    }
+}
---
+++ Week05/Task11/src/Rectangle.java
@@ -0,0 +1,34 @@
+public class Rectangle {
+    private int width;
+    private int height;
+
+    public Rectangle (int w, int h){
+      this.width = w;
+      this.height = h;
+    }
+
+    public boolean equals(Object obj){
+        Rectangle rect = (Rectangle) obj;
+        if(obj == null){
+            return false;
+        }else{
+            return rect.width == width && rect.height == height;
+        }
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
