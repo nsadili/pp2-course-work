db77d974be7934ada18d7b850ac22f3a67289995
Aliya Zulfugarli 13080 <azulfugarli13080@ada.edu.az>
2023-02-26 19:18:05 +0400

    Added my java files

---
+++ Week05/ex/EX2.java
@@ -0,0 +1,43 @@
+public class EX2 {
+    class A {
+      // static {
+      // System.out.println("In static init block of A");
+      // }
+  
+      public A() {
+        System.out.println("In constructor A.");
+      }
+  
+      {
+        System.out.println("In instance init block of A");
+      }
+    }
+  
+    class B extends A {
+      // static {
+      // System.out.println("In static init block of B");
+      // }
+  
+      public B() {
+        System.out.println("In constructor B.");
+      }
+  
+      {
+        System.out.println("In instance init block of B");
+      }
+    }
+  
+    public static class Main2 {
+      public static void main(String[] args) {
+        // B b = new B();
+      }
+    }
+  }
+  
+  // Output:
+  // In static init block of A
+  // In instance init block of A
+  // In constructor A.
+  // In static init block of B
+  // In instance init block of B
+  // In constructor B.
