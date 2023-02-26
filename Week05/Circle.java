213851ce39aafd1271b2d22160aa341919bd9230
Toghrul-ul <119342532+Toghrul-ul@users.noreply.github.com>
2023-02-26 22:16:42 +0400

    Extend circle

---
+++ Week05/Circle.java
@@ -0,0 +1,22 @@
+public class Circle extends Rectangle{
+    private int radius;
+
+    public Circle(int m) {
+        super(m, m);
+        this.radius = m;
+    }
+
+    @Override
+    public double getArea(){
+        return  (2 * (Math.PI * Math.pow(this.radius, 2)));
+    }
+
+    public double Length(){
+        return 2 * Math.PI * this.radius;
+    }
+
+    @Override
+    public String toString(){
+        return "[Radius : " + this.radius + "]";
+    }
+}
---
+++ Week05/Square.java
@@ -0,0 +1,17 @@
+public class Square extends Rectangle{
+    public int side;
+
+    public Square(int s){
+       super(s, s);
+    }
+
+    public int getSide() {
+        return this.side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+
+    }
