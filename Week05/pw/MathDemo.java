1f50ea7ec765c7a39f31ca2a1d1ff3c6eb1c0134
Aliya Zulfugarli 13080 <azulfugarli13080@ada.edu.az>
2023-02-26 19:28:51 +0400

    Added my java files

--- Week05/pw/MathDemo.java
+++ Week05/pw/MathDemo.java
@@ -1,31 +1,44 @@
 public class MathDemo {
-    
-    public int min(int a, int b) {
-        return Math.min(a, b);
-    }
-    
-    public int max(int a, int b) {
-        return Math.max(a, b);
-    }
-    
-    public int sum(int[] args) {
-        int sum = 0;
-        for (int arg : args) {
-            sum += arg;
-        }
-        return sum;
-    }
-    
-    public float mean(int[] args) {
-        int sum = sum(args);
-        return (float) sum / args.length;
-    }
-    
-    public int factorial(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        return n * factorial(n - 1);
-    }
-}
 
+
+  public static int min(int a, int b) {
+    return Math.min(a, b);
+  }
+
+ 
+  public static int max(int a, int b) {
+    return Math.max(a, b);
+  }
+
+ 
+  public static int sum(int[] args) {
+    int result = 0;
+    for (int arg : args) {
+      result += arg;
+    }
+    return result;
+  }
+
+  
+  public static float mean(int[] args) {
+    if (args.length == 0) {
+      return 0;
+    }
+    int sum = sum(args);
+    return (float) sum / args.length;
+  }
+
+  
+  public static int factorial(int n) {
+    if (n < 0) {
+      System.out.println("Factorial is undefined for negative numbers.");
+    }
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+
+  }
+  
+}
