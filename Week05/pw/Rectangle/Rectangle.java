6af8ea4ddb48b0af0d6c7083b4d18e95a10f7368
rufat_hajizada_16231 <rufeth04@gmail.com>
2023-02-28 00:02:01 +0400

    new pw

---
+++ Week05/pw/Rectangle/Rectangle.java
@@ -0,0 +1,15 @@
+package pw.Rectangle;
+
+public class Rectangle {
+        int width, height;
+        
+        public Rectangle(int w, int h) {
+        width = w;
+        height = h;
+        }
+        
+        public boolean equals(Object obj) {
+        Rectangle rect = (Rectangle) obj;
+        return (this.width==rect.width) && (this.height==rect.height);
+        }
+}
