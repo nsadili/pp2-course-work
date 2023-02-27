a6bce90e1068b7a38ba00ba8032d860b422f2669
Alisadiq Shukurov 14234 <ashukurov14234@ada.edu.az>
2023-02-27 07:31:55 -0800

    CustomerAdded

---
+++ Week05/Exercises/Customer.java
@@ -0,0 +1,36 @@
+package Week05.Exercises;
+
+public class Customer {
+    private int ID;
+    private String name;
+    private char gender;
+
+    public Customer(int ID, String name, char gender) {
+        this.ID = ID;
+        this.name = name;
+        this.gender = gender;
+    }
+    public int getID() {
+        return this.ID;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public char getGender() {
+        return this.gender;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + "(" + getID() + ")";
+    }
+
+    public static void main(String[] args) {
+        Customer customer=new Customer(14234,"Alisadiq",'M');
+        System.out.println( customer.toString());
+    }
+}
+
+
