7336fdae2f3ae2f246f7d0e8c71ab373ee0039a8
Alisadiq Shukurov 14234 <ashukurov14234@ada.edu.az>
2023-02-27 07:51:57 -0800

    Rectangle added

---
+++ Week05/Exercises/Rectangle.java
@@ -0,0 +1,24 @@
+package Week05.Exercises;
+
+public class Rectangle {
+    int width, height;
+
+    public Rectangle(int w, int h) {
+        this.width = w;
+        this.height = h;
+    }
+
+    public boolean equals(Object obj) {
+        Rectangle rect = (Rectangle) obj;
+        return rect.height == this.height && rect.width == this.width;
+
+    }
+
+    public static void main(String[] args) {
+        Rectangle r1=new Rectangle(5,10);
+        Rectangle  r2=new Rectangle(15,10);
+        Rectangle r3=new Rectangle(5,10);
+        System.out.println(r1.equals(r2)); //false
+        System.out.println(r1.equals(r3)); //true
+    }
+}
