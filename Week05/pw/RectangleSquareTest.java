df134530552d601c3782f167e36291072d1d9d68
Naik Pashayev 13148 <naikpasha1994@gmail.com>
2023-02-27 22:08:09 +0400

    Added my files

---
+++ Week05/pw/RectangleSquareTest.java
@@ -0,0 +1,13 @@
+package Week05.pw;
+
+public class RectangleSquareTest {
+    public static void main(String[] args){
+        Object o1 = new Rectangle(5,10);
+        Object o2 = new Rectangle(15, 15);
+        Object o3 = new Square(15);
+    
+        System.out.println("Objects are identical: " + o1.equals(o2));
+        System.out.println("Objects are identical: " + o1.equals(o1));
+        System.out.println("Objects are identical: " + o2.equals(o3));
+    }
+}
