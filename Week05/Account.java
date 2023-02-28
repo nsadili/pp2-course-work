ba105cdd8406e81a50f6b03a9598ee82e60cb4c7
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-02-28 23:03:38 +0400

    Add files via upload

---
+++ Week05/Account.java
@@ -0,0 +1,54 @@
+public class Account {
+    private int id;
+    private Customer customer;
+    private double balance;
+
+    public Account(int id, Customer customer, double balance) {
+        this.id = id;
+        this.customer = customer;
+        this.balance = balance;
+    }
+
+    public Account(int id, Customer customer) {
+        this.id = id;
+        this.customer = customer;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String toString() {
+        return customer.toString() + " balance = $" + String.format("%.2f", balance);
+    }
+
+    public String getCustomerName() {
+        return customer.getName();
+    }
+
+    public Account deposit(double amount) {
+        balance += amount;
+        return this;
+    }
+
+    public Account withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Amount withdrawn exceeds the current balance!");
+        }
+        return this;
+    }
+}
