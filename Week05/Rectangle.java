f550b9d6a397d36a67115a1bd95374043b1dc26f
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-02-28 23:34:37 +0400

    Add files via upload

---
+++ Week05/Rectangle.java
@@ -0,0 +1,31 @@
+public class Rectangle {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+
+        if (!(obj instanceof Rectangle)) {
+            return false;
+        }
+
+        Rectangle other = (Rectangle) obj;
+        return this.width == other.width && this.height == other.height;
+    }
+
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(5, 10);
+        Rectangle r2 = new Rectangle(15, 10);
+        Rectangle r3 = new Rectangle(5, 10);
+
+        System.out.println(r1.equals(r2)); // false
+        System.out.println(r1.equals(r3)); // true
+    }
+}
