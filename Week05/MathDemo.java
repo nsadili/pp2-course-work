0858564356ca56d2c1139a6fd3ba7e92275fb926
vmammadov16238 <118852797+vmammadov16238@users.noreply.github.com>
2023-02-28 04:48:40 +0400

    Week 05 Ex 7

--- Week05/MathDemo.java
+++ Week05/MathDemo.java
@@ -5,17 +5,17 @@ public class MathDemo {
     public static int min(int a, int b){
         return (a < b) ? a : b;
     }
-    public int max(int a, int b){
+    public static int max(int a, int b){
         return (a > b) ? a : b;
     }
-    public int sum(int[] args){
+    public static int sum(int[] args){
         int sum = 0;
         for(int x: args){
             sum += x;
         }
         return sum;
     }
-    public float mean(int[] args){
+    public static float mean(int[] args){
         int sum = 0;
         for(float x: args){
             sum += x;
@@ -23,7 +23,7 @@ public class MathDemo {
         float mean = sum/args.length; 
         return mean;
     }
-    public int factorial(int n){
+    public static int factorial(int n){
         int fac = 1;;
         for(int i = 1; i<=n; i++){
             fac *=i;
--- Week05/TestEx7.java
+++ Week05/TestEx7.java
@@ -1,5 +1,10 @@
 public class TestEx7 {
     public static void main(String[] args) {
+        int[] m = {1, 2, 3, 4};
         System.out.println(MathDemo.min(1, 2));
+        System.out.println(MathDemo.max(1, 2));
+        System.out.println(MathDemo.sum(m));
+        System.out.println(MathDemo.mean(m));
+        System.out.println(MathDemo.factorial(5));
     }
 }
