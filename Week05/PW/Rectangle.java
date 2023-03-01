95f8c18148ec3c44b9a48c5600cd0bfce6037161
Laman Jafarli 18018 <ljafarli18018@ada.edu.az>
2023-03-01 15:54:31 +0400

    .\PW\

---
+++ Week05/PW/Rectangle.java
@@ -0,0 +1,36 @@
+public class Rectangle {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Rectangle other = (Rectangle) obj;
+        return this.width == other.width && this.height == other.height;
+    }
+
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(5, 10);
+        Rectangle r2 = new Rectangle(15, 10);
+        Rectangle r3 = new Rectangle(5, 10);
+
+        System.out.println(r1.equals(r2));
+        System.out.println(r1.equals(r3));
+    }
+}
