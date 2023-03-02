957d00220b5c19ac3608bae39102c060dd1c387a
RashadHummatov85 <118662630+RashadHummatov85@users.noreply.github.com>
2023-03-02 14:06:31 +0400

    Add files via upload

---
+++ Week05/Main.java
@@ -0,0 +1,27 @@
+package Week05.EX.Ex08;
+
+public class Main {
+    public static void main(String[] args) {
+        // create a Person object
+        Person person = new Person("Anar", "Hummatov", "Male");
+        System.out.println(person);
+
+        // create a Teacher object
+        Teacher teacher = new Teacher("Nuraddin", "Sadili", "Male", "SITE", new String[] {"Programming", "Principles II"});
+        System.out.println(teacher);
+
+        // create a Student object
+        Student student = new Student("Someone", "someone", "Male", "1111");
+        System.out.println(student);
+
+        // create a PhdStudent object
+        PhdStudent phdStudent = new PhdStudent("*****", "*****", "*****", "*****", new String[] {"*****", "*****"}, "*****");
+        System.out.println(phdStudent);
+
+        // test equality
+        System.out.println(person.equals(teacher)); // should be false
+        System.out.println(student.equals(phdStudent)); // should be false
+        System.out.println(phdStudent.equals(new PhdStudent("*****", "*****", "*****", "*****", new String[] {"*****", "*****"}, "*****")));// should be true
+    }
+}
+
