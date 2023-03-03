01472fde6caf5b138129c11c7a81f6b0f73e9a7c
niluferbabayeva <118837646+niluferbabayeva@users.noreply.github.com>
2023-03-03 20:58:46 +0400

    Delete PhdStudent.java

--- Week05/exs/PhdStudent.java
+++
@@ -1,8 +0,0 @@
-package exs;
-
-public class PhdStudent extends Student {
-    private String department;
-    private List<String> courses;
-
-    public PhdStudent(String firstName, String lastName, String gender, int studentId, String department, List<String> courses) {
-        super(firstName, lastName, gender,
