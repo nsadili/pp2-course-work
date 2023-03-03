d94345c781e29c53a8741990f3727d5e0083b052
Samad045 <118782326+Samad045@users.noreply.github.com>
2023-03-03 11:05:19 +0400

    Update Seven.java

---
+++ Week05/7PW/Seven.java
@@ -0,0 +1,39 @@
+public class MathDemo {
+// If they are static, then we can use them without creating an object which helps us to use them whenever we want in other classes
+    public static int min(int a, int b){
+        return (a<b) ? a : b;
+    }
+    public static int max(int a, int b){
+        return (a > b) ? a : b;
+    }
+
+    public static int sum(int[] args){
+        int sum = 0;
+        for(int i: args){
+            sum+= i;
+        }
+        return sum;
+    }
+
+    public static float mean(int[] args){
+        int sum = 0;
+        for(int i: args){
+            sum += i;
+        }
+        return (float)sum /args.length;
+    }
+
+    public static int factorial(int n){
+        int f;
+        if(n == 0){
+           f = 1;
+        }else {
+            f = 1;
+            for (int i = 1; i <= n; i++) {
+                f *= i;
+            }
+        }
+        return f;
+    }
+
+}
