f637e5c429a16caead29c0fdc871ff20864f75c9
Toghrul-ul <119342532+Toghrul-ul@users.noreply.github.com>
2023-03-05 22:41:44 +0400

    Week 6 Square.java

---
+++ Week06/Square.java
@@ -0,0 +1,13 @@
+package Week06;
+
+public class Square extends Rectangle {
+
+ public Square(){}
+    public Square(double side) {
+            super(side, side);
+}
+
+     public Square(double side, String color, boolean filled) {
+    super(side, side, color ,filled);
+}
+}
