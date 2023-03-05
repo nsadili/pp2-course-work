75a7c335d4a93e547e07db1402a71d48063ef679
Tofiq041 <119070294+Tofiq041@users.noreply.github.com>
2023-03-05 23:00:15 +0400

    Create Movable.java

---
+++ Week06/Movable.java
@@ -0,0 +1,14 @@
+package Week06;
+
+public interface Movable {
+    public void moveUp();
+
+    
+    public void moveDown();
+    
+    
+    public void moveLeft();
+    
+    
+    public void moveRight();
+}
