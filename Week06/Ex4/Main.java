209dea7807f1ca9ae39f3e6ab626288b5cb0fba6
Garib Guluzada 16067 <gguluzada16067@ada.edu.az>
2023-03-06 19:23:18 +0400

    HalfOfWeek06

---
+++ Week06/Ex4/Main.java
@@ -0,0 +1,15 @@
+package Week06.Ex4;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Object r1 = new Rectangle(5, 10);
+        Object r2 = new Rectangle(10, 10);
+        Object r3 = new Square(10);
+
+        System.out.println(r1.equals(r2));
+        System.out.println(r1.equals(r3));
+        System.out.println(r2.equals(r3));
+        
+    }
+}
