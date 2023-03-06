469793d5b58b9c1afb0bc2b9e7c3489a2c58d4d1
ravanabdinli <118709878+ravanabdinli@users.noreply.github.com>
2023-03-06 22:18:04 +0400

    Create Circle.java

---
+++ Week06/week 6/Circle.java
@@ -0,0 +1,30 @@
+public class Circle implements Movable {
+    private Point center;
+    private double radius;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public void moveUp() {
+        center.moveUp();
+    }
+
+    public void moveDown() {
+        center.moveDown();
+    }
+
+    public void moveLeft() {
+        center.moveLeft();
+    }
+
+    public void moveRight() {
+        center.moveRight();
+    }
+
+    @Override
+    public String toString() {
+        return "Circle [center=" + center + ", radius=" + radius + "]";
+    }
+}
