b3aae7d0b239701522ff08ce1e7d948732158032
Samad Ganbarov, 16014 <sganbarov16014@ada.edu.az>
2023-03-06 19:07:05 +0400

    Task Eight

---
+++ Week06/Task8/src/Factorial.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Factorial {
+    public static class Main {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter the number ");
+            int m = scan.nextInt();
+            BigInteger b = new BigInteger("1");
+            for(int i = 1; i <= m; i++){
+                b = b.multiply(new BigInteger(String.valueOf(i)));
+            }
+            System.out.println(b + " is factorial of " + m);
+        }
+    }
+}
---
+++ Week06/Task8/src/Power.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+public class Power {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please, enter the number and its power respectively: 1");
+        int num = scan.nextInt();
+        int pow = scan.nextInt();
+        System.out.println(powerOf(num, pow).toString());
+    }
+
+    public static  BigInteger powerOf(int num, int pow){
+        BigInteger res = BigInteger.valueOf(num);
+        for(int i = 1; i < pow; i++){
+            res = res.multiply(BigInteger.valueOf(num));
+        }
+
+        return res;
+    }
+}
