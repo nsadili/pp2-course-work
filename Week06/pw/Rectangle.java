5d29ee02165c4129a1e866d033113bc02f6e4997
NIHAT ZEYNALOV <nzeynalov16263@ada.edu.az>
2023-03-07 23:38:59 +0400

    week6

---
+++ Week06/pw/Rectangle.java
@@ -0,0 +1,13 @@
+class Rectangle {
+    int width, height;
+
+    public Rectangle(int w, int h) {
+    width = w;
+    height= h;
+    }
+    public boolean equals(Object obj) {
+        Rectangle rect = (Rectangle) obj;
+        return false;
+    }
+}
+
---
+++ Week06/pw/Square.java
@@ -0,0 +1,8 @@
+public class Square {
+    public Square(int i) {
+    }
+    Object o1 = new Rectangle(5,10);
+    Object o2 =new Rectangle(15,15);
+    Object o3 = new Square(15);
+    
+}
