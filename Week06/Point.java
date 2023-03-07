6ddfa9d5b010d5dd293945f294a18cac3a5aa64c
Ulkar Ahmadli 17458 <uahmadli17458@ada.edu.az>
2023-03-07 21:27:29 +0400

    Submit Exercise 06

---
+++ Week06/Point.java
@@ -0,0 +1,54 @@
+package week06;
+
+import java.util.Objects;
+
+public class Point implements Cloneable {
+    private float x;
+    private float y;
+
+    public Point() {
+        this(0.0f, 0.0f);
+    }
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) {
+        this(p.x, p.y);
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" + "x=" + x + ", y=" + y + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Point point = (Point) obj;
+        return point.x == x && point.y == y;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ Week06/Segment.java
@@ -0,0 +1,44 @@
+package week06;
+
+public class Segment {
+    private Point p1;
+    private Point p2;
+
+    public Segment(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Segment(float x1, float y1, float x2, float y2) {
+        this(new Point(x1, y1), new Point(x2, y2));
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public void setP1(Point p1) {
+        this.p1 = p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+    public void setP2(Point p2) {
+        this.p2 = p2;
+    }
+
+    @Override
+    public String toString() {
+        return "Segment{" + "p1=" + p1 + ", p2=" + p2 + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Segment segment = (Segment) o;
+        return p1.equals(segment.p1) && p2.equals(segment.p2);
+    }
+}
