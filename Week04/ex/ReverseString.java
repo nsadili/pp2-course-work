9614f30f83c2921e90a8068724ca5d8a791d0b3f
Ziba Mursalova 16192 <zmursalova16192@ada.edu.az>
2023-03-09 11:23:34 +0400

    Tasks

--- Week04/ex/ReverseString.java
+++ Week04/ex/ReverseString.java
@@ -1,7 +1,7 @@
 public class ReverseString {
     public static void main(String[] args) {
-        String test = "sunset";      
-        reverse(test);         
+        String sb = "Sunset"; //StringBuilder sb = new StringBuilder("Sunset");    
+        reverse(sb);         
     }
     public static void reverse(String s){
      for(int i = s.length() - 1 ; i >= 0 ; i--) {
