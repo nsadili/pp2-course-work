3b21023f0262c716c013b5e71a682819fe9ed556
Ziba Mursalova 16192 <zmursalova16192@ada.edu.az>
2023-03-09 21:40:32 +0400

    Completed tasks

---
+++ Week04/ex/Practice.java
@@ -0,0 +1,9 @@
+public class Practice {
+    public static void main(String[] args) {
+        
+    
+    String text = "Programming";
+    System.out.println("The 5th character: "+text.charAt(5));
+
+}
+}
---
+++ Week04/ex/StringReverse.java
@@ -0,0 +1,18 @@
+public class StringReverse {
+    String reverse(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--)
+        sb.append(str.charAt(i));
+
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        StringReverse sr = new StringReverse();
+
+        System.out.println(sr.reverse("Hello"));
+        System.out.println(sr.reverse("I love Java"));
+
+    }
+
+}
