949d2257fae53e5c98e073788b68cfd2281397dd
rufat_hajizada_16231 <rufeth04@gmail.com>
2023-03-10 12:36:17 +0400

    Invoice added

---
+++ Week07/Week 07 - Incomplete/Invoice.java
@@ -0,0 +1,62 @@
+public class Invoice {
+    private String partNum;
+    private String partDesc;
+    private int n;
+    private double prc;
+
+    public Invoice(){
+        n=0;
+        prc=0.0;
+    }
+
+    public Invoice(String partNum, String partDesc, int n, double prc){
+        if (n<0) throw new IllegalArgumentException ("Quantity cannot be negative!");;
+        if (prc<0) throw new IllegalArgumentException ("Price cannot be negative!");;
+        this.partNum=partNum;
+        this.partDesc=partDesc;
+        this.n=n;
+        this.prc=prc;
+    }
+
+    public String getPartNum() {
+        return partNum;
+    }
+
+    public void setPartNum(String partNum) {
+        this.partNum = partNum;
+    }
+
+    public String getPartDesc() {
+        return partDesc;
+    }
+
+    public void setPartDesc(String partDesc) {
+        this.partDesc = partDesc;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        if (n<0) throw new IllegalArgumentException ("Quantity cannot be negative!");
+        this.n = n;
+    }
+
+    public double getPrc() {
+        return prc;
+    }
+
+    public void setPrc(double prc) {
+        if (prc<0) throw new IllegalArgumentException ("Price cannot be negative!");
+        this.prc = prc;
+    }
+
+    public double getInvoiceAmount(){
+        return this.n*this.prc;
+    }
+}
+
+
+
+
---
+++ Week07/Week 07 - Incomplete/InvoiceTest.java
@@ -0,0 +1,21 @@
+public class InvoiceTest {
+
+    public static void main (String[] args){
+
+        try {
+            Invoice i1 = new Invoice("N12453", "Processor AMD Ryzen 5700X 512GB", -5, -279.99);
+            Invoice i2 = new Invoice("N13627", "SSD 512GB", 3, 99.99);
+            System.out.printf("Item 1\nPart Number: %s\nPart Description: %s\nQuantity: %d\nPrice %f\n\n\n",
+            i1.getPartNum(), i1.getPartDesc(), i1.getN(), i1.getPrc());
+        
+            System.out.printf("Item 2\nPart Number: %s\nPart Description: %s\nQuantity: %d\nPrice %f\n\n\n",
+            i2.getPartNum(), i2.getPartDesc(), i2.getN(), i2.getPrc());
+    
+    
+             System.out.println("Invoice amount of item 1: " + i1.getInvoiceAmount());
+             System.out.println("Invoice amount of item 2: " + i2.getInvoiceAmount());
+        } catch (Exception e) {
+            System.out.println("Please, provide valid input values!");
+        }
+    }
+}
