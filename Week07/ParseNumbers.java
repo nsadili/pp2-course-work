916c242ba60fbf9dd9415e7f477ab8baff84730f
Shamil Abbasov 16084 <sabbasov16084@ada.edu.az>
2023-03-10 11:08:05 +0400

    ParseNumbers and Stack added

---
+++ Week07/ParseNumbers.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ParseNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);// reading from standard input
+
+        String line = scan.nextLine(), word = null;
+        scan.close();
+
+        scan = new Scanner(line); // reading from a String
+        int sum = 0, count = 0;
+        while (scan.hasNext()) { // while there is a token to be read from the string
+            word = scan.next();
+           try{
+            sum += Integer.parseInt(word); // read it and parse into Integer
+            count++;
+           } catch(NumberFormatException e){
+            //System.out.printf("<%s> is not number\n",word);
+           }
+            
+        }
+
+        scan.close();
+
+        // if (count == 0)
+        //     System.out.println("There are no VALID input provided!");
+        // else
+            System.out.printf("Sum = %d\nCount = %d\nAverage = %.3f\n", sum, count, count ==0 ? 0 : (float) sum / count);
+    }
+}
