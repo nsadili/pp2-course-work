c100f3e5b928670929075bc231d1b74e28dd9a7d
lamanasgarli <lasgarli17569@ada.edu.az>
2023-03-10 12:36:53 +0400

    add

---
+++ Week07/exercise1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class exercise1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] counts = null;
+        String word;
+
+        do {
+            counts = new int[26];
+            System.out.print("Enter a word, preferrably only english letters (0 to exit): ");
+            word = scan.nextLine();
+
+            word = word.toLowerCase();
+
+            if (word.trim().equals("0"))
+                break;
+
+            for (int i = 0; i < word.length(); i++)
+                counts[word.charAt(i) - 'a']++;
+
+            System.out.println("\tHere are the letters:");
+
+            for (int i = 0; i < counts.length; i++)
+                if (counts[i] != 0)
+                    System.out.println((char) (i + 'a') + ": " + counts[i]);
+
+        } while (true);
+
+        scan.close();
+    }
+}
