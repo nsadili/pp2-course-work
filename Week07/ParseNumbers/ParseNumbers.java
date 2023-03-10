e910db985289bc324387cf2836bd010116b9d5aa
Garib Guluzada 16067 <gguluzada16067@ada.edu.az>
2023-03-11 00:39:05 +0400

    AllOfPW.Week07

---
+++ Week07/ParseNumbers/ParseNumbers.java
@@ -0,0 +1,25 @@
+package Week07.ParseNumbers;
+import java.util.Scanner;
+
+public class ParseNumbers {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a sequence of numbers separated by commas: ");
+        String input = scan.nextLine();
+
+        String[] numbers = input.split(",");
+
+        int sum = 0;
+        for (String number : numbers) {
+            try {
+                int n = Integer.parseInt(number);
+                sum += n;
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        
+        System.out.println("The sum of the numbers is: " + sum);
+    }
+}
