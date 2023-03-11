dcc2344b5fb6bef7b2abc404f35f60bdd4d66e5a
Aliya Zulfugarli 13080 <azulfugarli13080@ada.edu.az>
2023-03-11 21:50:18 +0400

    Added my java files

---
+++ Week07/ex/CountLetters.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class CountLetters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(countLetters(str));
+    }
+
+    public static int countLetters(String str) {
+        int count = 0;
+        char[] array = str.toCharArray();
+        for (char x: array) {
+            if (Character.isAlphabetic(x)) count++;
+        }
+        return count;
+    }
+}
