756fff5ffd3f603948c1c1e4f48eb27ba7473b10
Behruz Gurbanli <bgurbanli14129@ada.edu.az>
2023-03-11 12:06:26 +0400

    ParseNumbers.java Modified

--- Week07/ParseNumbers.java
+++ Week07/ParseNumbers.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ParseNumbers {
 
     public static void main(String[] args) {
-        String str = "1,2,3,4,5";
+        String str = "";
         List<Integer> numbers = parseNumbers(str);
         System.out.println(numbers);
     }
@@ -14,12 +14,13 @@ public class ParseNumbers {
     public static List<Integer> parseNumbers(String str) {
         List<Integer> numbers = new ArrayList<>();
         String[] strNumbers = str.split(",");
-        for (String s : strNumbers) {
+        int i = 0;
+        for (String s: strNumbers) {
             try {
                 int num = Integer.parseInt(s.trim());
                 numbers.add(num);
             } catch (NumberFormatException e) {
-                // ignore non-numeric strings
+                System.err.println("(" + s + ")" + " contain non-numeric values which get ignored.");
             }
         }
         return numbers;
