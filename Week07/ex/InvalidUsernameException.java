4ac87db70ac2402e6dc3bcf4b6ce177c95ccca4a
roya alizada 16092 <ralizada16092@ada.edu.az>
2023-03-13 01:10:56 +0400

    ex

---
+++ Week07/ex/InvalidUsernameException.java
@@ -0,0 +1,9 @@
+public class InvalidUsernameException extends Exception{
+    public InvalidUsernameException() {
+        super();
+    }
+
+    public InvalidUsernameException(String message) {
+        super(message);
+    }
+}
