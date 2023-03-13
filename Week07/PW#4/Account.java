d10117055a7ec7078fad717bc6b328d538636c6b
ravanabdinli <118709878+ravanabdinli@users.noreply.github.com>
2023-03-14 00:02:06 +0400

    Create Account.java

---
+++ Week07/PW#4/Account.java
@@ -0,0 +1,28 @@
+public class Account {
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            throw new InvalidAmountException("Withdrawal amount cannot be greater than the balance");
+        }
+        balance -= amount;
+    }
+}
