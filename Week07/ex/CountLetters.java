6d171c64998c9377c644daa0ce648e402f6bcf7d
Fikrat Rustamli 16058 <frustamli16058@ada.edu.az>
2023-03-13 23:42:19 +0400

    Add week 07 ex

---
+++ Week07/ex/CountLetters.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class CountLetters {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] counts = null;
+        String word;
+
+        do {
+            counts = new int[26];
+            System.out.print("Enter a word, preferrably only english letters (0 to exit): ");
+            word = scan.nextLine();
+
+            word = word.toLowerCase();
+
+
+            if (word.trim().equals("0"))
+                break;
+
+                try{
+
+            for (int i = 0; i < word.length(); i++)
+                counts[word.charAt(i) - 'a']++;
+                }
+                catch(ArrayIndexOutOfBoundsException e) {
+                    System.out.printf("<%s> is an invalid word!\n", word);
+
+
+                }
+            System.out.println("\tHere are the letters:");
+
+            for (int i = 0; i < counts.length; i++)
+                if (counts[i] != 0)
+                    System.out.println((char) (i + 'a') + ": " + counts[i]);
+
+        } while (true);
+
+        scan.close();
+    }
+}
