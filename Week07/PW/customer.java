a59c5ba76f652a0bf1103609a5763237bfba699a
Laman Jafarli 18018 <ljafarli18018@ada.edu.az>
2023-03-16 13:40:54 +0400

    .\PW\

--- Week07/PW/customer.java
+++ Week07/PW/customer.java
@@ -1,4 +1,22 @@
-
 public class customer {
-
+    private String name;
+    private int ID;
+    private char gender;
+}
+public customer(String name, int ID, char gender) {
+    this.name = name;
+    this.ID = ID;
+    this.gender = gender;
+}
+public String getName() {
+    return name;
+}
+public int getID() {
+    return ID;
+}   
+public char getGender(){
+    return gender;
+}
+public String toString() {
+    return this.name +"(" + this.ID +")";
 }
