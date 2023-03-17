19d64a485890bf02c63a2cbad09f9e4fbbf8daf8
nibrahimli16079 <nibrahimli16079@ada.edu.az>
2023-03-17 12:29:36 +0400

    Create EX1.java

---
+++ Week08/ex1/EX1.java
@@ -0,0 +1,63 @@
+package Week08.ex1;
+import java.util.*;
+
+public class EX1 {
+    public static void main(String[] args) {
+        // Part a: Create a list of integers and add elements to it
+        List<Integer> intList = new ArrayList<>();
+        intList.add(10);
+        intList.add(20);
+        intList.add(30);
+
+        // Part b: Print the elements using various methods
+        System.out.println("Using toString() method: " + intList.toString());
+
+        System.out.println("Using for statement:");
+        for (int i = 0; i < intList.size(); i++) {
+            System.out.println(intList.get(i));
+        }
+
+        System.out.println("Using enhanced for statement:");
+        for (int element : intList) {
+            System.out.println(element);
+        }
+
+        System.out.println("Using iterator:");
+        Iterator<Integer> intIterator = intList.iterator();
+        while (intIterator.hasNext()) {
+            System.out.println(intIterator.next());
+        }
+
+        // Part c: Create a list of Strings, add elements, and manipulate the list
+        List<String> shoppingList = new ArrayList<>();
+        shoppingList.add("coffee");
+        shoppingList.add("tea");
+        shoppingList.add("bread");
+        shoppingList.add("butter");
+        shoppingList.add("eggs");
+
+        System.out.println("Using toString() method: " + shoppingList.toString());
+
+        System.out.println("Using for statement:");
+        for (int i = 0; i < shoppingList.size(); i++) {
+            System.out.println(shoppingList.get(i));
+        }
+
+        System.out.println("Using enhanced for statement:");
+        for (String element : shoppingList) {
+            System.out.println(element);
+        }
+
+        System.out.println("Using iterator:");
+        Iterator<String> stringIterator = shoppingList.iterator();
+        while (stringIterator.hasNext()) {
+            System.out.println(stringIterator.next());
+        }
+
+        shoppingList.remove("bread");
+        System.out.println("After removing 'bread': " + shoppingList.toString());
+
+        shoppingList.remove(2);
+        System.out.println("After removing the element at index 2: " + shoppingList.toString());
+    }
+}
