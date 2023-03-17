ab997dfc5aa02c87e28a956bea06e413b9a21507
Fuad2005 <fuadqasimov000@gmail.com>
2023-03-17 11:45:39 +0400

    stack error fixed

---
+++ Week07/src/StackTest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class StackTest {
+        public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+
+    }
+}
+
