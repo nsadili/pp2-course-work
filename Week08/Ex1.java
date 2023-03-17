1439618a651fed7f49cfdce5ca3ef406f40fefc7
Ilqar Malikov13943 <102352450+imalikov13943@users.noreply.github.com>
2023-03-17 17:14:44 +0400

    Exercise 1

---
+++ Week08/Ex1.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Ex1{
+    public static void main(String... args) {
+        List<Integer> nums = new ArrayList<>();
+
+        nums.add(10);
+        nums.add(5);
+        nums.add(3);
+        nums.add(1);
+
+        //System.out.println(nums);
+
+        //for(int i=0; i<nums.size(); i++)
+        //    System.out.print(nums.get(i)+ " ");
+        //System.out.println();
+
+        /*for(Integer el : nums){
+            el = el + 10;
+            System.out.print(el+ " ");
+         }
+        System.out.println();
+        System.out.println(nums); */
+
+        Iterator<Integer> it = nums.iterator();
+
+        if(!it.hasNext())
+            System.out.println("[]");
+
+        while(it.hasNext()){
+            System.out.println(it.next() + " ");
+        }
+        System.out.println();
+    }
+
+}
