273a90f7f580c1354f9054dcc203905a6bd52ec4
Shamil Abbasov 16084 <sabbasov16084@ada.edu.az>
2023-03-18 15:06:05 +0400

    QueueDemo added

---
+++ Week08/QueueDemo.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class QueueDemo {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> line = new LinkedList<>();
+
+        line.offer(3);
+        line.offer(4);
+        line.offer(7);
+        line.offer(1);
+        line.offer(2);
+        System.out.println(line);
+
+        int amount;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount of bread: ");
+        amount = sc.nextInt();
+
+        var breadNum = 0;
+        var peopleBuyBread = 0;
+
+        while (line.size() != 0) {
+            breadNum += line.poll();
+            
+            if (breadNum > amount) {
+                break;
+            }
+
+            peopleBuyBread++;
+
+        }
+
+        System.out.println("Number of People Buy Bread: " + peopleBuyBread);
+
+    }
+
+}
