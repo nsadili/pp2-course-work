5ebbd2cae8423f03a90330cf6d4cb6845273c6ed
Tofiq041 <119070294+Tofiq041@users.noreply.github.com>
2023-03-19 15:52:35 +0400

    comparator 2

---
+++ Week08/ComparatorForExerciseTwo.java
@@ -0,0 +1,9 @@
+package Week08;
+
+import java.util.Comparator;
+
+public class ComparatorForExerciseTwo implements Comparator<Integer> {
+    public int compare(Integer num1, Integer num2) {
+        return num1.compareTo(num2);
+    }
+}
