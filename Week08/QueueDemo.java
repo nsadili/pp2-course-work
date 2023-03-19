5268f90f97fa2f1b18abc23be8807a46172cc0eb
Behruz Gurbanli <bgurbanli14129@ada.edu.az>
2023-03-19 15:01:30 +0400

    QueueDemo added.

---
+++ Week08/QueueDemo.java
@@ -0,0 +1,26 @@
+package Week08;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Give the number of pieces of bread per each person: ");
+        int breadPerEachPerson = scan.nextInt();
+
+        System.out.print("The amount of bread in the store: ");
+        int amountInTheStore = scan.nextInt();
+
+        Queue<Integer> breadQueue = new LinkedList<>();
+
+        for (int i = 0; i < amountInTheStore / breadPerEachPerson; i++) {
+            breadQueue.offer(breadPerEachPerson);
+        }
+
+        System.out.println("The number of people who will be able to buy bread is: " + breadQueue.size());
+        System.out.println("Left bread: " + amountInTheStore % breadPerEachPerson);
+    }
+}
