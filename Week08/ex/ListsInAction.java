0cadce5de7a35d8c6253f7cfe929c872d05d0501
Seljan Musayeva <smusayeva17601@ada.edu.az.com>
2023-03-20 19:33:27 +0400

    Added EX from Week08

---
+++ Week08/ex/ListsInAction.java
@@ -0,0 +1,34 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ListsInAction {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList=new ArrayList<>();
+
+        myList.add(9);
+        myList.add(7);
+        myList.add(3);
+        myList.add(1);
+
+        //System.out.println(myList.toString());
+
+        //for(int c=0; c<myList.size();c++)
+        //System.out.println(myList.get(c) +" ");
+       // System.out.println();
+
+          // for(int el : myList){
+         //el=10+el;
+         // System.out.println(el+" ");
+         //  }
+         //System.out.println();
+          //System.out.println(myList);
+
+
+Iterator<Integer> it =((ArrayList<Integer>)myList).iterator();
+while(it.hasNext()){
+    System.out.print(it.next()+ " ");
+}
+System.out.println();
+    }
+}
