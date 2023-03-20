a456f90a05900eef0d8aa5b25fdfb2256744573d
asger_huseynli <ahuseynli17683@ada.edu.az>
2023-03-20 16:57:12 +0400

    Asgar

---
+++ Week08/PW/SortingList.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortingList {
+
+    public static void main(String[] args) {
+
+        List<String> myList = new ArrayList<>();
+        myList.add("Asgar");
+        myList.add("Huseynli");
+        myList.add("Hello");
+
+        Collections.sort(myList);
+        System.out.println(myList);
+
+        myList.sort(null);
+        System.out.println(myList);
+
+
+        Collections.sort(myList, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                return s1.compareTo(s2);
+            }
+        });
+        System.out.println(myList);
+
+        Collections.sort(myList, new MyComparator());
+        System.out.println(myList);
+    }
+
+    static class MyComparator implements Comparator<String> {
+        public int compare(String s1, String s2) {
+            return s1.compareTo(s2);
+        }
+    }
+}
