6fecb8b361c3d2403759c2eb9541c2787a8bfaec
Elnur Ismayilzada <[eismaylzada16096@ada.edu.az]>
2023-03-21 14:14:03 +0400

    ex

---
+++ Week08/Account.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Account implements Comparable<Account> {
+    private String firstname;
+    private String lastname;
+    private double balance;
+
+    public Account(String firstname, String lastname, double balance) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.balance = balance;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public int compareTo(Account other) {
+        return (this.firstname + this.lastname).compareTo(other.firstname + other.lastname);
+    }
+
+    @Override
+    public String toString() {
+        return "[" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", balance=" + balance +
+                ']';
+    }
+
+    public static void main(String[] args) {
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(new Account("Vaqif", "Cavadov", 1000));
+        accounts.add(new Account("Arda", "Turan", 550));
+        accounts.add(new Account("Şamxal", "Şirinov", 1000));
+        accounts.add(new Account("Mohammed", "Elneny", 2500));
+
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
+        Collections.sort(accounts);
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+
+        Comparator<Account> balanceComparator = new Comparator<Account>() {
+            @Override
+            public int compare(Account account1, Account account2) {
+                if (account1.getBalance() < account2.getBalance()) {
+                    return -1;
+                } else if (account1.getBalance() > account2.getBalance()) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+        Collections.sort(accounts, balanceComparator);
+        System.out.println("\nSorted by balance using Comparator:");
+        for (Account account : accounts) {
+            System.out.println(account);
+        }
+    }
+}
