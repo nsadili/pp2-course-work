9b6756ba61a2e28e619dbcfc92e13421430baa04
Muaataz <118675903+MuaCodez30@users.noreply.github.com>
2023-03-21 18:00:44 +0400

    Updated (issues fixed)

--- Week02/ex/Calc.java
+++ Week02/ex/Calc.java
@@ -1,19 +1,26 @@
 public class Calc {
     public static void main(String [] args){
 
-        if (args.length < 2){
+        if (args.length != 2){
             System.out.println("please, enter two integers.");
             System.exit(1);
         }
 
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
 
-        System.out.println(args[0]);
-        System.out.println(args[1]);
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
 
-        var a = Integer.parseInt(args[0]);
-        var b = Integer.parseInt(args[1]);
 
-        System.out.println(a + b);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);;
 
     }
 
