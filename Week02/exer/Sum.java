e23075e97c8cce43b74a31c7be1653a70d2a2c15
Said Ahadov 17939 <said@saids-MacBook-Pro-2.local>
2023-03-25 16:20:46 +0400

    leifclb

--- Week02/exer/Sum.java
+++ Week02/exer/Sum.java
@@ -1,7 +1,9 @@
 public class Sum {
     public static void main (String [] args) {
-        int a = 1; long b = 1; float c = 2;
-        var sum = a+b+c;
+       // int a = 1; long b = 1; float c = 2;
+        float sum = (float) (1/5) * (float) (2/3) - (float) 3/10;
         System.out.println(sum);
+        System.out.print(sum);
+        System.out.printf(" %.1f", sum);
     }
 }
