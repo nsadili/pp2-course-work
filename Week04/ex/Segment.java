b396ba21ea4e74f6fd0c89004b8f6f31be74e456
Gülnar Hesenli <ghasanli@Gulnars-MacBook-Air.local>
2023-03-27 03:17:19 +0400

    adding

---
+++ Week04/ex/Segment.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Segment {
+
+    public Segment(double x1, double y1, double x2, double y2) {
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Segment[] seg = new Segment[n];
+        for (int i = 0; i < n; i++) {
+            double x1 = sc.nextDouble();
+            double x2 = sc.nextDouble();
+            double y1 = sc.nextDouble();
+            double y2 = sc.nextDouble();
+            seg[i] = new Segment(x1, y1, x2, y2);
+        }
+        double mn = Double.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (seg[i].length() < mn)
+                mn = seg[i].length();
+        }
+        System.out.println("min" + mn);
+        for (int i = 0; i < n; i++) {
+            if (seg[i].length() == mn)
+                System.out.println("segment: " + i + 1 + ", " + seg[i].toString());
+        }
+    }
+
+    private double length() {
+        return 0;
+    }
+}
