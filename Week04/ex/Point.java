add38543b1d026a4372ecc17658af375f90f357f
Gülnar Hesenli <ghasanli@Gulnars-MacBook-Air.local>
2023-03-27 01:44:26 +0400

    adding

---
+++ Week04/ex/Point.java
@@ -0,0 +1,18 @@
+
+public class Point {
+
+    public Point(int i, int j) {
+    }
+
+    public Point() {
+    }
+
+    public int getY() {
+        return 0;
+    }
+
+    public int getX() {
+        return 0;
+    }
+
+}
---
+++ Week04/ex/Points.java
@@ -0,0 +1,32 @@
+public class Points {
+    public static void main(String[] args) {
+
+        Point[] pts = new Point[] { new Point(), 
+                new Point(3, 4), 
+                new Point(-3, 5), 
+                new Point(-2, -1), 
+                new Point(-5, 0) 
+            };
+
+        Point west, east, north, south;
+
+        west = east = north = south = pts[0];
+
+        for (int i = 1; i < pts.length; i++) {
+            if (pts[i].getX() < west.getX())
+                west = pts[i];
+            if (pts[i].getX() > east.getX())
+                east = pts[i];
+
+            if (pts[i].getY() > north.getY())
+                north = pts[i];
+            if (pts[i].getY() < south.getY())
+                south = pts[i];
+        }
+
+        System.out.println("West: " + west);
+        System.out.println("East: " + east);
+        System.out.println("North: " + north);
+        System.out.println("South: " + south);
+    }
+}
