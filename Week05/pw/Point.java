a8457c4691a5dd7714cfe6b269c5aee215e4a932
Gülnar Hesenli <ghasanli@Gulnars-MacBook-Air.local>
2023-03-27 11:31:07 +0400

    adding

---
+++ Week05/pw/Point.java
@@ -0,0 +1,65 @@
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public String toString() {
+        return "Point(" + x + ", " + y + ")";
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Point other = (Point) obj;
+        return Double.compare(other.x, x) == 0 && Double.compare(other.y, y) == 0;
+    }
+}
+
+public class Circle {
+    private Point center;
+    private double radius;
+
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String toString() {
+        return "Circle(" + center.toString() + ", " + radius + ")";
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Circle other = (Circle) obj;
+        return Double.compare(other.radius, radius) == 0 && center.equals(other.center);
+    }
+}
