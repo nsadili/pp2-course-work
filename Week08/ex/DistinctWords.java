6bc9e52c2c3b7c98fdfcc99580e078aacf27ca5f
Gülnar Hesenli <ghasanli@Gulnars-MacBook-Air.local>
2023-03-28 00:00:42 +0400

    add

--- Week08/ex/DistinctWords.java
+++ Week08/ex/DistinctWords.java
@@ -1,5 +1,8 @@
 package ex;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class DistinctWords {
--- Week08/ex/QueueDemo.java
+++ Week08/ex/QueueDemo.java
@@ -3,14 +3,12 @@ import java.util.*;
 
 public class QueueDemo {
     public static void main(String[] args) {
-        // Initialize queue of people and the amount of bread in the store
         Queue<Integer> queue = new LinkedList<>();
         queue.add(3);
         queue.add(1);
         queue.add(2);
         int breadInStore = 5;
 
-        // Calculate how many people can buy bread
         int numPeople = queue.size();
         int numPeopleWhoCanBuy = 0;
         while (!queue.isEmpty() && breadInStore >= queue.peek()) {
@@ -19,7 +17,6 @@ public class QueueDemo {
             breadInStore -= numBreadRequested;
         }
 
-        // Print the results
         System.out.println("Number of people in queue: " + numPeople);
         System.out.println("Number of people who can buy bread: " + numPeopleWhoCanBuy);
     }
