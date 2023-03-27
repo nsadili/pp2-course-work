910841cf25a8727906b16178e86540538e2dd959
Gülnar Hesenli <ghasanli@Gulnars-MacBook-Air.local>
2023-03-27 23:57:29 +0400

    add

---
+++ Week08/ex/DistinctWords.java
@@ -0,0 +1,31 @@
+package ex;
+
+import java.util.*;
+
+public class DistinctWords {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("names.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                names.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            System.exit(1);
+        }
+
+        Set<String> uniqueNames = new LinkedHashSet<>(names);
+
+        List<String> sortedNames = new ArrayList<>(uniqueNames);
+        Collections.sort(sortedNames);
+
+        System.out.println("Distinct names in alphabetical order:");
+        for (String name : sortedNames) {
+            System.out.println(name);
+        }
+    }
+}
--- Week08/ex/ListsOfAccounts.java
+++ Week08/ex/ListsOfAccounts.java
@@ -2,12 +2,12 @@ package ex;
 
 import java.util.*;
 
-public class ListsOfAccounts implements Comparable<ListsOfAction> {
+public class ListsOfAccounts implements Comparable<ListsOfAccounts> {
     private String firstname;
     private String lastname;
     private double balance;
 
-    public ListsOfAction(String firstname, String lastname, double balance) {
+    public ListsOfAccounts(String firstname, String lastname, double balance) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.balance = balance;
@@ -25,7 +25,7 @@ public class ListsOfAccounts implements Comparable<ListsOfAction> {
         return balance;
     }
 
-    @Override
+   
     public String toString() {
         return "Account{" +
                 "firstname='" + firstname + '\'' +
@@ -34,36 +34,37 @@ public class ListsOfAccounts implements Comparable<ListsOfAction> {
                 '}';
     }
 
-    @Override
-    public int compareTo(ListsOfAction other) {
+    
+    public int compareTo(ListsOfAccounts other) {
         String thisFullname = this.firstname + this.lastname;
         String otherFullname = other.firstname + other.lastname;
         return thisFullname.compareTo(otherFullname);
     }
 
     public static void main(String[] args) {
-        List<ListsOfAction> accounts = new ArrayList<>();
-        accounts.add(new ListsOfAction("John", "Doe", 1000));
-        accounts.add(new ListsOfAction("Jane", "Doe", 500));
-        accounts.add(new ListsOfAction("Alice", "Wonderland", 2000));
+     
+        List<ListsOfAccounts> accounts = new ArrayList<>();
+        accounts.add(new ListsOfAccounts("John", "Doe", 1000));
+        accounts.add(new ListsOfAccounts("Jane", "Doe", 500));
+        accounts.add(new ListsOfAccounts("Alice", "Wonderland", 2000));
 
         System.out.println("Original list:");
-        for (ListsOfAction account : accounts) {
+        for (ListsOfAccounts account : accounts) {
             System.out.println(account);
         }
 
         Collections.sort(accounts);
 
         System.out.println("Sorted by fullname:");
-        for (ListsOfAction account : accounts) {
+        for (ListsOfAccounts account : accounts) {
             System.out.println(account);
         }
 
-        Comparator<ListsOfAction> balanceComparator = Comparator.comparing(ListsOfAction::getBalance);
+        Comparator<ListsOfAccounts> balanceComparator = Comparator.comparing(ListsOfAccounts::getBalance);
         Collections.sort(accounts, balanceComparator);
 
         System.out.println("Sorted by balance:");
-        for (ListsOfAction account : accounts) {
+        for (ListsOfAccounts account : accounts) {
             System.out.println(account);
         }
     }
