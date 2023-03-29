0e6e3b37d7530f81f29ec8e0ed95305c678ebd7b
Shamil Abbasov 16084 <sabbasov16084@ada.edu.az>
2023-03-29 22:28:54 +0400

    Modified code for CatchingResult for first part of assignment

--- Week09/CachingResult.java
+++ Week09/CachingResult.java
@@ -1,20 +1,39 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CachingResult {
     public static void main(String[] args) {
 
         String test=args[0];
-
+       
         var list = mostLeastCharacter(test);
-        System.out.println(list);
+        System.out.println("The Least Frequented Characters");
+        System.out.print("[ ");
+
+        for(var least : list.keySet()){
+            if(list.get(least).equals(-1)) System.out.print(least + " ");
+        } 
+        System.out.print("]");
+        System.out.println("");
+        System.out.println("The Most Frequented Characters");
+      
+
+        System.out.print("[ ");
+
+        for(var least : list.keySet()){
+            if(list.get(least).equals(1)) System.out.print(least + " ");  
+    } 
+    System.out.print("]");
+
 
     }
 
-    static List<Character> mostLeastCharacter(String test){
-        List<Character> list = new ArrayList<>();
+    static Map<Character, Integer> mostLeastCharacter(String test){
+        Map<Character , Integer> list = new HashMap<>();
 
         int max= Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
@@ -27,8 +46,8 @@ public class CachingResult {
         }
 
         for(var key : numOfChar.keySet()){
-            if(numOfChar.get(key) == max ) list.add(key);
-            if(numOfChar.get(key) == min) list.add(key);
+            if(numOfChar.get(key) == max ) list.put(key, 1);
+            if(numOfChar.get(key) == min) list.put(key, -1);
         }
 
         return list;
