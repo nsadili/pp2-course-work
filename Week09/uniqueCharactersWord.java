a4bf09725daf391c552d4600eeb79fc9f5e3acb8
rufat_hajizada_16231 <rufeth04@gmail.com>
2023-04-01 18:50:07 +0400

    edited

--- Week09/uniqueCharactersWord.java
+++ Week09/uniqueCharactersWord.java
@@ -1,5 +1,6 @@
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -7,30 +8,39 @@ public class uniqueCharactersWord {
 
     private static Map<String, Set<Character>> cache = new TreeMap<>();
     public static void main(String[] args) {
-        // String str = String.join(" ", args);
-        String str1 = "hello";
-        String str2 = "rufat";
-        String str3 = "shaaamiiil";
-        String str4 = "rufat";
-        String str5 = "hellllo";
+        // String str1 = "hello";
+        // String str2 = "rufat";
+        // String str3 = "shaaamiiil";
+        // String str4 = "rufat";
+        // String str5 = "hellllo";
 
-        testAWord(str1);
-        testAWord(str2);
-        testAWord(str3);
-        testAWord(str4);
-        testAWord(str5);
+        // testAWord(str1);
+        // testAWord(str2);
+        // testAWord(str3);
+        // testAWord(str4);
+        // testAWord(str5);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            testAWord(sc.next());
+        }
+
+        sc.close();
+
     }
 
     public static void testAWord (String str){
-        long time1=System.currentTimeMillis();
+        long time1=System.nanoTime();
         System.out.println(uniqueChars(str));
-        long time2=System.currentTimeMillis();
+        long time2=System.nanoTime();
         System.out.println("Time ellapsed: "+(time2-time1));
         System.out.println();
     }
 
     public static Set<Character> uniqueChars(String str){
-        if (cache.containsKey(str)) System.out.printf("%s returning from the cache:\n", str);
+        str=str.toLowerCase();
+        if (cache.containsKey(str)) System.out.printf("<%s> returning from the cache:\n", str);
 
         else{
             Set<Character> set = new HashSet<>();
@@ -39,7 +49,7 @@ public class uniqueCharactersWord {
                 set.add(ch);
             }
             cache.put(str, set);
-            return set;
+            // return set;
         }
         return cache.get(str);
         
