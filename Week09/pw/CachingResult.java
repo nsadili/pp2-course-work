c52f96a9ac6e88509a8f691b1a8fdd4c3ed630e5
Rahima_Karimova <rkarimova17347@ada.edu.az>
2023-04-01 13:25:34 +0400

    CachingResult added

---
+++ Week09/pw/CachingResult.java
@@ -0,0 +1,67 @@
+package pw;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class CachingResult {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Map<String, Map<Character, Integer>> cache = new LinkedHashMap<>();
+        while (true) {
+            System.out.println("Print 'e' to exit\nPlease enter your string:");
+            String str = scan.nextLine();
+            if (str.equals("e")) {
+                break;
+            }
+            if (!cache.containsKey(str)) {
+                Map<Character, Integer> info = new LinkedHashMap<>();
+                for (Character ch : str.toCharArray()) {
+                    info.put(ch, info.getOrDefault(ch, 0) + 1);
+                }
+                cache.put(str, info);
+                System.out.println("creating new cache:" + cache.get(str));
+            } else {
+                System.out.println("retrieving from cache:" + cache.get(str));
+            }
+            Map<Character, Integer> freqMap = cache.get(str);
+
+            char mostFreqChar = ' ';
+            int maxFreq = Integer.MIN_VALUE;
+            for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+                if (entry.getValue() > maxFreq) {
+                    maxFreq = entry.getValue();
+                    mostFreqChar = entry.getKey();
+                }
+
+            }
+            System.out.println("Most frequent characters:");
+            for (Map.Entry<Character, Integer> entry1 : freqMap.entrySet()) {
+                if (entry1.getValue() == maxFreq) {
+                    System.out.println(entry1.getKey() + "(" + maxFreq + " times)");
+                    
+                }
+            }
+           
+            char leastFreqChar = ' ';
+            int leastFreq = Integer.MAX_VALUE;
+            for (Map.Entry<Character, Integer> entry1 : freqMap.entrySet()) {
+                if (entry1.getValue() < leastFreq) {
+                    leastFreq = entry1.getValue();
+                }
+            }
+            System.out.println("Least frequent characters:");
+            for (Map.Entry<Character, Integer> entry1 : freqMap.entrySet()) {
+                if (entry1.getValue() == leastFreq) {
+                    System.out.println(entry1.getKey() + "(" + leastFreq + " times)");
+                    
+                }
+            }
+           
+
+        }
+
+    }
+}
--- Week09/pw/CharacterOccurances.java
+++ Week09/pw/CharacterOccurances.java
@@ -10,31 +10,30 @@ import java.util.Scanner;
 public class CharacterOccurances {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String str= scan.nextLine().toUpperCase();
-   // String str = args[0].toLowerCase();
-     Map<Character,ArrayList<Integer>> charArray=new TreeMap<>(new Comparator<Character>() {
+        String str = scan.nextLine().toUpperCase();
+        // String str = args[0].toLowerCase();
+        Map<Character, ArrayList<Integer>> charArray = new TreeMap<>(new Comparator<Character>() {
 
-        @Override
-        public int compare(Character o1, Character o2) {
-           return o2-o1;
+            @Override
+            public int compare(Character o1, Character o2) {
+                return o2 - o1;
+            }
+
+        });
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (!(charArray.containsKey(c))) {
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(i);
+                charArray.put(c, list);
+            } else {
+                charArray.get(c).add(i);
+            }
         }
-        
-     });
-     
-for(int i=0;i<str.length();i++){
-char c =str.charAt(i);
-if(!(charArray.containsKey(c))){
-ArrayList<Integer> list= new ArrayList<>();
-list.add(i);
-charArray.put(c, list);
-}
-else{
-    charArray.get(c).add(i);
-}
-}
-for(var a:charArray.keySet() ){
-    System.out.println(a+"-->" +charArray.get(a));
-}
+        for (var a : charArray.keySet()) {
+            System.out.println(a + "-->" + charArray.get(a));
+        }
 
     }
 }
