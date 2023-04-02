97c5fc1c97ef26bcb55fd02ca1f987efce985964
Ibrahim Huseynov <ihuseynov16086@ada.edu.az>
2023-04-02 21:40:25 +0400

    Complete week09 ex

--- Week09/Ex/OptionalDemo.java
+++ Week09/Ex/OptionalDemo.java
@@ -3,6 +3,7 @@ import java.util.StringTokenizer;
 
 public class OptionalDemo {
     public static void main(String[] args) {
+        
         String text= "Today is a rainy day.";
         Optional<String> r1= findWord(text, "rai");
         System.out.println(r1);
@@ -13,6 +14,8 @@ public class OptionalDemo {
         Optional<String> r3= findWord(text, "sun");
         System.out.println(r3);
     }
+
+
     public static Optional<String> findWord(String text, String prefix){
         StringTokenizer t= new StringTokenizer(text);
         while(t.hasMoreTokens()){
