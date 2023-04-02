7408fd5c968765e9a55dc3a0101e32a0d1fce06b
Rahida <rasadli16055@ada.edu.az>
2023-04-02 14:59:48 +0400

    week8

---
+++ Week08/DistinctWords.java
@@ -0,0 +1,50 @@
+package Week08;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+public class DistinctWords {
+    public static void main(String[] args) {
+        LinkedHashSet<String> uniqueNames = new LinkedHashSet<>(initCollection());
+        System.out.println(uniqueNames.toString());
+
+        List<String> sortedNames = new ArrayList<>(uniqueNames);
+        Collections.sort(sortedNames);
+        System.out.println(sortedNames.toString());
+    }
+
+    static Collection<String> initCollection() {
+        ArrayList<String> collection = new ArrayList<String>();
+        Collections.addAll(collection,
+                "Andrew", "Collins", "Ruth", "Clark", "Melissa", "Bell", "William", "Powell", "Ashley", "Perry",
+                "Bonnie", "Jones", "Robert", "Jenkins", "Jonathan", "Long", "Edward", "Kelly", "Fred", "Evans", "Sara",
+                "Gray", "Lois", "Jackson", "Mary", "Cook", "Daniel", "Davis", "Barbara", "Rivera", "Lori", "Patterson",
+                "Theresa", "James", "Terry", "Sanders", "Norma", "Washington", "Sharon", "Phillips", "Martha", "Green",
+                "Tina", "Torres", "Louise", "Ward", "Kimberly", "Brown", "Ryan", "Smith", "Johnny", "Russell", "Jose",
+                "Morgan", "Russell", "Ramirez", "Sean", "Parker", "Steve", "Mitchell", "Matthew", "Nelson", "Philip",
+                "Bryant", "Deborah", "Barnes", "Clarence", "Lee", "Eric", "Peterson", "Thomas", "Simmons", "Margaret",
+                "Murphy", "Jack", "White", "Sandra", "Hill", "Bruce", "Martinez", "Angela", "Taylor", "Jeremy",
+                "Foster", "John", "Reed", "David", "Wilson", "Carolyn", "Coleman", "Stephanie", "Hughes", "Rose",
+                "Robinson", "Shawn", "Edwards", "Cheryl", "Morris", "Benjamin", "Lopez", "Elizabeth", "Bennett",
+                "Frances", "Roberts", "Kenneth", "Scott", "Anna", "Baker", "Diane", "Wood", "Tammy", "Harris", "Moore",
+                "Marilyn", "Anderson", "Ruby", "Campbell", "Christine", "Richardson", "Jean", "Rodriguez", "Jason",
+                "Garcia", "Adams", "Kevin", "Henderson", "Victor", "Bailey", "Julia", "Wright", "Randy", "Thompson",
+                "Brandon", "Gonzalez", "Donald", "King", "Amanda", "Price", "Phyllis", "Hall", "Stewart", "Lawrence",
+                "Cooper", "Peter", "Adams", "Kevin", "Henderson", "Victor", "Bailey", "Julia", "Wright", "Roy",
+                "Martin", "Chris", "Howard", "Irene", "Flores", "Donna", "Thomas", "Nancy", "Young", "Willie", "Brooks",
+                "Ernest", "Moore", "Marilyn", "Walker", "Marie", "Turner", "Wayne", "Hernandez", "Todd", "Miller",
+                "Nicole", "Sanchez", "Linda", "Griffin", "Michelle", "Stewart", "Betty", "Butler", "Diaz", "Mark",
+                "Cox", "Christina", "Ernest", "Moore", "Marilyn", "Anderson", "Ruby", "Campbell", "Christine",
+                "Richardson", "Jean", "Rodriguez", "Jason", "Garcia", "Adams", "Kevin", "Henderson", "Victor", "Bailey",
+                "Julia", "Wright", "Roy", "Martin", "Chris", "Howard", "Irene", "Flores", "Donna", "Thomas", "Nancy",
+                "Young", "Willie", "Brooks", "Ernest", "Moore", "Marilyn", "Walker", "Marie", "Turner", "Wayne",
+                "Gerald", "Watson", "Nicholas", "Alexander", "Ann", "Carter", "Kathy", "Perez", "Juan", "Gonzales",
+                "Gary", "Allen", "Carl", "Ross", "Jessica", "Rogers", "Rebecca", "Johnson", "Janice", "Lewis", "Julie",
+                "Williams", "Judy", "Diaz", "Mark", "Cox", "Heather");
+
+        return collection;
+    }
+}
---
+++ Week08/LinkedListStack.java
@@ -0,0 +1,60 @@
+package Week08;
+
+import java.util.EmptyStackException;
+
+public class LinkedListStack<T> {
+    private Node<T> top;
+    private int size;
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        private Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public LinkedListStack() {
+        top = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void push(T element) {
+        Node<T> newNode = new Node<>(element);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public void clear() {
+        top = null;
+        size = 0;
+    }
+}
