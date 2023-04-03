96869ddd6ba934fead694f74c1405dfc6117d776
Seljan Musayeva <smusayeva17601@ada.edu.az.com>
2023-04-03 22:47:26 +0400

    Completed pw

---
+++ Week09/pw/CharacterOccurrences.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CharacterOccurrences {
+    public static void main(String[] args) {
+
+        if (args.length == 0)
+            System.out.println("Enter a string!");
+
+        String str = String.join(" ", args);
+
+        TreeMap<Character, List<Integer>> map = (TreeMap<Character, List<Integer>>) getCharOccurences(str);
+
+        if (map == null)
+            System.out.println("NULL");
+        else {
+            System.out.println("nondescending");
+            for (Character ch : map.keySet())
+                System.out.println(ch + ": " + map.get(ch));
+
+            System.out.println("nonascending");
+            for (Character ch : map.descendingKeySet())
+                System.out.println(ch + ": " + map.get(ch));
+        }
+    }
+
+    static Map<Character, List<Integer>> getCharOccurences(String string) {
+        if (string == null)
+            return null;
+
+        Map<Character, List<Integer>> map = new TreeMap<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            Character ch = string.charAt(i);
+            if (!map.containsKey(ch))
+                map.put(ch, new ArrayList<>());
+
+            map.get(ch).add(i);
+        }
+
+        return map;
+    }
+}
+
