7be2d655a3e0df48452a5b907dfd26ea89b400d9
IsmayilovaSara <ismayilovassara@gmail.com>
2023-04-04 11:10:28 +0400

    Task added

---
+++ Week09/CharacterOccurences.java
@@ -0,0 +1,74 @@
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.Collections;
+import java.util.*;
+
+public class CharacterOccurences
+{
+    public static void main(String[] args)
+    {
+    
+
+        if(args.length > 0)
+        {
+            String str = args[0];
+        
+        
+
+        TreeMap<Character, ArrayList<Integer>> charOccurence = new TreeMap<>();      
+
+        for(int i = 0; i <str.length(); i++)
+        {
+            char c = str.charAt(i);
+
+            if(charOccurence.containsKey(c))
+            {
+                charOccurence.get(c).add(i);
+            }
+            else
+            {
+                ArrayList<Integer> indexList = new ArrayList<>();
+                indexList.add(i);
+                charOccurence.put(c, indexList);
+            }
+        }
+
+        System.out.println("Characters and their indexes in non-descending order : ");
+        for(Map.Entry<Character, ArrayList<Integer>> entry : charOccurence.entrySet())
+        {
+            System.out.print(entry.getKey() + ": ");
+            for(int index : entry.getValue())
+            {
+                System.out.print(index + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Characters and their indexes in non-ascending order : ");
+        ArrayList<Character> charListDesc = new ArrayList<>(charOccurence.keySet());
+        Collections.reverse(charListDesc);
+        for(char c : charListDesc)
+        {
+            System.out.print(c + ": ");
+            for(int index : charOccurence.get(c))
+            {
+                System.out.print(index + " ");
+            }
+            System.out.println();
+
+        }
+        }
+        else
+        {
+            System.out.println("Please provide a string argument");
+        }
+
+
+
+    }
+
+    
+
+}
