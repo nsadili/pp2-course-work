1ecf2de2dd1542b28743bd8bca7fb5ad8a761f67
Rahima_Karimova <rkarimova17347@ada.edu.az>
2023-04-04 04:05:37 +0400

    editing CachingResult little bit

--- Week09/pw/CachingResult.java
+++ Week09/pw/CachingResult.java
@@ -1,5 +1,6 @@
 package pw;
 
+import java.security.KeyStore.Entry;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +13,7 @@ public class CachingResult {
         Map<String, Map<Character, Integer>> cache = new LinkedHashMap<>();
         while (true) {
             System.out.println("Print 'e' to exit\nPlease enter your string:");
-            String str = scan.nextLine();
+            String str = scan.nextLine().toLowerCase();
             if (str.equals("e")) {
                 break;
             }
@@ -22,45 +23,38 @@ public class CachingResult {
                     info.put(ch, info.getOrDefault(ch, 0) + 1);
                 }
                 cache.put(str, info);
-                System.out.println("creating new cache:" + cache.get(str));
-            } else {
-                System.out.println("retrieving from cache:" + cache.get(str));
             }
+
             Map<Character, Integer> freqMap = cache.get(str);
 
-            char mostFreqChar = ' ';
             int maxFreq = Integer.MIN_VALUE;
+            int leastFreq = Integer.MAX_VALUE;
             for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
                 if (entry.getValue() > maxFreq) {
                     maxFreq = entry.getValue();
-                    mostFreqChar = entry.getKey();
+                }
+                if (entry.getValue() < leastFreq) {
+                    leastFreq = entry.getValue();
                 }
 
             }
-            System.out.println("Most frequent characters:");
-            for (Map.Entry<Character, Integer> entry1 : freqMap.entrySet()) {
-                if (entry1.getValue() == maxFreq) {
-                    System.out.println(entry1.getKey() + "(" + maxFreq + " times)");
+            System.out.println("Most frequent elements:");
+            printChar(maxFreq, freqMap);
+            System.out.println("Least frequent elements:");
+            printChar(leastFreq, freqMap);
+        }
 
-                }
-            }
+    }
 
-            char leastFreqChar = ' ';
-            int leastFreq = Integer.MAX_VALUE;
-            for (Map.Entry<Character, Integer> entry1 : freqMap.entrySet()) {
-                if (entry1.getValue() < leastFreq) {
-                    leastFreq = entry1.getValue();
-                }
-            }
-            System.out.println("Least frequent characters:");
-            for (Map.Entry<Character, Integer> entry1 : freqMap.entrySet()) {
-                if (entry1.getValue() == leastFreq) {
-                    System.out.println(entry1.getKey() + "(" + leastFreq + " times)");
-
-                }
-            }
+    static void printChar(Integer minMaxInteger, Map<Character, Integer> map) {
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.println(
+                    (entry.getValue().compareTo(minMaxInteger) == 0
+                            ? entry.getKey() + "-->" + entry.getValue()+"\n"
+                            : "\u001b[1A\r"));
 
         }
 
     }
+
 }
