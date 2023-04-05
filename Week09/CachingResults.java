a108826d1b346b39c2328e03f9d1e4b305efaac1
AliyevaVasila <vasilaaliyeffa@gmail.com>
2023-04-05 22:15:27 +0300

    modify

--- Week09/CachingResults.java
+++ Week09/CachingResults.java
@@ -5,16 +5,15 @@ import java.util.Map;
 
 public class CachingResults {
 
-    private static Map<String, List<String>> cache = new HashMap<>();
+    private static Map<String, List<Character>> cache = new HashMap<>();
 
     public static void main(String[] args) {
         String str = "Hello world";
         List<Character> res = getUniqueChar(str);
-
-        System.out.println("Unique Characters: " + res);
+        System.out.println("Unique characters: " + res);
 
         res = getUniqueChar(str);
-        System.out.println("Unique Characters: " + res);
+        System.out.println("Unique characters: " + res);
     }
 
     public static List<Character> getUniqueChar(String str) {
@@ -22,11 +21,13 @@ public class CachingResults {
             System.out.println("Result from cache!");
             return cache.get(str);
         }
+
         List<Character> uniqueChar = new ArrayList<>();
-        for (var i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (!uniqueChar.contains(ch))
-                uniqueChar.add(ch);
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (!uniqueChar.contains(c)) {
+                uniqueChar.add(c);
+            }
         }
         cache.put(str, uniqueChar);
         return uniqueChar;
