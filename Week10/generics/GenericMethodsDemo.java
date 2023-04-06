a1b4543108061a772f04c1199373f33c43dc80b4
Rahima_Karimova <rkarimova17347@ada.edu.az>
2023-04-06 12:08:16 +0400

    exercise session codes

---
+++ Week10/generics/GenericMethodsDemo.java
@@ -0,0 +1,77 @@
+package generics;
+
+public class GenericMethodsDemo {
+    public static void main(String[] args) {
+        Integer arr1[] = new Integer[] { 1, 5, 2, 6, 3, 6 };
+        Double arr2[] = new Double[] { 3.5, 2.2, 0.5, 6.1, 3.0, 6.0 };
+        Task arr3[] = new Task[] { new Task("T1", 5, 10), new Task("T2", 6, 30), new Task("T1", 9, 50) };
+        String arr4[] = new String[] { "Maryam", "Abdulla", "Nihad", "Asim", "Shamsi", "Nazrin" };
+        Point arr5[] = new Point[] { new Point(3, 5), new Point(), new Point(100, 200) };
+        printAll(arr1);
+
+        printAll(arr3);
+        sort(arr3);
+        printAll(arr3);
+
+        System.out.println(mid(arr2));
+        System.out.println(mid(arr5));
+
+        System.out.println(max(arr4));
+
+    }
+
+    static <T> void printAll(T[] arr) {
+        // for (T el : arr)
+        // System.out.print(el + " ");
+        // System.out.println();
+        printAll(arr, 0, arr.length - 1);
+
+    }
+
+    static <T> void printAll(T[] arr, int lower, int upper) {
+        if (lower < 0 || lower >= arr.length || upper < 0 || upper >= arr.length) {
+            throw new ArrayIndexOutOfBoundsException("Either lower or upper is out of bounds");
+        }
+        for (int i = lower; i <= upper; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    static void printAll(Task[] arr) {
+        for (Task all : arr) {
+            System.out.println(all);
+        }
+    }
+
+    static <T> T mid(T[] arr) {
+        if (arr.length == 0)
+            throw new IllegalArgumentException("Please enter input!");
+        return arr[arr.length / 2];
+    }
+
+    static <T extends Comparable<? super T>> T max(T[] arr) {
+        if (arr.length == 0)
+            throw new IllegalArgumentException("Please Enter input!");
+        T max = arr[0];
+        for (T el : arr)
+            if (max.compareTo(el) < 0)
+                max = el;
+        return max;
+
+    }
+
+    static <T extends Comparable<? super T>> void sort(T[] arr) {
+        for (int i = 0; i < arr.length; i++)
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    swap(arr, j, j + 1);
+                }
+            }
+    }
+
+    static <T> void swap(T[] arr, int x, int y) {
+        T temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+}
