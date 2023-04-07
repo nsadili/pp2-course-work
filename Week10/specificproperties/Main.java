1b3c7fee6f23a19d0edd90f2652c08708f72ecb3
NarimanMammadov <nmammadov16237@ada.edu.az>
2023-04-07 12:41:16 +0400

    property+

--- Week10/specificproperties/Main.java
+++ Week10/specificproperties/Main.java
@@ -6,7 +6,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,2,2,3,4,4,5,6,6,6);
+        List<String> list1 = Arrays.asList("bob", "anana", "hello");
         int count = Util.count(list, new EvenIntegerProperty());
+        int count1 = Util.count(list1, new PalindromeProperty());
         System.out.println(count);
+        System.out.println(count1);
     }   
 }
