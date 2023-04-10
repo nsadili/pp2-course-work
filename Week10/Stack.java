e87064d557e6a561e7efc5509c55f62da415a8e7
Leyla16125 <118552639+Leyla16125@users.noreply.github.com>
2023-04-11 00:07:52 +0400

    pw ex

---
+++ Week10/Stack.java
@@ -0,0 +1,43 @@
+package Week10;
+import java.util.ArrayList;
+
+public class Stack<T> {
+    private int maxSize;
+    private ArrayList<T> stack;
+
+    public Stack(int maxSize) {
+        this.maxSize = maxSize;
+        this.stack = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public boolean isFull() {
+        return stack.size() == maxSize;
+    }
+
+    public void push(T item) {
+        if (isFull()) {
+            throw new IllegalStateException("Stack is full");
+        }
+        stack.add(item);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return stack.get(stack.size() - 1);
+    }
+}
+
+
