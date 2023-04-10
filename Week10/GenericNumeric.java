7b5eaf9488eed6cbf51298ff12b7979a00934746
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-04-10 09:50:00 +0400

    Add files via upload

---
+++ Week10/GenericNumeric.java
@@ -0,0 +1,27 @@
+public class GenericNumeric<T extends Number> {
+    private T number;
+
+    public GenericNumeric(T number) {
+        this.number = number;
+    }
+
+    public T getNumber() {
+        return number;
+    }
+
+    public void setNumber(T number) {
+        this.number = number;
+    }
+
+    public double reciprocal() {
+        return 1 / number.doubleValue();
+    }
+
+    public double fractionalPart() {
+        return number.doubleValue() - number.intValue();
+    }
+
+    public boolean absoluteValueEqual(T number) {
+        return Math.abs(this.number.doubleValue()) == Math.abs(number.doubleValue());
+    }
+}
