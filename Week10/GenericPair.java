9374112df6a7ab7efd6939499d76aa547cc73b07
mtahmazli <123540675+mtahmazli@users.noreply.github.com>
2023-04-10 22:08:22 +0400

    Week 10

---
+++ Week10/GenericPair.java
@@ -0,0 +1,36 @@
+package Week10;
+
+
+
+public class GenericPair<A,B> {
+    private A a;
+    private B b;
+
+    public GenericPair(A a, B b){
+        this.a=a;
+        this.b=b;
+    }
+    public A getA() {
+        return a;
+    }
+    public B getB() {
+        return b;
+    }
+    public void setA(A a) {
+        this.a = a;
+    }
+    public void setB(B b) {
+        this.b = b;
+    }
+    @Override
+    public String toString() {
+        return getA()+" and "+getB();
+    }
+
+    public static void main(String[] args) {
+        GenericPair<Integer, Character> s = new GenericPair<>(15,'Y');
+        System.out.println(s);
+        System.out.println(s.getA());
+
+    }
+}
---
+++ Week10/Stack.java
@@ -0,0 +1,84 @@
+package Week10;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack<T> {
+    private List<T> elements;   
+    private int capacity;
+
+    public Stack(int capacity) {
+        if (capacity<0){
+            throw new NegativeArraySizeException("You cannot write a negative number as a capacity!");
+        }
+        this.capacity = capacity;
+        this.elements = new ArrayList<T>(capacity);
+    }
+
+    public boolean isEmpty() {
+        return elements.size() == -1;
+    }
+
+    public boolean isFull() {
+        return elements.size()== capacity;
+    }
+
+    public T peek() {
+        if (this.isEmpty()){
+            throw new ArrayIndexOutOfBoundsException("Stack is empty, you cannot peek!");
+        }
+        return elements.get(elements.size()-1);
+    }
+
+    public T pop() {
+        if (this.isEmpty()){
+            throw new ArrayIndexOutOfBoundsException("Stack is empty, you cannot pop!");
+        }
+        return elements.remove(elements.size()-1);
+    }
+
+    public void push(T el) {
+        if (this.isFull()){
+            throw new ArrayIndexOutOfBoundsException("Stack is full, you cannot push!");
+        }
+        elements.add(el);
+    }
+
+    public void __print() {
+        if (this.isEmpty()) {
+            System.out.println("Empty stack.");
+            return;
+        }
+        System.out.print("Bottom");
+        for (int i = 0; i < elements.size(); i++) {
+            System.out.print(" || " + elements.get(i));
+        }
+        System.out.println(" || Top");
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>(5);
+        
+
+        stack.push(7);
+        stack.push(8);
+        stack.push(9);
+        stack.push(10);
+        stack.push(11);
+
+        stack.pop();
+        stack.__print();
+        System.out.println("The peek element: " + stack.peek());
+
+        Stack<String> stack1 = new Stack<>(3);
+
+        stack1.push("Murad");
+        stack1.push("Ilkin");
+        stack1.push("Rauf");
+        stack1.pop();
+
+        stack1.__print();
+        System.out.println("The peek element: " + stack1.peek());
+
+
+    }
+}
