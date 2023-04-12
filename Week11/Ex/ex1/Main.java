fa71920422faa51b5a535898bf6a6ce8648bb22b
Ibrahim Huseynov <ihuseynov16086@ada.edu.az>
2023-04-12 10:52:57 +0400

    Add week11 ex

---
+++ Week11/Ex/ex1/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        AFuncInt f= new AFuncInt() {
+
+            @Override
+            public void apply(String str) {
+                System.out.println("Anonymous -> " + str);
+
+                
+            }
+            
+            
+            
+        };
+
+
+       AFuncInt.print("Hello");
+        f.print("Hello", "world");
+        f.apply("Nuraddin");
+        System.out.println("=====================================");
+
+
+        AFuncInt lambda = (str) -> System.out.println("Lambda -> "+str);
+        AFuncInt.print("Hello");
+        lambda.print("Hello", "world");
+        lambda.apply("Nuraddin");
+
+        
+    }
+    
+}
