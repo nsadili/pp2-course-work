51d1a3c8933995ccbf3d193769519cd802eed7b5
Seyidshah Muradov 16150 <smuradov16150@ada.edu.az>
2023-04-13 18:54:54 +0400

    Add ex1

---
+++ Week11/ex/ex1/AFuncInt.java
@@ -0,0 +1,14 @@
+package ex1;
+
+public interface AFuncInt {
+
+    void apply(String str);
+
+    static void print(String str) {
+        System.out.println("STATIC -> " + str);
+    }
+
+    default void print(String str1, String str2) {
+        System.out.println("DEFAULT -> " + str1 + " ; " + str2);
+    }
+}
