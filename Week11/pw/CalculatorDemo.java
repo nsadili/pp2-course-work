02070108c535c7fc7af63b2447a0a11b38a2ffea
NarimanMammadov <nmammadov16237@ada.edu.az>
2023-04-14 01:55:15 +0400

    methodreference

--- Week11/pw/CalculatorDemo.java
+++ Week11/pw/CalculatorDemo.java
@@ -9,15 +9,11 @@ public class CalculatorDemo{
         Calculator div = (a,b) -> a/b;
         Calculator exp = (a,b) -> Math.pow(a, b);
 
-        
         System.out.println("ADD: "+add.calculate(3,5));
         System.out.println("SUBT: "+subt.calculate(9,5));
         System.out.println("MULT: "+mult.calculate(3,5));
         System.out.println("DIV: "+div.calculate(30,5));
         System.out.println("EXP: "+exp.calculate(3,3));
-
-        
-        
     }
 }
 
---
+++ Week11/pw/MethodReference.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+class Employee{
+    private String firstName;
+    private int age;
+    private int salary;
+
+    public Employee(String firstName, int age, int salary) {
+        this.firstName = firstName;
+        this.age = age;
+        this.salary = salary;
+    }
+    @Override
+    public String toString() {
+        return getFirstName()+" | AGE: "+getAge()+" | SALARY: "+getSalary()+"$";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
+
+public class MethodReference {
+    public static int compareBySalary(Employee a, Employee b) {
+        return a.getSalary() - b.getSalary();
+    }
+    public static int compareByAge(Employee a, Employee b) {
+        return a.getAge() - b.getAge();
+    }
+    public static void main(String[] args) {
+    List<Employee> list = Arrays.asList(
+        new Employee("Nariman", 19, 2000),
+        new Employee("Shamil", 20, 300),
+        new Employee("Mahammad", 31, 10)
+        );
+
+        list.sort((a, b) -> a.getFirstName().compareTo(b.getFirstName())); //lambda exression
+        System.out.println("SORTED 1: "+ Arrays.asList(list));
+
+        // System.out.println(compareBySalary(new Employee("Shamil", 20, 3000), new Employee("Mahammad", 31, 10)));
+        list.sort(MethodReference::compareBySalary); //?
+        System.out.println("SORTED 2: "+ Arrays.asList(list));
+
+        list.sort(MethodReference::compareByAge);
+        System.out.println("SORTED 3: " + Arrays.asList(list));
+
+
+
+
+    }
+}
+class compareEmployeeAge implements Comparator<Employee> {
+    @Override
+    public int compare(Employee a, Employee b) {
+        return b.getAge() - a.getAge();
+    }
+}
