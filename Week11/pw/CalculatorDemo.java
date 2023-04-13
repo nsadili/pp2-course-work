fe023baa4f455db16bfd4531bd9d71bec0ce0144
NarimanMammadov <nmammadov16237@ada.edu.az>
2023-04-14 01:04:49 +0400

    calculator

---
+++ Week11/pw/CalculatorDemo.java
@@ -0,0 +1,23 @@
+interface Calculator{
+    double calculate(double a, double b);
+}
+public class CalculatorDemo{
+    public static void main(String[] args) {
+        Calculator add = (a,b) -> a+b;
+        Calculator subt = (a,b) -> a-b;
+        Calculator mult = (a,b) -> a*b;
+        Calculator div = (a,b) -> a/b;
+        Calculator exp = (a,b) -> Math.pow(a, b);
+
+        
+        System.out.println("ADD: "+add.calculate(3,5));
+        System.out.println("SUBT: "+subt.calculate(9,5));
+        System.out.println("MULT: "+mult.calculate(3,5));
+        System.out.println("DIV: "+div.calculate(30,5));
+        System.out.println("EXP: "+exp.calculate(3,3));
+
+        
+        
+    }
+}
+
