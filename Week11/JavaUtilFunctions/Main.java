cb726888f4ea5762cbf44144a4ff69627144dae0
Naik Pashayev 13148 <naikpasha1994@gmail.com>
2023-04-14 19:56:02 +0400

    Added my files

--- Week11/JavaUtilFunctions/Main.java
+++ Week11/JavaUtilFunctions/Main.java
@@ -1,5 +1,46 @@
 package Week11.JavaUtilFunctions;
 
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class Main {
-    
+    public static void main(String[] args) {
+
+        System.out.println("Consumer:");
+        Consumer<String> consumer = (str) -> System.out.println(str.toUpperCase());
+        consumer.accept("Hello");
+        consumer.accept("Naik Pashayev");
+
+        System.out.println();
+        System.out.println("Function:");
+        Function<String, Integer> lengthLambda = str -> str.length();
+        System.out.println(lengthLambda.apply("Hello"));
+        System.out.println(lengthLambda.apply("Naik"));
+
+        System.out.println();
+        System.out.println("Predicate:");
+        System.out.println();
+        System.out.println("Is even:");
+        Predicate<Integer> isEven = num -> num%2 == 0;
+        System.out.println(isEven.test(7));
+        System.out.println(isEven.test(8));
+        System.out.println();
+        System.out.println("Is upper:");
+        Predicate<Character> isUpper = ch -> Character.isUpperCase(ch);
+        System.out.println(isUpper.test('N'));
+        System.out.println(isUpper.test('n'));
+
+        System.out.println();
+        System.out.println("Supplier:");
+
+        final Random randGenerator = new Random();
+        Supplier<Integer> supplier = () -> randGenerator.nextInt(6) + 1;
+        for (int i = 0; i < 100; i++) {
+            System.out.print(supplier.get() + " ");
+        }
+        
+   }
 }
---
+++ Week11/StreamAPI/StreamAPI.java
@@ -0,0 +1,45 @@
+package Week11.StreamAPI;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StreamAPI {
+    public static void main(String[] args) {
+        String[] names = { "Naik", "Vidadi", "Arzu", "Zakir"};
+        Stream<String> namesStream = Arrays.stream(names);
+
+        Set<Integer> ages = new HashSet<>(Arrays.asList(18, 19, 19, 21, 18, 26, 23, 30));
+        var agesStream = ages.stream();
+
+        // Map<String, Integer> map = new HashMap<>();
+        // map.entrySet().stream();
+
+        // System.out.println(namesStream.anyMatch(name -> name.startsWith("N")));
+
+        // System.out.println(namesStream.noneMatch(name -> name.equals("Naik")));
+
+        final Random randGenerator = new Random();
+        Supplier<Integer> supplier = () -> randGenerator.nextInt(6) + 1;
+        var testStream = Stream.generate(supplier).limit(10);
+        // testStream.forEach(num -> System.out.println(num));
+        // testStream.forEach(System.out::println);
+
+        Optional<Integer> foundAge = agesStream
+        .filter(age -> age > 20)
+        .findAny();
+        
+        // System.out.println(
+        //     foundAge.isPresent() ? foundAge.get() : "Not Found"
+        //  );
+
+        foundAge.ifPresentOrElse(
+            System.out::println, () -> System.out.println("Not Found"));
+    }
+}
