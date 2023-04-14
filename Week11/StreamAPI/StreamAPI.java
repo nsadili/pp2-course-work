5eac1f21c18fa30970bd08d8328f7578d53d6fcc
Naik Pashayev 13148 <naikpasha1994@gmail.com>
2023-04-14 19:56:47 +0400

    Added my files

--- Week11/StreamAPI/StreamAPI.java
+++ Week11/StreamAPI/StreamAPI.java
@@ -18,26 +18,26 @@ public class StreamAPI {
         Set<Integer> ages = new HashSet<>(Arrays.asList(18, 19, 19, 21, 18, 26, 23, 30));
         var agesStream = ages.stream();
 
-        // Map<String, Integer> map = new HashMap<>();
-        // map.entrySet().stream();
+        Map<String, Integer> map = new HashMap<>();
+        map.entrySet().stream();
 
-        // System.out.println(namesStream.anyMatch(name -> name.startsWith("N")));
+        System.out.println(namesStream.anyMatch(name -> name.startsWith("N")));
 
-        // System.out.println(namesStream.noneMatch(name -> name.equals("Naik")));
+        System.out.println(namesStream.noneMatch(name -> name.equals("Naik")));
 
         final Random randGenerator = new Random();
         Supplier<Integer> supplier = () -> randGenerator.nextInt(6) + 1;
         var testStream = Stream.generate(supplier).limit(10);
-        // testStream.forEach(num -> System.out.println(num));
-        // testStream.forEach(System.out::println);
+        testStream.forEach(num -> System.out.println(num));
+        testStream.forEach(System.out::println);
 
         Optional<Integer> foundAge = agesStream
         .filter(age -> age > 20)
         .findAny();
         
-        // System.out.println(
-        //     foundAge.isPresent() ? foundAge.get() : "Not Found"
-        //  );
+        System.out.println(
+            foundAge.isPresent() ? foundAge.get() : "Not Found"
+         );
 
         foundAge.ifPresentOrElse(
             System.out::println, () -> System.out.println("Not Found"));
