fb4cfbee2caac7bbcc3d6dd17ef9f95969b4b616
Ilqar Malikov13943 <102352450+imalikov13943@users.noreply.github.com>
2023-04-15 10:26:48 +0400

    Exercise 4

--- Week11/Main.java
+++ Week11/Main.java
@@ -1,26 +1,51 @@
-package Ex3;
+package Week11.Ex4;
+
+import java.util.Random;
+import java.util. function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 
 public class Main {
-    public static void main(String[] args) {
-        Calculator addition = (a, b) -> a + b;
-        Calculator subtraction = (a, b) -> a - b;
-        Calculator multiplication = (a, b) -> a * b;
-        Calculator division = (a, b) -> {
-            if (b == 0) {
-                throw new ArithmeticException("Division by zero is not allowed.");
-            }
-            return a / b;
-        };
-        Calculator exponentiation = (a, b) -> Math.pow(a, b);
+public static void main(String[] args){
+    Consumer<String> c1 = (str) -> System.out.println("Consumed: " + str);
+c1.accept ("Hello");
+Consumer<String> c2 = str -> {
+    String newOne = str.toLowerCase () ;
+System.out.println(newOne);
+};
+// c1.accept ("Hello");
+// c2.accept ("Hey there");
 
-        double a = 5;
-        double b = 2;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println("a + b = " + addition.calculate(a, b));
-        System.out.println("a - b = " + subtraction.calculate(a, b));
-        System.out.println("a * b = " + multiplication.calculate(a, b));
-        System.out.println("a / b = " + division.calculate(a, b));
-        System.out.println("a ^ b = " + exponentiation.calculate(a, b));
-    }
+Function<String, Integer> funcLen = str -> (str==null ? 0 : str.length());
+
+Function<String, Integer> funcLen2 = (str) -> {
+    if(str == null)
+        throw new IllegalArgumentException();
+        int res = str.length();
+        return res;
+}; 
+
+// System.out.println(funcLen.apply("Nuraddin"));
+// System.out.println(funcLen2.apply("Nuraddin"));
+
+// System.out.println(funcLen.apply(null));
+// System.out.println(funcLen2.apply(null));
+
+Predicate<Integer> evenLambda = n -> n%2 == 0;
+
+Predicate<String> validWordLambda = s -> s!= null & s.length() > 3;
+
+// System.out.println(validWordLambda.test("abc"));
+// System.out.println(validWordLambda.test("ab"));
+// System.out.println(validWordLambda.test(null));
+// System.out.println(validWordLambda.test("Programming"));
+
+Supplier<Integer> randomIntSupplier = () -> new Random().nextInt(10) + 1;
+
+for(int i = 0; i <10; i++)
+    System.out.println(randomIntSupplier.get());
+
+}
 }
