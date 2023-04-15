e6df1e64ce6ea8c60078a237f0e04a3ba14be7fc
Ilqar Malikov13943 <102352450+imalikov13943@users.noreply.github.com>
2023-04-15 10:28:06 +0400

    Exercise 7(Bonus)

---
+++ Week11/Employee.java
@@ -0,0 +1,35 @@
+package Ex6;
+
+class Employee {
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getDepartment() {
+        return department;
+    }
+    
+    public double getSalary() {
+        return salary;
+    }
+    
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+}
--- Week11/Main.java
+++ Week11/Main.java
@@ -1,51 +1,81 @@
-package Week11.Ex4;
+package Ex6;
 
-import java.util.Random;
-import java.util. function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
-
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-public static void main(String[] args){
-    Consumer<String> c1 = (str) -> System.out.println("Consumed: " + str);
-c1.accept ("Hello");
-Consumer<String> c2 = str -> {
-    String newOne = str.toLowerCase () ;
-System.out.println(newOne);
-};
-// c1.accept ("Hello");
-// c2.accept ("Hey there");
 
-Function<String, Integer> funcLen = str -> (str==null ? 0 : str.length());
+    public static void main(String[] args) {
+        
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> evenNumbers = numbers.stream()
+                                            .filter(n -> n % 2 == 0)
+                                            .collect(Collectors.toList());
+        System.out.println(evenNumbers); 
+        
+        List<Person> persons = Arrays.asList(
+            new Person("John", 25, "New York"),
+            new Person("Ilqar", 35, "Baku"),
+            new Person("Jeyhun", 40, "Baku"),
+            new Person("Alice", 30, "London")
+        );
+        List<Person> filteredPersons = persons.stream()
+                                                .filter(p -> p.getAge() > 30 && p.getCity().equals("Baku"))
+                                                .collect(Collectors.toList());
+        System.out.println(filteredPersons); 
+        
+        List<String> strings = Arrays.asList("foo", "bar", "baz");
+        int sumOfLengths = strings.stream()
+                                    .mapToInt(String::length)
+                                    .sum();
+        System.out.println(sumOfLengths); 
+        
+       
+        List<Person> sortedPersons = persons.stream()
+                                                .sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getName))
+                                                .collect(Collectors.toList());
+        System.out.println(sortedPersons); 
+        
+        List<Double> doubles = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0);
+        OptionalDouble average = doubles.stream()
+                                            .mapToDouble(Double::doubleValue)
+                                            .average();
+        System.out.println(average.getAsDouble()); 
+        
+        List<String> filteredAndUppercaseStrings = strings.stream()
+                                                            .filter(s -> !s.startsWith("A"))
+                                                            .map(String::toUpperCase)
+                                                            .collect(Collectors.toList());
+        System.out.println(filteredAndUppercaseStrings); 
+        
+        List<Employee> employees = Arrays.asList(
+            new Employee("Brett", "IT", 2000),
+            new Employee("Kamal", "HR", 800),
+            new Employee("Antony", "Sales", 1500),
+            new Employee("Albert", "HR", 1200)
+        );
 
-Function<String, Integer> funcLen2 = (str) -> {
-    if(str == null)
-        throw new IllegalArgumentException();
-        int res = str.length();
-        return res;
-}; 
+    double totalSalary = employees.stream()
+                                    .filter(e -> e.getDepartment().equals("HR") && e.getSalary() < 1000)
+                                    .mapToDouble(Employee::getSalary)
+                                    .sum();
+    System.out.println(totalSalary); 
 
-// System.out.println(funcLen.apply("Nuraddin"));
-// System.out.println(funcLen2.apply("Nuraddin"));
-
-// System.out.println(funcLen.apply(null));
-// System.out.println(funcLen2.apply(null));
-
-Predicate<Integer> evenLambda = n -> n%2 == 0;
-
-Predicate<String> validWordLambda = s -> s!= null & s.length() > 3;
-
-// System.out.println(validWordLambda.test("abc"));
-// System.out.println(validWordLambda.test("ab"));
-// System.out.println(validWordLambda.test(null));
-// System.out.println(validWordLambda.test("Programming"));
-
-Supplier<Integer> randomIntSupplier = () -> new Random().nextInt(10) + 1;
-
-for(int i = 0; i <10; i++)
-    System.out.println(randomIntSupplier.get());
-
+    List<Integer> integers = Arrays.asList(5, 2, 8, 1, 9, 3, 6);
+    int maxInt = integers.stream()
+                            .max(Integer::compare)
+                            .get();
+    System.out.println(maxInt); 
+    
+    List<String> fruits = Arrays.asList("banana", "orange", "apple", "pear");
+    Optional<String> firstFruitWithApple = fruits.stream()
+                                                    .filter(s -> s.contains("apple"))
+                                                    .findFirst();
+    System.out.println(firstFruitWithApple.get()); 
+    
+    Map<String, Person> personsMap = persons.stream()
+                                                .collect(Collectors.toMap(Person::getName, p -> p));
+    System.out.println(personsMap);
 }
+
 }
