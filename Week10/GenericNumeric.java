604a1ea2eeda0a5ccebdb6db401dc8ad7c2138a4
Suel Heybatova 13641 <123533417+suelheybatova1@users.noreply.github.com>
2023-04-16 21:06:26 +0400

    New ex. added

---
+++ Week10/GenericNumeric.java
@@ -0,0 +1,23 @@
+package Week10;
+
+public class GenericNumeric <T extends Number> {
+    private T number;
+    public GenericNumeric(T number) {
+        this.number = number;
+    }
+    public T getNumber() {
+        return number;
+    }
+    public void setNumber(T number) {
+        this.number = number;
+    }
+    public GenericNumeric<Double> getReciprocal() {
+        return new GenericNumeric<>(1.0 / number.doubleValue());
+    }
+    public GenericNumeric<Double> fractionalPart() {
+        return new GenericNumeric<Double>(number.doubleValue() - number.intValue());
+    }
+    public boolean isAbsolute(GenericNumeric<?> otherValue) {
+        return Math.abs(getNumber().doubleValue()) == Math.abs(otherValue.getNumber().doubleValue());
+    }
+}
---
+++ Week11/Main.java
@@ -0,0 +1,61 @@
+package Week11;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> oddNumbers = (List<Integer>) filter(numbers, (n) -> n % 2 != 0);
+        System.out.println("odd numbers" + oddNumbers);
+
+        List<Point> points = Arrays.asList(new Point(1, 2), new Point(0, 1), new Point(-2, -3), new Point(3, 4));
+        List<Point> pointsInFirstQuadrant = (List<Point>) filter(points, (p) -> p.x > 0 && p.y > 0);
+        System.out.println("points in first quad. " + pointsInFirstQuadrant);
+
+        List<String> strings = Arrays.asList("car run over the  dog", "Hello World", "Give this code to me");
+        List<String> panagram = (List<String>) filter(strings, (s) -> s.toLowerCase().chars().filter(Character::isLetter).distinct().count() == 26);
+        System.out.println("panagram " + panagram);
+
+        List<Person> people = Arrays.asList(new Person("Samir", "Huseynov", 25), new Person("Aflatun", "Smith", 19), new Person("Bob", "Mammadov", 30));
+        List<Person> olderThan20 = (List<Person>) filter(people, (p) -> p.getAge() > 20);
+        System.out.println("people older than 20 " + olderThan20);
+    }
+    public interface SpecificProperty<T> {
+        boolean check(T item);
+    }
+    public static <T> Collection<T> filter(Collection<T> collection, SpecificProperty<T> property) {
+        List<T> result = new ArrayList<>();
+        for (T item : collection) {
+            if (property.check(item)) {
+                result.add(item);
+            }
+        }
+        return result;
+    }
+    public static class Person {
+        private String firstName;
+        private String lastName;
+        private int age;
+        public Person(String firstName, String lastName, int age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+        public String getFirstName() {
+            return firstName;
+        }
+        public String getLastName() {
+            return lastName;
+        }
+        public int getAge() {
+            return age;
+        }
+        @Override
+        public String toString() {
+            return firstName + " " + lastName + " (" + age + ")";
+        }
+    }
+}
