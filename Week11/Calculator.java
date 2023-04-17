7de09325542d1a34cdf9c6dbf48ad0fd62b20444
Leyla16125 <118552639+Leyla16125@users.noreply.github.com>
2023-04-17 22:49:04 +0400

    pw

---
+++ Week11/Calculator.java
@@ -0,0 +1,23 @@
+package Week11;
+
+public class Calculator {
+    interface CalculatorFunction {
+        double calculate(double a, double b);
+    }
+
+    public static void main(String[] args) {
+        CalculatorFunction addition = (a, b) -> a + b;
+        CalculatorFunction subtraction = (a, b) -> a - b;
+        CalculatorFunction multiplication = (a, b) -> a * b;
+        CalculatorFunction division = (a, b) -> a / b;
+        CalculatorFunction exponentiation = (a, b) -> Math.pow(a, b);
+
+
+
+        System.out.println("Addition: " + addition.calculate(5, 10));
+        System.out.println("Subtraction: " + subtraction.calculate(5, 10));
+        System.out.println("Multiplication: " + multiplication.calculate(5, 10));
+        System.out.println("Division: " + division.calculate(5, 10));
+        System.out.println("Exponentiation: " + exponentiation.calculate(5, 10));
+    }
+}
