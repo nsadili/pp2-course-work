979a0a44c05c279ef8aed74b5dc9e433e9bd07a3
vmammadov16238 <118852797+vmammadov16238@users.noreply.github.com>
2023-04-17 23:36:38 +0400

    Done all PWs from Week11

--- Week11/JavaUtilFunction.java
+++ Week11/JavaUtilFunction.java
@@ -1,9 +1,22 @@
-import java.util.*;
+
+import java.util.function.*;
+
 public class JavaUtilFunction {
     public static void main(String[] args) {
         // Costumer -> void
         // Function -> result
         // Predicate -> boolean
         // Supplier -> Taking nothing Returns result (Returning random number etc.)
+
+        Consumer<String> printString = str -> System.out.println(str);
+        printString.accept("Hello World!");
+        Function<Integer, Integer> squareOfIntegers = (Integer n) -> n * n;
+        System.out.println(squareOfIntegers.apply(5));
+        Predicate<String> isEmpty = str -> str.isEmpty();
+        boolean result = isEmpty.test("");
+        System.out.println(result);
+        Supplier<Integer> randomInt = () -> (int) (Math.random() * 1000);
+        int result1 = randomInt.get(); 
+        System.out.println(result1);
     }
 }
