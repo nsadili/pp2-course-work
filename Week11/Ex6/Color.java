34deb820334c759d2b3551226b16541fd360e108
rufat_hajizada_16231 <rufeth04@gmail.com>
2023-04-17 21:50:54 +0400

    almost done

---
+++ Week11/Ex6/Color.java
@@ -0,0 +1,29 @@
+package Week11.Ex6;
+
+public class Color {
+    int r,g,b;
+
+    public Color(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle [r=" + r + ", g=" + g + ", b=" + b + "]";
+    }
+    
+}
---
+++ Week11/Ex6/StreamDemo.java
@@ -0,0 +1,60 @@
+package Week11.Ex6;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StreamDemo {
+    public static void main(String[] args) {
+        String[] names = { "Shamil", "Vusal", "Leyla", "Elvin", "Vugar" };
+        Stream<String> namesStream = Arrays.stream(names);
+
+        Set<Integer> set = new HashSet<>(Arrays.asList( 6, 7, 3, 9, 1, 2, 8, 4, 5));
+        Stream<Integer> setStream = set.stream();
+
+        List<Color> colors = Arrays.asList(
+            new Color(255, 255, 255),
+            new Color(0, 0, 0),
+            new Color(127, 127, 127),
+            new Color(0, 0, 255));
+        Stream<Color> colorsStream = colors.stream();
+
+        // System.out.println(namesStream.allMatch(s -> s.startsWith("V")));
+        // System.out.println(namesStream.anyMatch(s -> s.startsWith("V")));
+        // System.out.println(namesStream.noneMatch(s -> s.startsWith("V")));
+
+        // System.out.println(setStream.count());
+
+        Random randGenerator = new Random();
+        Supplier<Integer> rand = ()->randGenerator.nextInt(10)+1;
+        Stream<Integer> randomStream = Stream.generate(rand).limit(5);
+        // randomStream.forEach(System.out::println);
+
+        Optional<Color> op = colorsStream.findAny();
+        // System.out.println(op.get()); 
+
+        // System.out.println(namesStream.findFirst());
+
+        // System.out.println(
+        //     op.isPresent()
+        //         ? op.get() 
+        //         : "Not found" 
+        // );
+
+        // setStream.filter(n->n%2!=0).forEach(System.out::println);
+
+        // System.out.println(setStream.filter(n->n%2!=0).filter(n->n>4).count());
+
+        // setStream.filter(n->n%2!=0).sorted().forEach(System.out::println);
+
+        setStream.filter(n->n%2!=0).sorted().map(n->n+2).forEach(System.out::println);
+
+
+
+    }
+}
