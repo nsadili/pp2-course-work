b2bf2873436b4707d98724b7456456d593b21be3
Shamil Abbasov 16084 <sabbasov16084@ada.edu.az>
2023-04-19 16:50:44 +0400

    Copying the text

--- Week12/Week 12 - Incomplete/DuplicateFiles.java
+++ Week12/Week 12 - Incomplete/DuplicateFiles.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class DuplicateFiles {
     public static void main(String[] args) {
@@ -12,8 +15,36 @@ public class DuplicateFiles {
 
         // TODO
 
+        copy(source, dest);
+
+
+
+
         end = System.currentTimeMillis();
         System.out.println("Total duration: " + (end - start));
+    }
+
+    static void copy(File source, File dest){
+
+        if(!source.exists()) return ;
+
+        try (FileInputStream fis = new FileInputStream(source);
+            FileOutputStream fos = new FileOutputStream(dest)
+        ) {
+            byte[] buffer = new byte[8];
+            int len;
+            while((len = fis.read(buffer)) > 0){
+                fos.write(buffer, 0, len);
+
+            }
+
+
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+
+
     }
 
 }
