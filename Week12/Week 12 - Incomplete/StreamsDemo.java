1c6c948cf26d5f02b0bf262c8a111fdb78d6ceaf
Shamil Abbasov 16084 <sabbasov16084@ada.edu.az>
2023-04-19 16:36:02 +0400

    StreamsDemo added

--- Week12/Week 12 - Incomplete/StreamsDemo.java
+++ Week12/Week 12 - Incomplete/StreamsDemo.java
@@ -1,19 +1,68 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class StreamsDemo {
     public static void main(String[] args) {
-        String fileName = "Resources/ex1/data1.dat";
+        String fileName = "Resources/ex1/data1.txt";
         String data = "Programming Principles II with JavA";
 
         // writeData(fileName, data);
-        // readData(fileName);
+        System.out.println(readDataV2(fileName));;
 
     }
 
     static void writeData(String path, String data) {
         // TODO
+        try (FileOutputStream file = new FileOutputStream(path, false)) {
+
+            file.write(data.getBytes());
+            file.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    static void readData(String path) {
+    static void readDataV1(String path) {
         // TODO
+
+        try (FileInputStream fis = new FileInputStream(path)) {
+
+            int ch;
+            while ((ch = fis.read()) != -1) {
+                System.out.print((char) ch);
+            }
+            System.out.println();
+
+        } catch (IOException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+    static String readDataV2(String path) {
+        // TODO
+        StringBuilder res = new StringBuilder();
+        try (FileInputStream fis = new FileInputStream(path)) {
+
+           byte[] arr = new byte[8];
+            // fis.read(arr);
+            int len;
+            while( (len = fis.read(arr)) > 0){
+            //    System.out.print(new String(arr, 0, len));
+                res.append(new String(arr, 0, len ));
+          
+            //  System.out.print(new String(arr));               
+
+            }
+
+        } catch (IOException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return res.toString();
     }
 }
