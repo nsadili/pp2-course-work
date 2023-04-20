5be81285d0a70c7581b180b347ae6ad253d3ada1
Shamil Abbasov 16084 <sabbasov16084@ada.edu.az>
2023-04-20 18:30:20 +0400

    SerializableDemo added

--- Week12/Week 12 - Incomplete/SerializablaDemo.java
+++ Week12/Week 12 - Incomplete/SerializablaDemo.java
@@ -1,5 +1,15 @@
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 
-class Person {
+class Person implements Serializable {
     int id;
     String fname;
     String lname;
@@ -31,19 +41,53 @@ public class SerializablaDemo {
     public static void main(String[] args) {
         String filename = "Resources/ex1/persons.dat";
         Person[] persons = new Person[] { new Person(1, "Ali", "Aliyev"), new Person(2, "Muhammed", "Faud"),
-                new Person(3, "Samir", "Musayev") };
+                new Person(3, "Samir", "Musayev"), new Person(4, "Shamil", "Abbasov") };
 
-        // writeData(persons, filename);
-        // readData(filename);
+        writeData(persons, filename);
+        var personArr = readData(filename);
+       for(var x: personArr) System.out.println(x);
+
     }
 
     static void writeData(Person[] persons, String path) {
-        // TODO
+        try (
+                FileOutputStream fis = new FileOutputStream(path);
+                ObjectOutputStream ous = new ObjectOutputStream(fis)) {
+
+            for (var i : persons) {
+                ous.writeObject(i);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    static void readData(String path) {
-        // TODO
+    static Person[] readData(String path) {
+        List<Person> persons = new LinkedList<>();
+        try (FileInputStream fis = new FileInputStream(path);
+                ObjectInputStream os = new ObjectInputStream(fis)) {
+            Object readed = null;
 
+            while (os.available() >= 0) {
+                // System.out.println("Burdayam");
+                readed = os.readObject();
+                persons.add((Person) readed);
+                System.out.println(readed);
+            }
+        }
+
+        catch (EOFException e) {
+            // System.out.println(e.getClass().getName() );
+            // System.out.println(e.getCause());
+            System.out.println("End OF File! EOF exception catched");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        
+        return persons.toArray(new Person[persons.size()]);
     }
 
 }
