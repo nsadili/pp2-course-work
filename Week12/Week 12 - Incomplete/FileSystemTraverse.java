1b0ecb5b2a988e34cd00205c3226defe4bed5e72
Shamil Abbasov 16084 <sabbasov16084@ada.edu.az>
2023-04-20 20:37:11 +0400

    FileSYstemTraverse added

--- Week12/Week 12 - Incomplete/FileSystemTraverse.java
+++ Week12/Week 12 - Incomplete/FileSystemTraverse.java
@@ -1,4 +1,7 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class FileSystemTraverse {
     public static void main(String[] args) {
@@ -8,23 +11,80 @@ public class FileSystemTraverse {
         }
 
         String filePath = args[0]; // first argument from command line
+        // String filePath = "C:\\Users\\TUF DASH\\Documents\\PP2\\contest";
+        // String filePath = "C:\\Users\\TUF DASH\\Documents\\PP2\\contest\\text.txt";
+        // String filePath = "C:\\Users\\TUF DASH\\Documents\\PP2\\contest\\qovluq1";
+        // String filePath = "C:\\Users\\TUF
+        // DASH\\Documents\\PP2\\contest\\qovluq1\\file1inqovluq1.txt";
+        // String filePath = "Resources";
 
         File rootFile = new File(filePath);
 
-        if (!rootFile.exists()) {
-            System.out.println("Specified file/directory does not exist!");
-            return;
-        }
+        // if (!rootFile.exists()) {
+        //     System.out.println("Specified file/directory does not exist!");
+        //     System.out.println("To create specified File enter 1:");
+        //     Scanner sc = new Scanner(System.in);
+        //     var x = sc.nextInt();
+        //     if (x == 1) {
+        //         try {
+        //             rootFile.createNewFile();
+        //             System.out.println("Path is created");
+        //         } catch (IOException e) {
+        //             e.printStackTrace();
+        //         }
+        //     } else
+        //         return;
+
+        // }
 
         recPrint(rootFile);
 
     }
 
     static void recPrint(File rootPath) {
-        // TODO
+        if (rootPath.isFile()) {
+            printInfo(rootPath);
+            return;
+        } else if (rootPath.isDirectory()) {
+
+            System.out.println("Path is Directory -> " + rootPath.getName());
+
+            String[] arr = rootPath.list();
+            if (arr == null)
+                return;
+            System.out.println("Information for sub-files of <" + rootPath.getName() + ">");
+
+            for (String path : arr) {
+                // System.out.println("printin path: " + path);
+                File newDir = new File(rootPath, path);
+
+                // System.out.print("Printing: "+newDir +" " + arr.length);
+                // System.out.println();
+                // System.out.println("Absolute path:" + newDir.getAbsolutePath());
+
+                recPrint(newDir);
+            }
+            System.out.println("END of Directory <" + rootPath.getName() + ">");
+
+        }
+
     }
 
     static void printInfo(File path) {
-        // TODO
+
+        if (path.isFile())
+            System.out.println("Path is file -> " + path.getName());
+        // System.out.println("Name:" + path.getName());
+        System.out.println("Absolute Path:" + path.getAbsolutePath());
+        System.out.println("Parent folder:" + path.getParent());
+
+        // path.delete();
+
+        // var x= path.getParent();
+        // File parent = new File(x);
+        // System.out.println(path.list());
+        // var arr = parent.listFiles();
+        // Arrays.stream(arr).forEach((a) -> System.out.print(a.getName() + ", "));
+
     }
 }
