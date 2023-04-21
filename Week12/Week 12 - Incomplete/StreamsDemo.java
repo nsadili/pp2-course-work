047c38fd3361f626c20ee08d5b1c4328af04cc07
jala-asadli-16143 <jasadli16143@ada.edu.az>
2023-04-21 17:22:03 +0400

    Add exe 2 and 3

---
+++ Week12/Week 12 - Incomplete/StreamsDemo.java
@@ -0,0 +1,51 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class StreamsDemo {
+    public static void main(String[] args) {
+        String filePath = "C:/Users/Lenovo/pp2-course-work/Week12/Week 12 - Incomplete/Resources/ex1/data1.txt";
+        String data = "Programming Principles II with Java <3";
+
+        writeData(filePath, data);
+        readData(filePath);
+        System.out.println(readAndReturnData(filePath));
+
+    }
+
+    static void writeData(String path, String data) {
+        try (FileOutputStream file = new FileOutputStream(path)) {
+
+            file.write(data.getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void readData(String path) {
+        try (FileInputStream fos = new FileInputStream(path)) {
+            int ch;
+            while ((ch = fos.read()) > 0) {
+                System.out.print((char) ch);
+            }
+            System.out.println();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    static String readAndReturnData(String path) {
+        StringBuilder str = new StringBuilder();
+        try (FileInputStream fos = new FileInputStream(path)) {
+            int len;
+            byte buffer[] = new byte[8];
+            while ((len = fos.read(buffer)) > 0)
+                str.append(new String(buffer, 0, len));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str.toString();
+    }
+}
