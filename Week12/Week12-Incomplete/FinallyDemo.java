b66cd833d0227ed6836ae305ab72fb0ca14b3870
rufat_hajizada_16231 <rufeth04@gmail.com>
2023-04-21 14:58:38 +0400

    duplicate also done

---
+++ Week12/Week12-Incomplete/FinallyDemo.java
@@ -0,0 +1,31 @@
+public class FinallyDemo {
+    public static void main(String[] args) {
+        try {
+            testFinally();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void testFinally() throws Exception {
+        try {
+            throw new ExceptionDemo1();
+        } catch (ExceptionDemo1 e) {
+            throw new ExceptionDemo2();
+        } finally {
+            throw new ExceptionDemo3();
+        }
+    }
+}
+
+class ExceptionDemo1 extends Exception {
+}
+
+class ExceptionDemo2 extends Exception {
+}
+
+class ExceptionDemo3 extends Exception {
+}
+
+class ExceptionDemo4 extends Exception {
+}
