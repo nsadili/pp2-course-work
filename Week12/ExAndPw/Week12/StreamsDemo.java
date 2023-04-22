dbffa475e655b556e24f447b4ac1236d9735db2e
Ibrahim Huseynov <ihuseynov16086@ada.edu.az>
2023-04-22 19:13:03 +0400

    Add week12 ex

--- Week12/ExAndPw/Week12/StreamsDemo.java
+++ Week12/ExAndPw/Week12/StreamsDemo.java
@@ -1,19 +1,53 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class StreamsDemo {
     public static void main(String[] args) {
-        String fileName = "Resources/ex1/data1.dat";
+        String filePath = "Week12/Resources/ex1/data1.txt";
         String data = "Programming Principles II with JavA";
 
-        // writeData(fileName, data);
-        // readData(fileName);
+        // writeData(filePath, data);
+        //eadData(filePath);
+        System.out.println(readData(filePath));
 
     }
 
     static void writeData(String path, String data) {
-        // TODO
+       
+    try(FileOutputStream fos = new FileOutputStream(path)){
+        fos.write(data.getBytes());
+       
+         
+            
+       }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
-    static void readData(String path) {
-        // TODO
+    static String readData(String path) {
+        StringBuilder sb= new StringBuilder();
+        try (var fis = new FileInputStream(path)) {
+            //int b;
+            //while((b=fis.read())!=-1)
+            //System.out.print((char) b);
+
+            byte[] buf= new byte[8];
+            int len;
+            
+            while((len=fis.read(buf)) > 0)
+            sb.append(new String(buf,0,len));
+
+
+
+
+        } catch (IOException e) {
+          
+            e.printStackTrace();
+        }
+        return sb.toString();
+
+        
     }
 }
