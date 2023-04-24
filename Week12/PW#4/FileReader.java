5b7eae709c8c0fdd6ec9366a43535028ad524520
ravanabdinli <118709878+ravanabdinli@users.noreply.github.com>
2023-04-24 17:54:14 +0400

    Create FileReader.java

---
+++ Week12/PW#4/FileReader.java
@@ -0,0 +1,38 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileExample {
+  public static void main(String[] args) {
+    try {
+      FileReader reader = new FileReader("output.txt");
+      int character;
+      String content = "";
+
+      // Read file using read() method
+      while ((character = reader.read()) != -1) {
+        content += (char) character;
+      }
+      System.out.println("File content using read() method:");
+      System.out.println(content);
+
+      // Reset reader
+      reader.close();
+      reader = new FileReader("output.txt");
+
+      // Read file using read(char[]) method
+      char[] buffer = new char[1024];
+      int length;
+      content = "";
+      while ((length = reader.read(buffer)) != -1) {
+        content += new String(buffer, 0, length);
+      }
+      System.out.println("File content using read(char[]) method:");
+      System.out.println(content);
+
+      reader.close();
+    } catch (IOException e) {
+      System.out.println("An error occurred while reading the file.");
+      e.printStackTrace();
+    }
+  }
+}
