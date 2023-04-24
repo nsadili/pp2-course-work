dcdff1fc5a8521830333afe24b5552cbf6c60070
NarimanMammadov <nmammadov16237@ada.edu.az>
2023-04-24 19:21:06 +0400

    serial

--- Week12/Week 12 - Incomplete/SerializablaDemo.java
+++ Week12/Week 12 - Incomplete/SerializablaDemo.java
@@ -1,5 +1,56 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-class Person {
+public class SerializablaDemo {
+    public static void main(String[] args) {
+        String filename = "Resources/ex1/persons.dat";
+        Person[] persons = new Person[] { new Person(1, "Ali", "Aliyev"), new Person(2, "Muhammed", "Faud"),
+                new Person(3, "Samir", "Musayev") };
+
+        writeData(persons, filename);
+        readData(filename);
+    }
+
+    static void writeData(Person[] persons, String path) {
+        File file = new File(path);
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+            for(Person p: persons){
+                oos.writeObject(p);  
+
+            }
+            oos.writeObject(new EndOfFileIndicatorObject());
+        } catch (IOException e) {
+        e.printStackTrace();
+        }
+    }
+
+    static void readData(String path) {
+        File file = new File(path);
+        Object o;
+        Person p;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))){
+            o = ois.readObject();
+
+            while(!(o instanceof EndOfFileIndicatorObject)){
+                p = (Person) o;
+                System.out.println(p);
+                o = ois.readObject();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+class EndOfFileIndicatorObject implements Serializable{
+}
+class Person implements Serializable{
     int id;
     String fname;
     String lname;
@@ -25,25 +76,4 @@ class Person {
     public String toString() {
         return "Person[id = " + id + ", fname = " + fname + ", lname = " + lname + "]";
     }
-}
-
-public class SerializablaDemo {
-    public static void main(String[] args) {
-        String filename = "Resources/ex1/persons.dat";
-        Person[] persons = new Person[] { new Person(1, "Ali", "Aliyev"), new Person(2, "Muhammed", "Faud"),
-                new Person(3, "Samir", "Musayev") };
-
-        // writeData(persons, filename);
-        // readData(filename);
-    }
-
-    static void writeData(Person[] persons, String path) {
-        // TODO
-    }
-
-    static void readData(String path) {
-        // TODO
-
-    }
-
 }
