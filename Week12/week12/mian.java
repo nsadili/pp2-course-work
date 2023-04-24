baeb5ed7867ab2c06de10f010018e4543eaf1b8b
Ismayil_Ismayilov_18008 <118397214+iismayilov18008@users.noreply.github.com>
2023-04-25 01:41:10 +0400

    Add files via upload

---
+++ Week12/week12/mian.java
@@ -0,0 +1,7 @@
+package week12;
+public class mian{
+public static void main(String[] args) {
+    FileInfo.printFileInfo("example.txt");
+    FileInfo.printDirInfoRecursive("example-directory");
+}
+}
---
+++ Week12/week12/person.java
@@ -0,0 +1,41 @@
+package week12;
+
+import java.io.*;
+
+public class person {
+    public static void savePersons(person[] persons, String fileName) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(persons);
+            objectOutputStream.close();
+            fileOutputStream.close();
+            System.out.println("Successfully saved persons to file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving persons to file: " + e.getMessage());
+        }
+    }
+
+    public static void readPersons(String fileName) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            person[] persons = (person[]) objectInputStream.readObject();
+            while (persons != null) {
+                for (person person : persons) {
+                    System.out.println(person.toString());
+                }
+                persons = (person[]) objectInputStream.readObject();
+            }
+
+            objectInputStream.close();
+            fileInputStream.close();
+        } catch (EOFException e) {
+            System.out.println("End ");
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(" error " + e.getMessage());
+        }
+    }
+}
+
