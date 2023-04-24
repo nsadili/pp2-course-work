6942a64b7a65c5db427977757bdb91e9523fc7ab
Falibayli <118827299+Falibayli@users.noreply.github.com>
2023-04-24 19:08:15 +0400

    Add files via upload

---
+++ Week12/DuplicateFiles.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class DuplicateFiles {
+    public static void main(String[] args) {
+
+        long start, end;
+
+        File source = new File("Resources\\forCopy\\dump.txt");
+        File dest = new File("Resources\\forCopy\\copy-dump.txt");
+
+        start = System.currentTimeMillis();
+
+        copyFile(source, dest);
+
+        end = System.currentTimeMillis();
+        System.out.println("Total duration: " + (end - start));
+    }
+
+    static void copyFile(File source, File dest) {
+        try (InputStream is = new FileInputStream(source);
+             OutputStream os = new FileOutputStream(dest)) {
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+
+            System.out.println("File copied successfully");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
