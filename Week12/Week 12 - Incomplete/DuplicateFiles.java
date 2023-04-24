85c97034ec98f5cf9c621500441e832bcefbc74d
Ravan Mehraliyev 16226 <rmehraliyev16226@ada.edu.az>
2023-04-24 17:46:04 +0400

    Pw, Ex and Bonus - Done!

---
+++ Week12/Week 12 - Incomplete/DuplicateFiles.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DuplicateFiles {
+    public static void main(String[] args) {
+
+        long start, end;
+
+        File source = new File("Week12/Resources/forCopy/Martin Iden - Jack London.txt");
+        File dest = new File("Week12/Resources/forCopy/copy-Martin Iden - Jack London.txt");
+
+        start = System.currentTimeMillis();
+        CopyFile(source, dest);
+
+        // TODO
+
+        end = System.currentTimeMillis();
+        System.out.println("Total duration: " + (end - start));
+    }
+    static void CopyFile(File src, File dst){
+        try (var fis = new FileInputStream(src);
+               var fos= new FileOutputStream(dst)) {
+
+            byte[] buff= new byte[1024];
+            int len;
+            while((len= fis.read(buff))>0)
+               fos.write(buff,0,len);
+
+        } catch (IOException e) {
+           
+            e.printStackTrace();
+        }
+
+    }
+
+}
