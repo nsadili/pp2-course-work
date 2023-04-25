bc4b8a866edfe4442c6251a420ade1beb123e290
Seljan Mamedova <118668057+SeljanMamedova@users.noreply.github.com>
2023-04-25 14:50:47 +0400

    Week 12 added

---
+++ Week12/Week 12/FinallyDemo.java
@@ -0,0 +1,35 @@
+public class FinallyDemo {
+    public static void main(String[] args) {
+        try {
+            testFinally();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getClass().getName());
+        }
+    }
+
+    static void testFinally() throws Exception {
+        try {
+            throw new ExceptionDemo4();
+        } catch (ExceptionDemo4 e) {
+            throw new ExceptionDemo2();
+            // System.out.println("In catch");
+        } finally {
+            // System.out.println("finally");
+            throw new ExceptionDemo3();
+           
+        }
+    }
+}
+
+class ExceptionDemo1 extends Exception {
+}
+
+class ExceptionDemo2 extends Exception {
+}
+
+class ExceptionDemo3 extends Exception {
+}
+
+class ExceptionDemo4 extends Exception {
+}
---
+++ Week12/Week 12/SerializablaDemo.java
@@ -0,0 +1,92 @@
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+class Person implements Serializable {
+    int id;
+    String fname;
+    String lname;
+
+    public Person(int id, String fname, String lname) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String toString() {
+        return "Person[id = " + id + ", fname = " + fname + ", lname = " + lname + "]";
+    }
+}
+
+public class SerializablaDemo {
+    public static void main(String[] args) {
+        String filename = "Resources/ex1/persons.dat";
+        Person[] persons = new Person[] { new Person(1, "Zumrud", "Alizade"), new Person(2, "Sabina", "Agayeva"),
+                new Person(3, "Sabir", "Alizade"), new Person(4, "Fariz", "Abiyev") };
+
+        writeData(persons, filename);
+        var personArr = readData(filename);
+       for(var x: personArr) System.out.println(x);
+
+    }
+
+    static void writeData(Person[] persons, String path) {
+        try (
+                FileOutputStream fis = new FileOutputStream(path);
+                ObjectOutputStream ous = new ObjectOutputStream(fis)) {
+
+            for (var i : persons) {
+                ous.writeObject(i);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static Person[] readData(String path) {
+        List<Person> persons = new LinkedList<>();
+        try (FileInputStream fis = new FileInputStream(path);
+                ObjectInputStream os = new ObjectInputStream(fis)) {
+            Object readed = null;
+
+            while (os.available() >= 0) {
+               
+                readed = os.readObject();
+                persons.add((Person) readed);
+                System.out.println(readed);
+            }
+        }
+
+        catch (EOFException e) {
+            
+            System.out.println("End OF File! EOF exception catched");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        
+        return persons.toArray(new Person[persons.size()]);
+    }
+
+}
