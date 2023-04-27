ae75101438f74ac2cddc61142de14387c5af856b
saidaimanova1 <118837556+saidaimanova1@users.noreply.github.com>
2023-04-27 11:18:16 +0400

    Add files via upload

---
+++ Week02/Primes.java
@@ -0,0 +1,29 @@
+package Week02;
+import java.util.Scanner;
+
+public class Primes {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int num = scanner.nextInt();
+
+            boolean isPrime = true;
+
+            if (num <= 1) {
+                isPrime = false;
+            } else {
+                for (int i = 2; i <= Math.sqrt(num); i++) {
+                    if (num % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(num + " is prime");
+            } else {
+                System.out.println(num + " is not prime");
+            }
+        }
+    }
+}
---
+++ Week02/RollingDie.java
@@ -0,0 +1,21 @@
+package Week02;
+import java.util.Random;
+
+public class RollingDie {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int num = rand.nextInt(8) + 1;
+        
+        if (num <= 4) {
+            System.out.println(num);
+        } else {
+            int rollAgain = rand.nextInt(2);
+            if (rollAgain == 0) {
+                System.out.println(5);
+            } else {
+                System.out.println(6);
+            }
+        }
+    }
+}
+
