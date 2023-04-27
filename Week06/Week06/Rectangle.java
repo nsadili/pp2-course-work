f70ad746e2156678d0fef8e2bec774ae9bc44467
LamanAliyeva <118842996+LamanAliyeva@users.noreply.github.com>
2023-04-27 13:37:19 +0400

    Add files via upload

---
+++ Week06/Week06/Rectangle.java
@@ -0,0 +1,65 @@
+package Week06;
+
+public class Rectangle extends Shape implements Cloneable{
+    public double width = 1.0;
+    public double length = 1.0;
+
+    public Rectangle() {}
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public boolean equals(Object obj) {
+        Rectangle rect = (Rectangle)obj;
+        return rect.length == this.length && rect.width == this.width;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                "} " + super.toString();
+    }
+}
