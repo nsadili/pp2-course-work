a9e15fd9bfc5868aba2fd91d8914227b7d48109f
Javid_Ibadov <cibadov13097@ada.edu.az>
2023-05-01 01:32:20 +0400

    yes

---
+++ Week12/Serialization.java
@@ -0,0 +1,63 @@
+
+import java.io.Serializable;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Serialization {
+    public static class Person implements Serializable {
+        private int Age;
+        private String Name;
+
+
+        public Person(String name, int age) {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public String getName() {
+            return Name;
+        }
+
+        public int getAge() {
+            return Age;
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String file = "C:/Users/user/pp2-course-work/Week12.txt";
+        Person[] people = {
+                new Person("Javid", 17),
+                new Person("tom", 20) ,
+                new Person("james", 50) };
+
+        writeInfo(people, file);
+        readInfo(file);
+    }
+
+    public static void writeInfo(Person[] people, String file) throws IOException {
+
+        try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(file)))
+        {
+            output.writeObject(people);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readInfo(String file) throws IOException, ClassNotFoundException {
+
+        try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(file))) {
+            try {
+                Person person = (Person) input.readObject();
+                System.out.println(person.getName() + ", " + person.getAge());
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
---
+++ Week12/Week 12 - Incomplete/StreamsDemo.java
@@ -0,0 +1,19 @@
+
+public class StreamsDemo {
+    public static void main(String[] args) {
+        String fileName = "Resources/ex1/data1.dat";
+        String data = "Programming Principles II with JavA";
+
+        // writeData(fileName, data);
+        // readData(fileName);
+
+    }
+
+    static void writeData(String path, String data) {
+        // TODO
+    }
+
+    static void readData(String path) {
+        // TODO
+    }
+}
