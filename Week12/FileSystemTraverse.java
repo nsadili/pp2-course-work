fea216ccc16f41efa899be9a6cb574d6ff8e8f2a
Seljan Musayeva <smusayeva17601@ada.edu.az.com>
2023-05-01 15:38:53 +0400

    Completed

---
+++ Week12/FileSystemTraverse.java
@@ -0,0 +1,42 @@
+import java.io.File;
+
+public class FileSystemTraverse {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Enter a path!");
+            return;
+        }
+
+        String filePath = args[0]; // first argument from command line
+
+        File rootFile = new File(filePath);
+
+        if (!rootFile.exists()) {
+            System.out.println("Given file/directory doesnt exist");
+            return;
+        }
+
+        recPrint(rootFile);
+
+    }
+
+    static void recPrint(File root) {
+        if (root == null)
+            return;
+
+        if (root.isFile()) {
+            System.out.printf("file:  %s %s\n", root.getName(), root.getAbsolutePath());
+            return;
+        }
+
+        System.out.printf("dir(%d) %s %s\n", root.list().length, root.getName(), root.getAbsolutePath());
+
+        for (File subFile : root.listFiles()) {
+            recPrint(subFile);
+        }
+    }
+
+    static void printInfo(File path) {
+        // TODO
+    }
+}
