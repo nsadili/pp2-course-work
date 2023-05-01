0547dccccf8baabf49f61b92b99a56435cd91a21
Tofiq041 <119070294+Tofiq041@users.noreply.github.com>
2023-05-01 22:13:18 +0400

    b.

---
+++ Week13/problem1/WordCounterWithoutConcurrency.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class WordCounterWithoutConcurrency {
+    public static void main(String[] args) throws IOException {
+        String fileName = "input.txt"; // replace with the name of your file
+        int count = 0;
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] words = line.split("\\s+");
+            for (String word : words) {
+                if (word.length() > 3) {
+                    count++;
+                }
+            }
+        }
+        reader.close();
+        System.out.println("Total number of words: " + count);
+    }
+}
