3baf9d039ceb6708480eba805ab032b88a8fbea8
roya alizada 16092 <ralizada16092@ada.edu.az>
2023-05-01 18:09:26 +0400

    Week13

---
+++ Week13/problem1/single/SingleProcessor.java
@@ -0,0 +1,24 @@
+package problem1.single;
+
+import java.util.StringTokenizer;
+
+public class SingleProcessor {
+	private String text;
+
+	public SingleProcessor(String text) {
+		this.text = text;
+	}
+
+	public int count() {
+		int count = 0;
+		StringTokenizer st = new StringTokenizer(text);
+
+		while (st.hasMoreTokens()) {
+			String word = st.nextToken();
+			if (word.length() > 3) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
