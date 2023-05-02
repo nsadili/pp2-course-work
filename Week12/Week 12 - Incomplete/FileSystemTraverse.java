091c06f6655c09af421ccac83951ae6ea8c1b751
jala-asadli-16143 <jasadli16143@ada.edu.az>
2023-05-02 20:48:56 +0400

    Completed files

--- Week12/Week 12 - Incomplete/FileSystemTraverse.java
+++ Week12/Week 12 - Incomplete/FileSystemTraverse.java
@@ -20,8 +20,20 @@ public class FileSystemTraverse {
 
     }
 
-    static void recPrint(File rootPath) {
-        // TODO
+    static void recPrint(File root) {
+        if (root == null)
+            return;
+
+        if (root.isFile()) {
+            System.out.printf("file:  %s %s\n", root.getName(), root.getAbsolutePath());
+            return;
+        }
+
+        System.out.printf("dir(%d) %s %s\n", root.list().length, root.getName(), root.getAbsolutePath());
+
+        for (File subFile : root.listFiles()) {
+            recPrint(subFile);
+        }
     }
 
     static void printInfo(File path) {
