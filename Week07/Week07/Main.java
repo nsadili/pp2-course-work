b851621a34b2822d02e9c4e61e1293d15a6b1942
LamanAliyeva <118842996+LamanAliyeva@users.noreply.github.com>
2023-05-02 20:43:59 +0400

    Add files via upload

---
+++ Week07/Week07/Main.java
@@ -0,0 +1,34 @@
+package Week07;
+
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        // Invoice
+//        Invoice inv = null;
+//        try {
+//            inv = new Invoice("123", "Desc", -5, 5);
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Value cannot be negative");
+//        }
+
+        // Account
+//        Account acc = new Account(14129, 3000);
+//        try {
+//            acc.withdraw(3500);
+//        } catch (InvalidAmountException e) {
+//            System.out.println("Amount cannot be more than the balance.");
+//        }
+
+//        Homework 1 (non-graded)
+//        try {
+//            int a = System.in.read();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+
+
+    }
+}
