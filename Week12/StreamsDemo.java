a39488f370a7650fe42387a86c312a84cd12455b
IbrahimIskandarov <119100568+IbrahimIskandarov@users.noreply.github.com>
2023-05-03 20:35:07 +0400

    Add files via upload

---
+++ Week12/StreamsDemo.java
@@ -0,0 +1,41 @@
+package week12codes;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class StreamsDemo {
+    public static void main(String[] args) {
+        String data = "A data of string";
+
+        try(FileOutputStream fos = new FileOutputStream("Week12/output.txt")) {
+            fos.write(data.getBytes());
+        } catch (IOException a) {
+            System.out.println(a);
+        }
+        String content = "";
+        try(FileInputStream fis = new FileInputStream("Week12/output.txt")) {
+            int ch;
+            while((ch = fis.read()) != -1) {
+                content += (char)ch;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        finally {
+            System.out.println(content);
+        }
+
+
+        try(FileInputStream fis = new FileInputStream("Week12/output.txt")) {
+            byte[] array = new byte[10];
+            fis.read(array);
+            content = new String(array, 0, array.length);
+        } catch (IOException a) {
+            System.out.println(a);
+        }
+        finally {
+            System.out.println(content);
+        }
+    }
+}
