ed83aff21df63e44fa3e5748e61b7b196dafdf9f
IbrahimIskandarov <119100568+IbrahimIskandarov@users.noreply.github.com>
2023-05-03 19:42:05 +0400

    Add files via upload

---
+++ Week10/GenericClass.java
@@ -0,0 +1,35 @@
+package week10codes;
+
+public class GenericClass <U, V> {
+    private U key;
+    private V value;
+
+    public GenericClass(U key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public GenericClass() {
+    }
+
+    public U getKey() {
+        return this.key;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+
+    public void setKey(U key) {
+        this.key = key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "GenericClass{" + "key=" + key + ", value=" + value + '}';
+    }
+}
---
+++ Week10/GenericNumeric.java
@@ -0,0 +1,32 @@
+package week10codes;
+
+public class Numeric <A extends Number> {
+    private A value;
+
+    public A getValue() {
+        return value;
+    }
+
+    public Numeric(A value) {
+        this.value = value;
+    }
+
+    public double reciprocal() {
+        return (1 / value.doubleValue());
+    }
+
+    public double fractionalPart() {
+        return value.doubleValue() - value.intValue();
+    }
+
+    public boolean isAbsolute(Numeric<?> otherValue) {
+        return Math.abs(value.doubleValue()) == Math.abs(otherValue.getValue().doubleValue());
+    }
+}
+
+class Main2 {
+    public static void main(String[] args) {
+        Numeric<Double> numericValue1 = new Numeric<>(5.7983);
+        System.out.println(numericValue1.fractionalPart());
+    }
+}
