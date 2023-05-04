b133296dca7cec17b8e29b6cf36e291a522368d1
LamanAliyeva <118842996+LamanAliyeva@users.noreply.github.com>
2023-05-04 21:51:56 +0400

    Add files via upload

---
+++ Week09/Week09/FriendsMap.java
@@ -0,0 +1,51 @@
+package Week09;
+
+import java.util.*;
+
+public class FriendsMap {
+    private HashMap<String, List<String>> friendsMap;
+
+    public FriendsMap() {
+        friendsMap = new HashMap<>();
+    }
+
+    public void addFriendPair(String friend1, String friend2) {
+        if (!friendsMap.containsKey(friend1)) {
+            friendsMap.put(friend1, new ArrayList<>());
+        }
+        if (!friendsMap.containsKey(friend2)) {
+            friendsMap.put(friend2, new ArrayList<>());
+        }
+        friendsMap.get(friend1).add(friend2);
+        friendsMap.get(friend2).add(friend1);
+    }
+
+    public void removeFriendPair(String friend1, String friend2) {
+        if (friendsMap.containsKey(friend1) && friendsMap.containsKey(friend2)) {
+            friendsMap.get(friend1).remove(friend2);
+            friendsMap.get(friend2).remove(friend1);
+            if (friendsMap.get(friend1).isEmpty()) {
+                friendsMap.remove(friend1);
+            }
+            if (friendsMap.get(friend2).isEmpty()) {
+                friendsMap.remove(friend2);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "FriendsMap{" +
+                "friendsMap=" + friendsMap +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        FriendsMap map = new FriendsMap();
+        map.addFriendPair("Ali", "Sadi");
+        map.addFriendPair("Sadi", "Togrul");
+        System.out.println(map.friendsMap);
+        map.removeFriendPair("Ali", "Sadi");
+        System.out.println(map.friendsMap);
+    }
+}
