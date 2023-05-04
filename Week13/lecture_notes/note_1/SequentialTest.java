8ebc722832be9883020171da39ed134858fd48c8
AliyevaVasila <vasilaaliyeffa@gmail.com>
2023-05-04 22:56:56 +0300

    codes

---
+++ Week13/lecture_notes/note_1/SequentialTest.java
@@ -0,0 +1,19 @@
+package lecture_notes.note_1;
+
+import java.util.Arrays;
+
+public class SequentialTest {
+    public static void main(String[] args) {
+        long[] sum = new long[16];
+
+        long before = System.currentTimeMillis();
+        for (int i = 1; i <= 16; i++)
+            for (int j = 0; j < 10_000_000; j++)
+                sum[i - 1] += i * j + Math.pow(i, 3);
+
+        long after = System.currentTimeMillis();
+
+        System.out.println("Time spend " + (after - before) + " millis");
+        System.out.println("Result: " + Arrays.stream(sum).sum());
+    }
+}
