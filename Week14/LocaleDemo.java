f42f8128cc9aa237665a972bb234220e11d2fd91
leylaneymat <lneymat17256@ada.edu.az>
2023-05-05 10:30:02 +0400

    week 14

--- Week14/LocaleDemo.java
+++ Week14/LocaleDemo.java
@@ -1,3 +1,4 @@
+import java.text.NumberFormat;
 import java.util.Locale;
 
 public class LocaleDemo {
@@ -8,7 +9,18 @@ public class LocaleDemo {
                 .setLanguage("fr")
                 .setRegion("CA")
                 .build();
-                
+
+        var value = 123.456;
+        NumberFormat defFormatter = NumberFormat.getInstance();
+        System.out.println(defFormatter.format(value));
+
+        // Locale. CANADA_FRENCH
+        NumberFormat frCAFormatter = NumberFormat. getInstance(Locale.of("fr", "CA")); System.out.println(frCAFormatter.format(value));
+       
+        var share = 0.0045;
+        System.out.println(NumberFormat.getPercentInstance().format(share));
+        System.out.println(NumberFormat.getPercentInstance(Locale.CANADA_FRENCH).format(share));
+
         System.out.println(defLocale);
         System.out.println(enUkLocale);
         Locale.setDefault(enUkLocale);
