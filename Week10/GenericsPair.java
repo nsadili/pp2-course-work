f5094225860f472adb744a62bf8094e7c58192ce
Sabir18123 <118388176+Sabir18123@users.noreply.github.com>
2023-05-07 20:28:25 +0400

    Add files via upload

---
+++ Week10/GenericsPair.java
@@ -0,0 +1,45 @@
+package Week10;
+
+import java.util.Objects;
+
+public class GenericsPair<K, V> {
+    private final K key;
+    private final V value;
+
+    public GenericsPair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GenericsPair<?, ?> pair = (GenericsPair<?, ?>) o;
+        if (!Objects.equals(key, pair.key)) return false;
+        return Objects.equals(value, pair.value);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = key != null ? key.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ Week10/Stack.java
@@ -0,0 +1,64 @@
+package Week10;
+
+import java.util.EmptyStackException;
+
+public class Stack<T> {
+    private class Node<T> {
+        T data;
+        Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<T> top;
+    private int size;
+    private int capacity;
+
+    public Stack(int capacity) {
+        this.top = null;
+        this.size = 0;
+        this.capacity = capacity;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void push(T data) throws StackOverflowError {
+        if (isFull()) {
+            throw new StackOverflowError();
+        }
+        Node<T> newNode = new Node<>(data);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public T pop() throws EmptyStackException {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
+    }
+
+    public T peek() throws EmptyStackException {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+}
