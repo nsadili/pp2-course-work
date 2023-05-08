1e28f92ec2770a5ef2143f9ff2d7780545120f27
Gulnar Hasanli 17578 <ghasanli17578@ada.edu.az>
2023-05-08 15:26:04 +0400

    bonus add

---
+++ Week09/Bonus.java
@@ -0,0 +1,43 @@
+package Week09;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Random;
+
+public class Bonus {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        Random random = new Random();
+
+        long methodOneStarts = System.nanoTime();
+        for (int i = 0;i < 100; i++) {
+            int randomNumber = random.nextInt(100);
+            ListIterator<Integer> iterator = list.listIterator();
+            while (iterator.hasNext()) {
+                if (randomNumber < iterator.next()) {
+                    iterator.previous();
+                    iterator.add(randomNumber);
+                    break;
+                }
+            }
+            if (!iterator.hasNext()) {
+                iterator.add(randomNumber);
+            }
+        }
+        long methodOneEnds = System.nanoTime();
+        System.out.println("Time elapsed for the first method: " + (methodOneEnds - methodOneStarts));
+
+        long methodTwoStarts = System.nanoTime();
+        for (int i = 0; i < 100; i++) {
+            int randomNumber = random.nextInt(100);
+            list.add(randomNumber);
+        }
+        list.sort(null);
+        long methodTwoEnds = System.nanoTime();
+        System.out.println("Time elapsed for the second method: " + (methodTwoEnds - methodTwoStarts));
+
+        System.out.println(list.toString());
+    }
+}
