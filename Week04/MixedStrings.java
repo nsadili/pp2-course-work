c0e823e51a184258af64c4782fc2347f2003f96f
Aysu Rahimli 18053 <arahimli18053@ada.edu.az>
2023-05-09 22:29:19 +0400

    week04

---
+++ Week04/MixedStrings.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class MixedStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please, enter a sentence: ");
+        String sentence = scanner.nextLine();
+        String mixedSentence = swapFirstAndLastChars(sentence);
+        System.out.println("Mixed sentence: " + mixedSentence);
+    }
+
+    public static String swapFirstAndLastChars(String sentence) {
+        String[] words = sentence.split("\\s+");
+        StringBuilder mixedSentence = new StringBuilder();
+
+        for (String word : words) {
+            if (word.length() == 1) {
+                mixedSentence.append(word);
+            } else {
+                char firstChar = word.charAt(0);
+                char lastChar = word.charAt(word.length() - 1);
+                String middleChars = word.substring(1, word.length() - 1);
+                mixedSentence.append(lastChar).append(middleChars).append(firstChar);
+            }
+            mixedSentence.append(" ");
+        }
+
+        return mixedSentence.toString().trim();
+    }
+}
