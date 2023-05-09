918e217b0fd90f0bb6c7fe16ad14dc6ca2a6417d
itsmurad <118719384+itsmurad@users.noreply.github.com>
2023-05-09 13:28:06 +0400

    Create Locale.java

---
+++ Week14/Locale.java
@@ -0,0 +1,31 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Locale {
+    public static void main(String[] args) {
+        Locale defLocale = Locale.getDefault();
+        Locale enUkLocale = Locale.of("en", "UK");
+        var testLocal = new Locale.Builder()
+                .setLanguage("fr")
+                .setRegion("CA")
+                .build();
+
+        var value = 123.456;
+        NumberFormat defFormatter = NumberFormat.getInstance();
+        System.out.println(defFormatter.format(value));
+
+        // Locale. CANADA_FRENCH
+        NumberFormat frCAFormatter = NumberFormat. getInstance(Locale.of("fr", "CA")); System.out.println(frCAFormatter.format(value));
+       
+        var share = 0.0045;
+        System.out.println(NumberFormat.getPercentInstance().format(share));
+        System.out.println(NumberFormat.getPercentInstance(Locale.CANADA_FRENCH).format(share));
+
+        System.out.println(defLocale);
+        System.out.println(enUkLocale);
+        Locale.setDefault(enUkLocale);
+        System.out.println(Locale.getDefault());
+        System.out.println(testLocal);
+
+    }
+}
