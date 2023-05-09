b7a462afa3094d3a1285c98b5b093b2d1b186e75
itsmurad <118719384+itsmurad@users.noreply.github.com>
2023-05-09 13:19:38 +0400

    Create Sum.java

---
+++ Week06/Arithmetics/Sum.java
@@ -0,0 +1,14 @@
+package Week06.Arithmetics;
+
+public class Sum extends BinaryOperation {
+
+    public Sum(EvalInterface op1, EvalInterface op2) {
+        super("+", op1, op2);
+    }
+
+    @Override
+    protected double calc(EvalInterface op1, EvalInterface op2) {
+        return op1.toValue() + op2.toValue();
+    }
+
+}
