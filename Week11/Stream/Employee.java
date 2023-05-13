661c57cedcc926418cb4bbe70335dba3c7cf35b8
IsmayilovaSara <ismayilovassara@gmail.com>
2023-05-13 13:45:05 +0400

    final changes

---
+++ Week11/Stream/Employee.java
@@ -0,0 +1,34 @@
+public class Employee 
+{
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(String name, String department, double salary) 
+    {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getDepartment() 
+    {
+        return department;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public double getSalary() 
+    {
+        return salary;
+    }
+    @Override
+    public String toString() 
+    {
+        return name + " department:"+ department + " salary:" + salary + " ";
+    }
+
+}
