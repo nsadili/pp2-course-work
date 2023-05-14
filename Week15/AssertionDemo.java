b52f5d96591c84c29a26e16ed2aff8cfbe427013
Vusat Ahmadzada <vahmadzada14086@ada.edu.az>
2023-05-14 18:01:15 +0400

    Week 15

---
+++ Week15/AssertionDemo.java
@@ -0,0 +1,7 @@
+public class AssertionDemo {
+    public static void main(String[] args) {
+        var age = 10;
+        assert age > 20 : "Underage are not allowed";
+        System.out.println("Welcome to party");
+    }
+}
