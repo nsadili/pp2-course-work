7f8c26dbcf5b258b902eef7289f406d1a99d2b71
BakhtiyarGuluzada-15987 <bguluzada15987@ada.edu.az>
2023-05-18 10:46:10 +0400

    week 13 missing commit

---
+++ Week13/problem4/Consumer.java
@@ -0,0 +1,18 @@
+package problem4;
+
+class Consumer extends Thread {
+	private Stock stock;
+
+	public Consumer(String name, Stock stock) {
+		super(name);
+		this.stock = stock;
+	}
+
+	public void run() {
+		try {
+			stock.consume();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
