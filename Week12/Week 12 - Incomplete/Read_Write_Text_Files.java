99c6a942dd253eab55f9541f7054b34c93b5bc10
Garib Guluzada 16067 <gguluzada16067@ada.edu.az>
2023-05-19 16:24:26 +0400

    Week 12

---
+++ Week12/Week 12 - Incomplete/Read_Write_Text_Files.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Read_Write_Text_Files {
+
+    public static void main(String[] args) {
+
+        File input = new File("input.txt");
+        // fileWrite(input,"Hello! My name is Shamil Abbasov");
+        // fileWrite(input,"\nToday, I got my driving license.");
+
+        fileWrite(input, "I am happy , Gladious :):)");
+
+        var res = fileRead(input);
+        System.out.println(res);
+
+    }
+
+    static void fileWrite(File file, String text) {
+
+        try (FileWriter fw = new FileWriter(file, false)) {
+
+            BufferedWriter bf = new BufferedWriter(fw);
+            bf.write(text);
+            bf.flush();
+            bf.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static String fileRead(File file) {
+        StringBuilder res = new StringBuilder();
+        try (FileReader fw = new FileReader(file)) {
+
+            BufferedReader bf = new BufferedReader(fw);
+            char[] arr = new char[16];
+            // bf.read(arr);
+
+            int len;
+            while ((len = bf.read(arr)) != -1) {
+
+                // res.append(new String (arr,0,len));
+                res.append(arr, 0, len);
+
+            }
+            // var x=0;
+            // while((x=bf.read())!=-1){
+            // res.append((char)x);
+            // }
+
+            bf.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+        return res.toString();
+
+    }
+
+}
