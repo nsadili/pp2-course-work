dd1c6ad882ede709814304624934759568bbea87
Samad045 <118782326+Samad045@users.noreply.github.com>
2023-05-20 00:44:56 +0400

    PoemPrinter.java

---
+++ Week12/PoemPrinter.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+class PoemPrinter {
+    public static void main(String args[]) {
+        readLines("Resources/ex1/poem.txt");
+    }
+
+    static void readLines(String path) {
+        try (FileReader fr = new FileReader(new File(path), Charset.forName("UTF-8"));
+                BufferedReader br = new BufferedReader(fr);
+
+                BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt", StandardCharsets.UTF_8))) {
+            String str = "";
+            while ((str = br.readLine()) != null)
+                // System.out.println(str);
+                bw.write(str);
+        } catch (IOException ex) {
+            System.out.println("Reading a text file: " + ex);
+        }
+    }
+
+    // Advantage of wrapping is that it has readLine() method which is very helpful
+    // while reading poems line by line
+
+}
