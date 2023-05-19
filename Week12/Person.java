2d4788c5ac566a782431258b8ae0846af4be2a59
Said Ahadov 17939 <said@saids-MacBook-Pro-2.local>
2023-05-19 17:03:19 +0400

    Person class

---
+++ Week12/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    // Constructor, getters, and setters
+
+    // toString() method
+}
