61beb3feae2551698e27f2cd107d3a0b3edbffc1
nibrahimli16079 <nibrahimli16079@ada.edu.az>
2023-05-20 12:24:34 +0400

    Week 14

---
+++ Week14/AssertDemo.java
@@ -0,0 +1,14 @@
+package Week14;
+
+public class AssertDemo {
+    public static void main(String[] args) {
+        
+        int age = 15;
+
+        assert age > 20 : "Teens are not allowed! ";
+
+        System.out.println("You are welcome");
+
+
+    }
+}
