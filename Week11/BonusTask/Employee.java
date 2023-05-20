aa9f021e8443ea286e52dcfded537a9fb6e0049f
Said Ahadov 17939 <said@saids-MacBook-Pro-2.local>
2023-05-20 16:46:06 +0400

    Employee class

---
+++ Week11/BonusTask/Employee.java
@@ -0,0 +1,25 @@
+package BonusTask;
+
+class Employee {
+    private String name;
+    private String department;
+    private double salary;
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
