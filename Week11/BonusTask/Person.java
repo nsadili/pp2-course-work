df91fcdb275263df4f875b1a9c3ffb0594a1a54c
Said Ahadov 17939 <said@saids-MacBook-Pro-2.local>
2023-05-20 16:44:44 +0400

    Person class

---
+++ Week11/BonusTask/Person.java
@@ -0,0 +1,25 @@
+package BonusTask;
+
+class Person {
+    private String name;
+    private int age;
+    private String city;
+
+    public Person(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
