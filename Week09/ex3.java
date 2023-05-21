33ae8ddef4660252983975294f1c7dda891c6f08
fbakirli <113505730+fbakirli@users.noreply.github.com>
2023-05-21 20:06:56 +0400

    new exercise

---
+++ Week09/ex3.java
@@ -0,0 +1,41 @@
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ex3 {
+    public static void main(String[] args) {
+         Map<String, Double> students = new HashMap<>();
+         students.put("Ayten", 3.8);
+         students.put("Ilqar", 1.8);
+         students.put("Daniz", 3.1);
+         students.put("Firangiz", 2.9);
+         students.put("Farida", 2.8);
+
+         double highestGPA = 0;
+         String bestStudent = "";
+         for (Map.Entry<String, Double> entry : students.entrySet()) {
+             if (entry.getValue() > highestGPA) {
+                 highestGPA = entry.getValue();
+                 bestStudent = entry.getKey();
+             }
+         }
+         System.out.println(bestStudent + " has the highest GPA of " + highestGPA);
+ 
+         double total = 0;
+         for (Double gpa : students.values()) {
+             total += gpa;
+         }
+         double averageGPA = total / students.size();
+         System.out.println("The average GPA of all students is " + averageGPA);
+ 
+         int belowAverage = 0;
+         for (Double gpa : students.values()) {
+             if (gpa < averageGPA) {
+                 belowAverage++;
+             }
+         }
+         System.out.println(belowAverage + " students have less GPA than the average");
+     
+    }
+}
---
+++ Week09/ex4.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ex4 {
+    public static void main(String[] args) {
+        String input=args[0].toLowerCase();
+        ArrayList<String> list= new ArrayList<String>();
+        char[] chars=input.toCharArray();
+        
+        for(int i=0;i<chars.length;i++){
+          list.add(String.valueOf(chars[i]));    
+        }
+        Collections.sort(list);
+          System.out.println("Non-descending order:");
+          System.out.println(list);
+          System.out.println("Non-ascending order:");
+          Collections.sort(list, Collections.reverseOrder());
+          System.out.println(list);
+    }
+}
