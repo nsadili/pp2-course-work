e57abd43ff5a5a2603f96734299ae38d0449bb15
fbakirli <113505730+fbakirli@users.noreply.github.com>
2023-05-24 17:53:36 +0400

    add

---
+++ Week08/Stack1.java
@@ -0,0 +1,13 @@
+public class Stack1 {
+    public static void main(String[] args) {
+        LinkedList ll=new LinkedList<>();
+
+        ll.push(3);
+        ll.push(5);
+        ll.push(7);
+        ll.push(10);
+        ll.push(100);
+
+        System.out.println(ll);
+    }
+}
