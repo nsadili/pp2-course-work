f2a2db0652164998f3104698a6905dc00d100739
Ulkar Iskandarli <uiskandarli17482@ada.edu.az>
2023-05-25 12:28:22 +0400

    add

---
+++ Week12/FileSystemTraversal.java
@@ -0,0 +1,44 @@
+package Week12;
+
+import java.io.File;
+
+public class FileSystemTraversal {
+
+    public static void main(String[] args) {
+        String path = "C:/Users/username/Desktop";
+        File file = new File(path);
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                traverseDirectory(file);
+            } else {
+                printFileInfo(file);
+            }
+        } else {
+            System.out.println("File or directory not found.");
+        }
+    }
+
+    public static void traverseDirectory(File directory) {
+        System.out.println("Directory: " + directory.getAbsolutePath());
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    traverseDirectory(file);
+                } else {
+                    printFileInfo(file);
+                }
+            }
+        }
+    }
+
+    public static void printFileInfo(File file) {
+        System.out.println("File: " + file.getAbsolutePath());
+        System.out.println("Size: " + file.length() + " bytes");
+        System.out.println("Readable: " + file.canRead());
+        System.out.println("Writable: " + file.canWrite());
+        System.out.println("Executable: " + file.canExecute());
+        System.out.println("Hidden: " + file.isHidden());
+        System.out.println();
+    }
+}
