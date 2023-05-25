fbf72315cec1ae304c6639f30d275f25f6ce12c6
Teymur Aliyev 16286 <taliyev16286@ada.edu.az>
2023-05-25 15:32:16 +0400

    add exercise

---
+++ Week03/CustomDateTest.java
@@ -0,0 +1,20 @@
+public class CustomDateTest {
+    public static void main(String[] args) {
+        CustomDate date1 = new CustomDate(31, 12, 2022);
+        CustomDate date2 = new CustomDate(1, 1, 2023);
+
+        System.out.println("The Date1 is: ");
+        date1.displayDate();
+        date1.displayFormatted();
+
+        System.out.println();
+
+        System.out.println("The Date2 is: ");
+        date2.displayDate();
+        date2.displayFormatted();
+
+        System.out.println();
+
+        System.out.println(CustomDate.compare(date1, date2));
+    }
+}
---
+++ Week03/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Point p1 = new Point(1, 5);
+        Point p2 = new Point(9, 12);
+        Segment s = new Segment(p1, p2);
+
+        System.out.println("P1 is: (" + p1.getX() + "; " + p1.getY() + ").");
+        System.out.println("P2 is: (" + p2.getX() + "; " + p2.getY() + ").");
+        System.out.println("P1 equals P2: " + p1.equals(p2));
+        System.out.println("Distance between P1 and P2 is: " + p1.distance(p2));
+        System.out.println("The segment is on line with P1: " + s.isOnLine(p1));
+        System.out.println("The segment is on line with P2: " + s.isOnLine(p2));
+        System.out.println("The intercept of segment is: " + s.getIntercept());
+        System.out.println("The slope of the segment: " + s.getSlope());
+    }
+}
