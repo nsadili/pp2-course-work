e459e0e055359ac4f78291d28868072bdb89a40b
Ulkar Iskandarli <uiskandarli17482@ada.edu.az>
2023-05-25 11:55:09 +0400

    add

---
+++ Week10/GenericPair.java
@@ -0,0 +1,25 @@
+package Week10;
+
+public class GenericPair<K, V> {
+    private final K key;
+    private final V value;
+    
+    public GenericPair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+    
+    public K getKey() {
+        return key;
+    }
+    
+    public V getValue() {
+        return value;
+    }
+    
+    public String toString() {
+        return "(" + key + ", " + value + ")";
+    }
+}
+
+
