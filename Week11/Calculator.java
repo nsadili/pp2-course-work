dcdd9c06300f77729dd677c84dcb474850552232
Ulkar Iskandarli <uiskandarli17482@ada.edu.az>
2023-05-25 12:10:19 +0400

    add

---
+++ Week11/Calculator.java
@@ -0,0 +1,16 @@
+
+
+public class Calculator {
+    public static void main(String[] args) {
+        CalculatorInterface addition = (a, b) -> a + b;
+        CalculatorInterface subtraction = (a, b) -> a - b;
+        CalculatorInterface multiplication = (a, b) -> a * b;
+        CalculatorInterface division = (a, b) -> a / b;
+        CalculatorInterface exponentiation = (a, b) -> Math.pow(a, b);
+    }
+}
+
+
+interface CalculatorInterface {
+    double calculate(double a, double b);
+}
